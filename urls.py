ce4167fe58ddeff6a4f3c4c267d64a7214aa4def
Salvatore Iovene <salvatore@iovene.com>
2010-11-27 19:59:08 +0000

    Use view.

--- urls.py
+++ urls.py
@@ -3,8 +3,10 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
+from bin import views
+
 urlpatterns = patterns('',
-    (r'^bin/$', include('bin.views.index')),
+    (r'^bin/$', views.index),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
