9e3e2d8f16d640799ee540ddf01d358e1ab144b8
Salvatore Iovene <salvatore@iovene.com>
2010-11-27 19:11:18 +0000

    Add models to admin interface.

---
+++ bin/admin.py
@@ -0,0 +1,11 @@
+from bin.models import *
+from django.contrib import admin
+
+admin.site.register(Camera)
+admin.site.register(Telescope)
+admin.site.register(Mount)
+admin.site.register(FocalReducer)
+admin.site.register(Subject)
+admin.site.register(User)
+admin.site.register(Image)
+
