bbdb093d5e48b6e765298a8d3fd0019143663d36
Salvatore Iovene <salvatore@iovene.com>
2010-11-27 20:33:36 +0000

    Basic auth works.

--- settings.py
+++ settings.py
@@ -69,6 +69,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/var/www/astrobin/templates'
 )
 
 INSTALLED_APPS = (
@@ -78,4 +79,5 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'bin',
     'django.contrib.admin',
+    'registration',
 )
