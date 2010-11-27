f01427b9db1340bc5e621f8cbd9d40e7d072ed88
Salvatore Iovene <salvatore@iovene.com>
2010-11-27 18:48:56 +0000

    Add models.

--- bin/models.py
+++ bin/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Gear(models.Model):
+	name = models.CharField(max_length=64)
+	purchase_date = models.DateTimeField('date purchased')
+
+class Camera(Gear):
+	chip_size = models.IntegerField()
+
+class Telescope(Gear):
+	focal_length = models.IntegerField()
+	aperture = models.IntegerField()
+
+class Mount(Gear):
+
+class FocalReduced(Gear):
+	ratio = models.DoubleField()
+
+class Subject(models.Model):
+	name = model.CharField(max_length=64)
+
+	# gear
+	camera = models.ForeignKey(Camera)
+	focal_reducer = models.ForegnKey(FocalReducer)
+	telescope = models.ForeignKey(Telescope)
+	mount = models.ForeignKey(Mount)
+
+	# data
