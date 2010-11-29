8b5098d843ae8638bb17d6d2959dd3a64bbc1789
Salvatore Iovene <salvatore@iovene.com>
2010-11-29 20:14:58 +0000

    Able to save basic image data but no file name yet.

--- settings.py
+++ settings.py
@@ -35,7 +35,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/var/www/astrobin/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
