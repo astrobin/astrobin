a5b8f1876eba9b7e49631237f3e9c17d7a8823df
Salvatore Iovene <salvatore@iovene.com>
2010-11-29 17:35:42 +0000

    Work.

--- urls.py
+++ urls.py
@@ -6,12 +6,15 @@ admin.autodiscover()
 from bin import views
 
 urlpatterns = patterns('',
-    (r'^bin/$', views.index),
-    (r'^accounts/', include('registration.urls')),
+	(r'^bin/$', views.index),
+	(r'^list/$', views.image_list),
+	(r'^show/(?P<id>\d+)/$', views.image_detail),
+	(r'^upload/$', views.image_create),
+	(r'^accounts/', include('registration.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+	# to INSTALLED_APPS to enable admin documentation:
+	# (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    (r'^admin/(.*)', admin.site.root),
+	(r'^admin/(.*)', admin.site.root),
 )
