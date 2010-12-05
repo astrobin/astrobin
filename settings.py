f2b1aef70fedab545416179a7fdfca8336849404
Salvatore Iovene <salvatore@iovene.com>
2010-12-05 15:36:21 +0000

    Some CSS and HTML.

--- settings.py
+++ settings.py
@@ -35,12 +35,12 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/var/www/astrobin/media'
+MEDIA_ROOT = '/var/www/astrobin/static'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
