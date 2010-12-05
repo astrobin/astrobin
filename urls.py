d35c281f4aeb534e1ed465f5ed4ab76ca8437724
Salvatore Iovene <salvatore@iovene.com>
2010-12-05 15:36:21 +0000

    Some CSS and HTML.

--- urls.py
+++ urls.py
@@ -7,7 +7,6 @@ from bin import views
 
 urlpatterns = patterns('',
     (r'^$', views.index),
-    (r'^list/$', views.image_list),
     (r'^show/(?P<id>\d+)/$', views.image_detail),
     (r'^upload/$', views.image_upload),
     (r'^upload/process$', views.image_upload_process),
@@ -18,7 +17,7 @@ urlpatterns = patterns('',
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/(.*)', admin.site.root),
-
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     (r'^(?P<username>\w+)/$', views.user_page),
     (r'^(?P<username>\w+)/edit/$', views.user_profile_edit),
 )
