2dd2a5498d0345c01e73856138e31fce3b9265d7
Salvatore Iovene <salvatore@iovene.com>
2010-12-10 18:35:51 +0000

    ajax-select in admin interface.

--- bin/admin.py
+++ bin/admin.py
@@ -1,14 +1,24 @@
 from bin.models import *
 from django.contrib import admin
 from ajax_select import make_ajax_form
+from ajax_select.admin import AjaxSelectAdmin
+from ajax_select.fields import autoselect_fields_check_can_add
 
-#admin.site.register(Camera)
-#admin.site.register(Telescope)
-#admin.site.register(Mount)
-#admin.site.register(FocalReducer)
-#admin.site.register(Subject)
-#admin.site.register(Image)
+admin.site.register(Gear)
+admin.site.register(Telescope)
+admin.site.register(Mount)
+admin.site.register(Camera)
+admin.site.register(FocalReducer)
+admin.site.register(Subject)
+admin.site.register(Image)
+admin.site.register(ABPOD)
 
-#class UserProfileEditGearAdmin(Admin):
-#    form = make_ajax_form(UserProfile, 
-#        dict(telescopes='telescope'))
+class UserProfileAdmin(AjaxSelectAdmin):
+    form = make_ajax_form(UserProfile, dict(telescopes='telescope'))
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(AjaxSelectAdmin, self).get_form(request, obj, **kwargs) 
+        autoselect_fields_check_can_add(form, self.model, request.user)
+        return form
+        
+admin.site.register(UserProfile, UserProfileAdmin)
