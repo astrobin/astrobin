f16d8eada34e61f37823dd8bf9f7d790d807748e
Salvatore Iovene <salvatore@iovene.com>
2010-12-11 09:54:21 +0000

    Specify ADMIN_MEDIA_PREFIX.

--- settings.py
+++ settings.py
@@ -45,7 +45,7 @@ MEDIA_URL = '/static/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '***REMOVED***'
