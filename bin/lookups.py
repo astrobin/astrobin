7ff54e806855ba99254458cc803b0ef972e56617
Salvatore Iovene <salvatore@iovene.com>
2010-12-11 17:02:30 +0000

    Use fcbkcomplete.

--- bin/lookups.py
+++ bin/lookups.py
@@ -1,16 +1,12 @@
 from models import Telescope
 from django.db.models import Q 
+from django.http import HttpResponse
+import simplejson
 
-class TelescopeLookup(object):
-    def get_query(self,q,request):
-        return Telescope.objects.filter(Q(name__istartswith=q))
+def autocomplete(request, what):
+    values = ()
+    if what == 'telescopes':
+        values = Telescope.objects.filter(Q(name__istartswith=request.GET['tag']))
 
-    def format_item(self,telescope):
-        return unicode(telescope)
-
-    def format_result(self,telescope):
-        return "%s<br/>" % unicode(telescope)
-
-    def get_objects(self,ids):
-        return Telescope.objects.filter(pk__in=ids).order_by('name')
+    return HttpResponse(simplejson.dumps([{'key': v.name, 'value': v.id} for v in values]))
 
