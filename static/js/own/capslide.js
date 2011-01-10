3e1786fb9f275b62bf1a72e2780e6affc42dae90
Salvatore Iovene <salvatore@iovene.com>
2011-01-10 16:15:56 +0000

    Show user's image list.

---
+++ static/js/own/capslide.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".ic_container").capslide({
+        caption_color   : 'black',
+        caption_bgcolor : 'white',
+        overlay_bgcolor : 'white',
+        border          : '',
+        showcaption     : false
+    });
+});
+
