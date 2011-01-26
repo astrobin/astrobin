1f3c6d21a45434921539e3846fd915099283a1c0
Salvatore Iovene <salvatore@iovene.com>
2011-01-26 08:17:29 +0000

    Better menus.

--- static/js/menus.js
+++ static/js/menus.js
@@ -2,15 +2,16 @@ var timeout    = 500;
 var closetimer = 0;
 var item       = 0;
 
+function top_close() {
+    if (item) {
+        item.css('visibility', 'hidden');
+    }
+}
+
 function top_open(_parent, _klass) {
     top_canceltimer();
     top_close();
     item = _parent.find(_klass).css('visibility', 'visible');
-}
-
-function top_close() {
-    if (item)
-        item.css('visibility', 'hidden');
 }
 
 function top_timer() {
