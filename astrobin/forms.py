442f79dbed081d3475a17be7adf80e0b41691254
Salvatore Iovene <salvatore@iovene.com>
2011-02-14 18:56:03 +0000

    Hint for noscript fields that would otherwise autosuggest.

--- astrobin/forms.py
+++ astrobin/forms.py
@@ -12,37 +12,37 @@ class ImageUploadForm(forms.Form):
 class ImageEditBasicForm(forms.Form):
     title = forms.CharField(max_length=64)
     subjects = forms.CharField(required=False)
-    locations = forms.CharField(required=False)
+    locations = forms.CharField(required=False, help_text="<noscript>*</noscript>")
     description = forms.CharField(widget=forms.Textarea, required=False)
 
 
 class ImageEditGearForm(forms.Form):
-    imaging_telescopes = forms.CharField(max_length=256)
-    guiding_telescopes = forms.CharField(max_length=256)
-    mounts = forms.CharField(max_length=256)
-    imaging_cameras = forms.CharField(max_length=256)
-    guiding_cameras = forms.CharField(max_length=256)
-    focal_reducers = forms.CharField(max_length=256)
-    software = forms.CharField(max_length=256)
-    filters = forms.CharField(max_length=256)
-    accessories = forms.CharField(max_length=256)
+    imaging_telescopes = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    guiding_telescopes = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    mounts = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    imaging_cameras = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    guiding_cameras = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    focal_reducers = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    software = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    filters = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    accessories = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
 
 
 class UserProfileEditBasicForm(forms.ModelForm):
-    locations = forms.CharField(max_length=64, required=False)
+    locations = forms.CharField(max_length=64, required=False, help_text="<noscript>*</noscript>")
     class Meta:
         model = UserProfile
         fields = ('website', 'job', 'hobbies')
 
 
 class UserProfileEditGearForm(forms.Form):
-    telescopes = forms.CharField(max_length=256)
-    mounts = forms.CharField(max_length=256)
-    cameras = forms.CharField(max_length=256)
-    focal_reducers = forms.CharField(max_length=256)
-    software = forms.CharField(max_length=256)
-    filters = forms.CharField(max_length=256)
-    accessories = forms.CharField(max_length=256)
+    telescopes = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    mounts = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    cameras = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    focal_reducers = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    software = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    filters = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
+    accessories = forms.CharField(max_length=256, help_text="<noscript>*</noscript>")
 
 
 class PrivateMessageForm(forms.Form):
