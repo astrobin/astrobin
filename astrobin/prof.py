325e943bfb45e1a3be0a8626180b2b0d713fda42
Salvatore Iovene <salvatore@iovene.com>
2011-02-20 13:41:32 +0000

    Add files.

---
+++ astrobin/prof.py
@@ -0,0 +1,22 @@
+import sys
+import cProfile
+from cStringIO import StringIO
+from django.conf import settings
+
+class ProfilerMiddleware(object):
+    def process_view(self, request, callback, callback_args, callback_kwargs):
+        if settings.DEBUG and 'prof' in request.GET:
+            self.profiler = cProfile.Profile()
+            args = (request,) + callback_args
+            return self.profiler.runcall(callback, *args, **callback_kwargs)
+
+    def process_response(self, request, response):
+        if settings.DEBUG and 'prof' in request.GET:
+            self.profiler.create_stats()
+            out = StringIO()
+            old_stdout, sys.stdout = sys.stdout, out
+            self.profiler.print_stats(1)
+            sys.stdout = old_stdout
+            response.content = '<pre>%s</pre>' % out.getvalue()
+        return response
+
