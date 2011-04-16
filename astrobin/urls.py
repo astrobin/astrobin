fc320d6713f6c5d64383d1af286b1501b9ddf2bf
Alberto Mardegan <mardy@users.sourceforge.net>
2011-04-16 13:18:52 +0000

    urls.py: admin.site.root is deprecated
    It is deprecated since 1.1:
    http://docs.djangoproject.com/en/dev/releases/1.1/
    
    committer: Salvatore Iovene <salvatore@iovene.com>

--- astrobin/urls.py
+++ astrobin/urls.py
@@ -33,7 +33,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', include(admin.site.urls)),
 
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     (r'^profile/edit/$', views.user_profile_edit_basic),
