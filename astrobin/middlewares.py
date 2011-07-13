b2a04b139a3b0e5e73c75539fb371427c578e9ea
Salvatore Iovene <salvatore@iovene.com>
2011-07-13 15:38:52 +0300

    Install the privatebeta app.

--- astrobin/middlewares.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponseRedirect
-from django.conf import settings
-
-class ComingSoonMiddleware:
-    def process_request(self, request):
-        if settings.ASTROBIN_COMING_SOON:
-            if request.path_info != settings.ASTROBIN_COMING_SOON_URL: 
-                return HttpResponseRedirect(settings.ASTROBIN_COMING_SOON_URL)
-
