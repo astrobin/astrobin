95f298c0b3f2d270e2b607732a3c6288a7679fce
Salvatore Iovene <salvatore@iovene.com>
2011-07-16 22:50:18 +0300

    Visual improvements to the advanced search form.
    Somehow, the form's search method is not run. Need to investigate.

--- astrobin/forms.py
+++ astrobin/forms.py
@@ -90,11 +90,11 @@ class AdvancedSearchForm(SearchForm):
 
         # Check to see if a start_date was chosen.
         if self.cleaned_data['start_date']:
-            sqs = sqs.filter(pub_date__gte=self.cleaned_data['start_date'])
+            sqs = sqs.filter(uploaded__gte=self.cleaned_data['start_date'])
 
         # Check to see if an end_date was chosen.
         if self.cleaned_data['end_date']:
-            sqs = sqs.filter(pub_date__lte=self.cleaned_data['end_date'])
+            sqs = sqs.filter(uploaded__lte=self.cleaned_data['end_date'])
 
         return sqs
 
