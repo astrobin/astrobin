4e91994da19e3cc9dbac58dbdceff327cdd549ae
Salvatore Iovene <salvatore@iovene.com>
2011-07-24 11:38:45 +0300

    Fixing names of the fields in the Subject model.

--- scripts/reset_db.sh
+++ scripts/reset_db.sh
@@ -3,4 +3,4 @@ echo "Remember to run this from astrobin's top source dir."
 echo "Use mysql root password below."
 mysql -u root -p < sql/reset_db.sql
 ./scripts/syncdb.sh
-./scripts/create_objects_db.sh
+./scripts/create_objects_db.py
