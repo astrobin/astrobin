bb9e62a09fa7ea0c187118c9a1b2b277c5f18abc
Salvatore Iovene <salvatore@iovene.com>
2011-07-24 11:38:45 +0300

    Fixing names of the fields in the Subject model.

--- astrobin/management/commands/create_objects_db.py
+++ astrobin/management/commands/create_objects_db.py
@@ -6,15 +6,35 @@ import csv
 class Command(BaseCommand):
     help = "Creates the full list of subjects, using the SAC database."
 
+
+    def normalized_name(self, name):
+        return name.strip().replace(' ', '_')
+
+
+    def normalized_value(self, value):
+        import re
+        return re.sub(' +', ' ', value.strip())
+
+
     def handle(self, *args, **options):
         data = csv.reader(open('data/SAC_DeepSky_Ver81_QCQ.TXT'))
         fields = data.next()
         for row in data:
-            print("Creating data for: %s" % row[0])
-            s = Subject()
-            # Zip together the field names and values  
-            items = zip(fields, row)  
-            # Add the value to our dictionary  
-            for (name, value) in items:  
-                setattr(s, name, value.strip())
+            # Zip together the field names and values
+            items = zip(fields, row)
+            s = None
+            try:
+                s = Subject.objects.get(OBJECT=self.normalized_name(items[0][1]))
+            except Subject.DoesNotExist:
+                s = Subject()
+
+            # Add the value to our dictionary
+            for (name, value) in items:
+                norm_n = self.normalized_name(name)
+                norm_v = self.normalized_value(value)
+
+                if norm_v is not None and norm_v != '':
+                    print("%s: %s" % (norm_n, norm_v))
+                    setattr(s, norm_n, norm_v)
+
             s.save()
