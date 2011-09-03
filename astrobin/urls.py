c50fca76551c1d8f6060ef543d520b14db43a8e4
Salvatore Iovene <salvatore@iovene.com>
2011-09-03 12:37:42 +0300

    Fixes #13 The admin section doesn't work anymore

--- astrobin/urls.py
+++ astrobin/urls.py
@@ -34,7 +34,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-       (r'^admin/(.*)', include(admin.site.urls)),
+       (r'^admin/', include(admin.site.urls)),
 
        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
