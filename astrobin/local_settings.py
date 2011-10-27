fe306f4b9d7be3e7593927d6de9f98b0d55b4e96
Salvatore Iovene <salvatore@iovene.com>
2011-10-27 20:24:46 +0300

    Moving templates/ into astrobin/

--- astrobin/local_settings.py
+++ astrobin/local_settings.py
@@ -114,7 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ASTROBIN_BASE_PATH + '/templates',
+    ASTROBIN_BASE_PATH + '/astrobin/templates',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
