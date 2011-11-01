719d23ead32cf9d0157ee2bf402ac4d36eb4edf4
Salvatore Iovene <salvatore@iovene.com>
2011-11-01 20:37:59 +0200

    Refactor subprocessing of solve-field, and timeout and killing.

--- astrobin/tasks.py
+++ astrobin/tasks.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.core.cache import cache
+
 from celery.decorators import task
 from celery.task.sets import subtask
 from boto.exception import S3CreateError
@@ -10,59 +12,82 @@ import StringIO
 import os
 import os.path
 import signal
+import time
+import threading
 
 from image_utils import *
 from storage import *
 from notifications import *
 
-class Alarm(Exception):
-    pass
-
-
-def alarm_handler(signum, frame):
-    raise Alarm
-
+SUBPROCESS_EXPIRE = 60 * 5
+LOCK_EXPIRE = SUBPROCESS_EXPIRE + 10
+LOCK_ID = "solve-image.lock"
 
 @task()
 def solve_image(image, callback=None):
-    # If solving is disabled in the settings, then we override what we're
-    # asked to do.
-    if not settings.ASTROBIN_ENABLE_SOLVING:
-        return
+    acquire_lock = lambda: cache.add(LOCK_ID, "true", LOCK_EXPIRE)
+    release_lock = lambda: cache.delete(LOCK_ID)
 
-    # Solve
-    path = settings.UPLOADS_DIRECTORY
-    uid = image.filename
-    original_ext = image.original_ext
-    solved = False
+    while not acquire_lock():
+        print "Waiting for lock."
+        time.sleep(5)
 
-    signal.signal(signal.SIGALRM, alarm_handler)
-    signal.alarm(270) # 4.5 minutes
-    command = ['nice', '-n', '5', '/usr/local/astrometry/bin/solve-field', path + uid + original_ext]
-    proc = subprocess.Popen(command)
     try:
-        proc.communicate()
-    except Alarm:
-        print "Task taking too long."
+        # If solving is disabled in the settings, then we override what we're
+        # asked to do.
+        if not settings.ASTROBIN_ENABLE_SOLVING:
+            return
 
-    solved_filename = settings.UPLOADS_DIRECTORY + image.filename + '-ngc.png'
-    if os.path.exists(settings.UPLOADS_DIRECTORY + image.filename + '.solved'):
-        solved = True
-        solved_file = open(solved_filename)
-        solved_data = StringIO.StringIO(solved_file.read())
-        solved_image = PILImage.open(solved_data)
+        # Solve
+        path = settings.UPLOADS_DIRECTORY
+        uid = image.filename
+        original_ext = image.original_ext
+        solved = False
 
-        (w, h) = solved_image.size
-        (w, h) = scale_dimensions(w, h, settings.RESIZED_IMAGE_SIZE)
-        solved_resizedImage = solved_image.resize((w, h), PILImage.ANTIALIAS)
+        def run_popen_with_timeout(command, timeout):
+            """
+            Run a sub-program in subprocess.Popen,
+            kill it if the specified timeout has passed.
+            """
+            kill_check = threading.Event()
+            def _kill_process_after_a_timeout(pid):
+                os.kill(pid, signal.SIGKILL)
+                os.system('killall -9 backend')
+                kill_check.set() # tell the main routine that we had to kill
+                return
+            p = subprocess.Popen(command)
+            pid = p.pid
+            watchdog = threading.Timer(timeout, _kill_process_after_a_timeout, args=(pid, ))
+            watchdog.start()
+            p.communicate()
+            watchdog.cancel() # if it's still waiting to run
+            success = not kill_check.isSet()
+            kill_check.clear()
+            return success
 
-        # Then save to bucket
-        solved_resizedFile = StringIO.StringIO()
-        solved_resizedImage.save(solved_resizedFile, 'PNG')
-        save_to_bucket(uid + '_solved.png', solved_resizedFile.getvalue())
+        command = ['/usr/local/astrometry/bin/solve-field', path + uid + original_ext]
+        run_popen_with_timeout(command, 300)
 
-    if callback is not None:
-        subtask(callback).delay(image, solved, '%s%s*' % (path, uid))
+        solved_filename = settings.UPLOADS_DIRECTORY + image.filename + '-ngc.png'
+        if os.path.exists(settings.UPLOADS_DIRECTORY + image.filename + '.solved'):
+            solved = True
+            solved_file = open(solved_filename)
+            solved_data = StringIO.StringIO(solved_file.read())
+            solved_image = PILImage.open(solved_data)
+
+            (w, h) = solved_image.size
+            (w, h) = scale_dimensions(w, h, settings.RESIZED_IMAGE_SIZE)
+            solved_resizedImage = solved_image.resize((w, h), PILImage.ANTIALIAS)
+
+            # Then save to bucket
+            solved_resizedFile = StringIO.StringIO()
+            solved_resizedImage.save(solved_resizedFile, 'PNG')
+            save_to_bucket(uid + '_solved.png', solved_resizedFile.getvalue())
+
+        if callback is not None:
+            subtask(callback).delay(image, solved, '%s%s*' % (path, uid))
+    finally:
+        release_lock()
 
 
 @task()
