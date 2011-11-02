170fa5dfe0d1523d18577085e6ff05e761681e1e
Salvatore Iovene <salvatore@iovene.com>
2011-11-02 20:11:08 +0200

    Run two celeryd queues (one exclusive to platesolving).
    The plate-solve queue has a concurrency of 1, that means the lock
    mechanism we use in the solve_image task is now redundant. It's ok
    to keep it though.

--- scripts/celeryd.sh
+++ scripts/celeryd.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-./manage.py celeryd -E --verbosity=2 --loglevel=DEBUG -c 4
+./manage.py celeryd -E --verbosity=2 --loglevel=DEBUG -c 4 --queues=default &
+./manage.py celeryd -E --verbosity=2 --loglevel=DEBUG -c 1 --queues=plate_solve &
