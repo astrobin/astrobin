377b3da37279b87144cc3dd0e0c34ef3913dd19f
Salvatore Iovene <salvatore@iovene.com>
2011-11-05 22:23:37 +0200

    Fix preparation of integration index.

--- astrobin/search_indexes.py
+++ astrobin/search_indexes.py
@@ -87,8 +87,7 @@ class ImageIndex(SearchIndex):
 
         if deep_sky_acquisitions:
             for a in deep_sky_acquisitions:
-                if a.acquisition_type in ('0l', '1r', '2g', '3b'):
-                    integration += (a.duration * a.number)
+                integration += (a.duration * a.number)
         elif solar_system_acquisition:
             integration = solar_system_acquisition.frames
 
