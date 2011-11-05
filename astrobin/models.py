62cb44a2fa2f1dc5c85a3b6871ee8ec56c0e5f7d
Salvatore Iovene <salvatore@iovene.com>
2011-11-05 22:52:13 +0200

    Use gettext_laxy in models.

--- astrobin/models.py
+++ astrobin/models.py
@@ -9,7 +9,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django import forms
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
 from celery.decorators import task
