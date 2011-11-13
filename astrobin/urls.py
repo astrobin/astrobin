4d8beaf0f460910f69475f11ade1a4efc605f1ae
Salvatore Iovene <salvatore@iovene.com>
2011-11-13 14:34:06 +0000

    On a second thought, it's ok if we 404 on invite/

--- astrobin/urls.py
+++ astrobin/urls.py
@@ -75,7 +75,6 @@ urlpatterns = patterns('',
     url(r'^request/image/additional_information/(?P<image_id>\d+)/$', views.image_request_additional_information, name='image_request_additional_information'),
     url(r'^request/image/fits/(?P<image_id>\d+)/$', views.image_request_fits, name='image_request_fits'),
 
-    url(r'^invite/', views.index, name='index'),
     url(r'^help/', views.help, name='help'),
     url(r'^faq/', views.faq, name='faq'),
     url(r'^locations/edit/(?P<id>\d+)/$', views.location_edit, name='location_edit'),
