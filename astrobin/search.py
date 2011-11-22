c17c69b1fb22847113d741857fe69bcdbab04d4d
Salvatore Iovene <salvatore@iovene.com>
2011-11-22 10:43:33 -0500

    More compact code FTW.

--- astrobin/search.py
+++ astrobin/search.py
@@ -12,24 +12,8 @@ class ImageSearchView(SearchView):
         search_type = self.request.GET.get('type')
         q = xapian_escape(self.request.GET.get('q')).replace(' ', '')
 
-        if search_type == 'imaging_telescopes':
-            sqs = sqs.filter(imaging_telescopes = q)
-        elif search_type == 'guiding_telescopes':
-            sqs = sqs.filter(guiding_telescopes = q)
-        elif search_type == 'mounts':
-            sqs = sqs.filter(mounts = q)
-        elif search_type == 'imaging_cameras':
-            sqs = sqs.filter(imaging_cameras = q)
-        elif search_type == 'guiding_cameras':
-            sqs = sqs.filter(guiding_cameras = q)
-        elif search_type == 'focal_reducers':
-            sqs = sqs.filter(focal_reducers = q)
-        elif search_type == 'software':
-            sqs = sqs.filter(software = q)
-        elif search_type == 'filters':
-            sqs = sqs.filter(filters = q)
-        elif search_type == 'accessories':
-            sqs = sqs.filter(accessories = q)
+        if search_type:
+            sqs = sqs.filter(**{search_type: q})
 
         if 'sort' in self.request.GET:
             order_by = self.request.GET['sort']
