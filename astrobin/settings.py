57dc9b4c1a10d1c1564fa7c4cae4757531630f08
Salvatore Iovene <salvatore@iovene.com>
2011-11-23 14:08:45 -0500

    Added ZINNIA_COPYRIGHT_SETTING.

--- astrobin/settings.py
+++ astrobin/settings.py
@@ -239,3 +239,6 @@ SIMBAD_QUERY_URL="http://simbad.u-strasbg.fr/simbad/sim-nameresolver?data=@,I.0,
 SIMBAD_SEARCH_QUERY_URL="http://simbad.u-strasbg.fr/simbad/sim-nameresolver?data=@,I.0,J,C.0,T,D,M,I&output=json&Ident="
 
 NOTIFICATION_LANGUAGE_MODULE = "astrobin.UserProfile"
+
+ZINNIA_COPYRIGHT = 'AstroBin'
+
