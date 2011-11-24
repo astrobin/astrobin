966cee1a38f9099fe041714fa4aad9c801835828
Salvatore Iovene <salvatore@iovene.com>
2011-11-24 00:34:40 -0500

    Adding support for tinymce.

---
+++ static/zinnia/css/config.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "."
+sass_dir = "src"
+images_dir = "../img"
+output_style = :compact
+line_comments = false
