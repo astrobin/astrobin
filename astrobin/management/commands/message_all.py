2d7e9a353cb19e49e072615c09ca9c590eabace2
Salvatore Iovene <salvatore@iovene.com>
2011-11-24 01:36:02 -0500

    Add command to message everybody.

---
+++ astrobin/management/commands/message_all.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+import persistent_messages
+
+class Command(BaseCommand):
+    help = "Sends a message to all users."
+
+    def handle(self, *args, **options):
+        if len(args) < 2:
+            print "Need two arbuments: subject and body."
+
+        subject = args[0]
+        body = args[1]
+        sender = User.objects.get(username = 'astrobin')
+
+        for recipient in User.objects.all():
+            if recipient.username != 'astrobin':
+                persistent_messages.add_message_without_storage(
+                    recipient,
+                    sender,
+                    persistent_messages.SUCCESS,
+                    body,
+                    subject = subject)
