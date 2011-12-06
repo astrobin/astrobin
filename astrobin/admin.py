a67f452f8741c47651fae232e88786cdc641a536
Alberto Mardegan <mardy@users.sourceforge.net>
2011-12-06 11:36:40 +0200

    Provide custom admin forms for UserProfile and Image
    The presence of the location field was making astrobin stuck and using tons of
    memory for loading the cities.

--- astrobin/admin.py
+++ astrobin/admin.py
@@ -1,6 +1,48 @@
 from astrobin.models import *
 from django.contrib import admin
 
+class ImageAdmin(admin.ModelAdmin):
+    fields = (
+        'title',
+        'subjects',
+        'description',
+        'focal_length',
+        'pixel_size',
+        'binning',
+        'scaling',
+        'imaging_telescopes',
+        'guiding_telescopes',
+        'mounts',
+        'imaging_cameras',
+        'guiding_cameras',
+        'focal_reducers',
+        'software',
+        'filters',
+        'accessories',
+        'user',
+        'license',
+    )
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    fields = (
+        'website',
+        'job',
+        'hobbies',
+        'timezone',
+        'telescopes',
+        'mounts',
+        'cameras',
+        'focal_reducers',
+        'software',
+        'filters',
+        'accessories',
+        'follows',
+        'default_license',
+        'language'
+    )
+
+
 admin.site.register(Gear)
 admin.site.register(Telescope)
 admin.site.register(Mount)
@@ -13,11 +55,11 @@ admin.site.register(Subject)
 admin.site.register(SubjectIdentifier)
 admin.site.register(DeepSky_Acquisition)
 admin.site.register(SolarSystem_Acquisition)
-admin.site.register(Image)
+admin.site.register(Image, ImageAdmin)
 admin.site.register(ImageRevision)
 admin.site.register(Request)
 admin.site.register(LocationRequest)
 admin.site.register(ImageRequest)
 admin.site.register(ABPOD)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Location)
