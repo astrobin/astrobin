2953dde8dea2d53721b7210eb2db2cb21151867f
Salvatore Iovene <salvatore@iovene.com>
2011-12-06 11:05:23 -0500

    Removed non-utf character.

--- static/js/jquery.form.js
+++ static/js/jquery.form.js
@@ -331,7 +331,7 @@ $.fn.ajaxSubmit = function(options) {
 			return doc;
 		}
 		
-		// Rails CSRF hack (thanks to Yvan BARTHÉLEMY)
+		// Rails CSRF hack (thanks to Yvan BARTH?LEMY)
 		var csrf_token = $('meta[name=csrf-token]').attr('content');
 		var csrf_param = $('meta[name=csrf-param]').attr('content');
 		if (csrf_param && csrf_token) {
