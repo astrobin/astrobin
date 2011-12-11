f3b1299560f20da1d4f5765cbbdbf57ca92f6f93
Salvatore Iovene <salvatore@iovene.com>
2011-12-11 09:29:04 +0000

    Removed astrometry.net.

--- astrometry.net-0.38/util/convolve-image.h
+++
@@ -1,8 +0,0 @@
-
-
-float* convolve_get_gaussian_kernel_f(double sigma, double nsigma, int* k0, int* NK);
-
-float* convolve_1d_f(const float* img, int W, int H,
-					 const float* kernel, int k0, int NK,
-					 float* outimg, float* tempimg);
-
