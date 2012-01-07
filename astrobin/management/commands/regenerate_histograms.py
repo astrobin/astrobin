ef619459a774c55c04bceb8d58d92c5a7692d0da
Salvatore Iovene <salvatore@iovene.com>
2012-01-07 03:41:58 -0500

    Added command to regenerate all histograms.

---
+++ astrobin/management/commands/regenerate_histograms.py
@@ -0,0 +1,50 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+from astrobin.models import Image
+from astrobin.image_utils import generate_histogram
+from astrobin.storage import save_to_bucket
+
+from PIL import Image as PILImage
+import os
+import urllib2
+import tempfile
+import StringIO
+
+class Command(BaseCommand):
+    help = "Regenerates all histograms."
+
+    def handle(self, *args, **options):
+        images = Image.objects.all()
+
+        tempdir = tempfile.mkdtemp()
+        for i in images:
+            url = 'http://astrobin_images.s3.amazonaws.com/%s%s' % (i.filename, i.original_ext)
+            path = os.path.join(tempdir, i.filename + i.original_ext)
+
+            try:
+                u = urllib2.urlopen(url)
+            except:
+                print i.filename + ": HTTP error."
+                continue
+
+            f = open(path, 'w')
+            f.write(u.read())
+            f.close()
+
+            f = open(path, 'r')
+            image = PILImage.open(f)
+            new_hist = generate_histogram(image)
+            new_hist_f = StringIO.StringIO()
+            new_hist.save(new_hist_f, 'PNG')
+            save_to_bucket(i.filename + '_hist.png', new_hist_f.getvalue())
+            
+            f.close()
+            os.remove(path)
+
+            print i.filename + ": done."
+
+        os.removedirs(tempdir)    
+
+        print "All done."
+
