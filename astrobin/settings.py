ea47b3488e8d35b381f50edd9721e8a93f26b3e7
Salvatore Iovene <salvatore@iovene.com>
2012-01-18 13:14:49 -0500

    Fixes #73: Add little number telling how many new notifications there are

--- astrobin/settings.py
+++ astrobin/settings.py
@@ -138,6 +138,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.auth',
     'notification.context_processors.notification',
     'astrobin.context_processors.privatebeta_enabled',
+    'astrobin.context_processors.notices_count',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.static',
