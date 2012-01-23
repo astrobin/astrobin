4a9afbc6629dcd5fecc103ee0e2efe077366160a
Salvatore Iovene <salvatore@iovene.com>
2012-01-23 19:14:07 +0000

    Two small oopses.

--- astrobin/views.py
+++ astrobin/views.py
@@ -199,7 +199,7 @@ def wall(request):
         response_dict['sort'] = '-views'
         sqs = sqs.order_by('-views');
     elif request.GET.get('sort') == '-rating':
-        response_dict['sort'] = '-views'
+        response_dict['sort'] = '-rating'
         sqs = sqs.order_by('-rating')
     elif request.GET.get('sort') == '-integration':
         response_dict['sort'] = '-integration'
