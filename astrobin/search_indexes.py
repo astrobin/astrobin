fc610e21d890f693078e7fa25341689e408c2090
Salvatore Iovene <salvatore@iovene.com>
2012-02-04 10:57:41 -0500

    License should be an integer, in search index.

--- astrobin/search_indexes.py
+++ astrobin/search_indexes.py
@@ -121,7 +121,7 @@ class ImageIndex(SearchIndex):
     is_planets = BooleanField()
     is_comets = BooleanField()
 
-    license = CharField(model_attr = 'license')
+    license = IntegerField(model_attr = 'license')
 
     def index_queryset(self):
         return Image.objects.filter(Q(is_stored = True), Q(is_wip = False))
