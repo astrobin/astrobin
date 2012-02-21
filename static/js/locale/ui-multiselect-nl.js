42b2f8e025faeca6ad91d8d8c2befa0917039cde
Salvatore Iovene <salvatore@iovene.com>
2012-02-21 08:24:56 +0000

    Added nl localization for multiselect.

---
+++ static/js/locale/ui-multiselect-nl.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale nl, nl-NL
+ */
+
+$.extend($.ui.multiselect.locale, {
+    addAll:'Voeg alle',
+    removeAll:'Verwijder alle',
+    itemsCount:'items geselecteerd'
+});
