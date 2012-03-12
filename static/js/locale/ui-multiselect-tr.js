5901c3a99988bed8253502d223b7702050722dd0
Salvatore Iovene <salvatore@iovene.com>
2012-03-12 18:51:52 +0000

    Added Turkish multiselect l10n.

---
+++ static/js/locale/ui-multiselect-tr.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale en, en-US
+ */
+
+$.extend($.ui.multiselect.locale, {
+	addAll:'Tümünü Ekle',
+	removeAll:'Tümünü Çıkar',
+	itemsCount:'öğe seçildi'
+});
