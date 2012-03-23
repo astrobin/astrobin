738de871a6b3e169a908ee6b1ba37fbacfeae503
Salvatore Iovene <salvatore@iovene.com>
2012-03-23 17:19:37 +0000

    Added Albanian language.

---
+++ static/js/locale/ui-multiselect-sq.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale en, en-US
+ */
+
+$.extend($.ui.multiselect.locale, {
+	addAll:'Shtoji te gjitha',
+	removeAll:'Hiqi te gjitha',
+	itemsCount:'artikujt e zgjedhur'
+});
