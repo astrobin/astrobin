865c1cd29eaf766a10fcdb7a541c20acdc9b5f9e
Salvatore Iovene <salvatore@iovene.com>
2012-04-11 05:10:28 -0400

    Added empty value to Country choices.
    Fixed: #139: Editing locations broken because the extra form won't validate

--- astrobin/models.py
+++ astrobin/models.py
@@ -1242,7 +1242,7 @@ class Location(models.Model):
     country = CountryField(
         verbose_name = _("Country"),
         null = True,
-        blank = False,
+        blank = True,
     )
     lat_deg = models.IntegerField(
         null = True,
