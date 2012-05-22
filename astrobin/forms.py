538b5d6b584c6aa07af0559bb3dc5fe2e29a18c0
Salvatore Iovene <salvatore@iovene.com>
2012-05-22 05:22:21 -0400

    Community editing gear items.

--- astrobin/forms.py
+++ astrobin/forms.py
@@ -14,8 +14,17 @@ from search_indexes import xapian_escape
 import string
 import unicodedata
 import operator
+import datetime
 
 from management import NOTICE_TYPES
+
+def uniq(seq):
+   # Not order preserving
+   keys = {}
+   for e in seq:
+       keys[e] = 1
+   return keys.keys()
+
 
 class ImageUploadForm(forms.Form):
     file = forms.ImageField()
@@ -671,4 +680,53 @@ class GearUserInfoForm(forms.ModelForm):
 
     class Meta:
         model = GearUserInfo
+
+
+class ModeratorGearFixForm(forms.ModelForm):
+    error_css_class = 'error'
+
+    class Meta:
+        model = Gear
+        fields = ('make', 'name',)
+        widgets = {
+            'make': forms.TextInput(attrs = {
+                'data-provide': 'typeahead',
+                'data-source': simplejson.dumps(
+                    uniq([x.make for x in Gear.objects.exclude(make = None).exclude(make = '')])),
+                'autocomplete': 'off',
+            }),
+        }
+
+    def clean_make(self):
+        return self.cleaned_data['make'].strip()
+
+    def clean_name(self):
+        return self.cleaned_data['name'].strip()
+
+    def save(self, force_insert = False, force_update = False, commit = True):
+        instance = getattr(self, 'instance', None)
+        old_make = Gear.objects.get(id = instance.id).make
+        old_name = Gear.objects.get(id = instance.id).name
+
+        print "Old make: %s" % old_make
+
+        m = super(ModeratorGearFixForm, self).save(commit = False)
+
+        # Update the time
+        m.moderator_fixed = datetime.datetime.now()
+
+        if commit:
+            m.save()
+
+        print "New make: %s" % m.make
+
+        if old_make and m.make != old_make:
+            # Fix all with the same make
+            same_make = Gear.objects.filter(make = old_make)
+            for i in same_make:
+                print "Updating gear %d from %s to %s..." % (i.id, i.make, m.make)
+                i.make = m.make
+                i.save()
+
+        return m
 
