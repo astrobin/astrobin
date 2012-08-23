eefe4cdd7ff29fce47dbd98ac9cc8b7026e25066
Salvatore Iovene <salvatore@iovene.com>
2012-08-23 03:55:19 -0400

    Bumps the CSS version.

--- astrobin/settings.py
+++ astrobin/settings.py
@@ -313,7 +313,7 @@ PIPELINE_CSS = {
             'css/bootstrap-responsive.css',
             'css/astrobin.css',
         ),
-        'output_filename': 'css/astrobin_pipeline_screen_v17.css',
+        'output_filename': 'css/astrobin_pipeline_screen_v18.css',
         'extra_content':  {
             'media': 'screen, projection',
         },
