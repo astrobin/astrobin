707efd4c1b1dca22ad7c2596f818157f637a94e1
Salvatore Iovene <salvatore@iovene.com>
2012-11-20 11:18:59 -0500

    (rawdata) Verifies file type.

--- rawdata/forms.py
+++ rawdata/forms.py
@@ -1,10 +1,24 @@
+# Python
+import os
+
 # Django
-from django.form import ModelForm
+from django.forms import ModelForm, ValidationError
+from django.utils.translation import ugettext_lazy as _
 
 # This app
 from .models import RawImage
 
 class RawImageUploadForm(ModelForm):
+    def clean_file(self):
+        file = self.cleaned_data['file']
+        if file:
+            name, ext = os.path.splitext(file.name)
+            supported_types = ('fit', 'fits', 'cr2')
+            if ext.lower() not in supported_types:
+                raise ValidationError(_('File type is not supported'))
+
+        return file
+
     class Meta:
         model = RawImage
         fields = ('file',)
