1985cb291d648a6d94f3271bf1d88c064b67a22c
Salvatore Iovene <salvatore@iovene.com>
2012-11-20 10:33:51 -0500

    Adds original_filename fields to rawdata image.

--- rawdata/models.py
+++ rawdata/models.py
@@ -9,6 +9,7 @@ from django.db import models
 
 
 def upload_path(instance, filename):
+    instance.original_filename = filename
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (uuid.uuid4(), ext)
     return os.path.join('rawdata', str(instance.user.id), filename)
@@ -22,6 +23,11 @@ class RawImage(models.Model):
     file = models.FileField(
         storage = FileSystemStorage(location = '/webserver/www'),
         upload_to = upload_path,
+    )
+
+    original_filename = models.CharField(
+        max_length = 256,
+        editable = False,
     )
 
     uploaded = models.DateTimeField(
