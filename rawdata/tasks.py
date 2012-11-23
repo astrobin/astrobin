61ff51c059bfe316f5c82009bd694feaaad3e90a
Salvatore Iovene <salvatore@iovene.com>
2012-11-23 15:40:21 -0500

    (rawdata) celery task indexes files (with dummy process, at the moment).

---
+++ rawdata/tasks.py
@@ -0,0 +1,24 @@
+from celery.task import task
+
+from .models import RawImage
+
+@task()
+def index_raw_image(id):
+    def generate_dummy_data():
+        from random import choice
+        image_type = choice(('U', 'O', 'D', 'F', 'L',))
+
+        return {
+            'image_type': image_type,
+        }
+
+    try:
+        image = RawImage.objects.get(id = id)
+    except RawImage.DoesNotExist:
+        return
+
+    data = generate_dummy_data()
+    image.image_type = data['image_type']
+    image.indexed = True
+    image.save(index = False)
+    
