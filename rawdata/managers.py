dea3571ff983d17b29b52bee97505c6e87153caa
Salvatore Iovene <salvatore@iovene.com>
2012-11-24 03:21:04 -0500

    (rawdata) Allows the admin interface to edit inactive raw images.

--- rawdata/managers.py
+++ rawdata/managers.py
@@ -6,3 +6,13 @@ class SoftDeleteManager(models.Manager):
 
     def all_with_inactive(self):
         return super(SoftDeleteManager, self).get_query_set()
+
+    def get(self, *args, **kwargs):
+        ''' if a specific record was requested, return it even if it's inactive '''
+        return self.all_with_inactive().get(*args, **kwargs)
+ 
+    def filter(self, *args, **kwargs):
+        ''' if pk was specified as a kwarg, return even if it's inactive '''
+        if 'pk' in kwargs:
+            return self.all_with_inactive().filter(*args, **kwargs)
+        return self.get_query_set().filter(*args, **kwargs)
