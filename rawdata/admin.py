9465743072bf083eddacb064ea5fee18262e6e4c
Salvatore Iovene <salvatore@iovene.com>
2012-11-24 03:19:22 -0500

    (rawdata) Adds admin interface to RawImage.

---
+++ rawdata/admin.py
@@ -0,0 +1,33 @@
+# Django
+from django.contrib import admin
+
+# This app
+from .models import *
+
+
+class RawImageAdmin(admin.ModelAdmin):
+    list_display = (
+        'original_filename',
+        'user',
+        'size',
+        'uploaded',
+        'indexed',
+        'active',
+    )
+
+    list_filter = (
+        'indexed',
+        'active',
+    )
+
+    def queryset(self, request):
+        # Default: qs = self.model._default_manager.get_query_set()
+        qs = self.model._default_manager.all_with_inactive()
+        # TODO: this should be handled by some parameter to the ChangeList.
+        ordering = self.ordering or () # otherwise we might try to *None, which is bad ;)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
+
+
+admin.site.register(RawImage, RawImageAdmin)
