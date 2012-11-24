90dacd1119a84bc170d9a4c0e23f9fa7fd07979a
Salvatore Iovene <salvatore@iovene.com>
2012-11-24 03:19:22 -0500

    (rawdata) Adds admin interface to RawImage.

--- rawdata/managers.py
+++ rawdata/managers.py
@@ -3,3 +3,6 @@ from django.db import models
 class SoftDeleteManager(models.Manager):
     def get_query_set(self):
         return super(SoftDeleteManager, self).get_query_set().filter(active = True)
+
+    def all_with_inactive(self):
+        return super(SoftDeleteManager, self).get_query_set()
