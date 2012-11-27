100cbd9cf90f60cd774a9fc617453ef8411ae2ba
Salvatore Iovene <salvatore@iovene.com>
2012-11-27 07:53:54 -0500

    (rawdata) Allows selection and deletion of virtual folders.

--- rawdata/urls.py
+++ rawdata/urls.py
@@ -10,7 +10,7 @@ signal_handlers.install()
 urlpatterns = patterns('',
     url(r'^upload/$', login_required(RawImageCreateView.as_view()), name = 'rawdata.upload'),
     url(r'^download/(?P<ids>[\d+,?]+)/$', RawImageDownloadView.as_view(), name = 'rawdata.download'),
-    url(r'^delete/(?P<ids>[\d+,?]+)/$', login_required(RawImageDeleteView.as_view()), name = 'rawdata.delete'),
+    url(r'^delete/(?P<ids>[\d+,?]+)?/$', login_required(RawImageDeleteView.as_view()), name = 'rawdata.delete'),
     url(r'^archive/(?P<pk>\d+)/$', login_required(TemporaryArchiveDetailView.as_view()), name = 'rawdata.temporary_archive_detail'),
     url(r'', login_required(RawImageLibrary.as_view()), name = 'rawdata.library'),
 )
