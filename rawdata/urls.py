221eb438cef923172376473e76185d873d072153
Salvatore Iovene <salvatore@iovene.com>
2012-11-27 06:27:42 -0500

    (rawdata) Removes username from url, because users can only see their own anyway.

--- rawdata/urls.py
+++ rawdata/urls.py
@@ -12,5 +12,5 @@ urlpatterns = patterns('',
     url(r'^download/(?P<ids>[\d+,?]+)/$', RawImageDownloadView.as_view(), name = 'rawdata.download'),
     url(r'^delete/(?P<ids>[\d+,?]+)/$', login_required(RawImageDeleteView.as_view()), name = 'rawdata.delete'),
     url(r'^archive/(?P<pk>\d+)/$', login_required(TemporaryArchiveDetailView.as_view()), name = 'rawdata.temporary_archive_detail'),
-    url(r'^(?P<username>[\w.@+-]+)/$', login_required(RawImageLibrary.as_view()), name = 'rawdata.library'),
+    url(r'', login_required(RawImageLibrary.as_view()), name = 'rawdata.library'),
 )
