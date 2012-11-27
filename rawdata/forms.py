8743df2f8e2131474b66aa6c12aef48d74c24b06
Salvatore Iovene <salvatore@iovene.com>
2012-11-27 13:00:23 -0500

    (rawdata) Removes mentions of CR2, reorgs plans.

--- rawdata/forms.py
+++ rawdata/forms.py
@@ -7,14 +7,14 @@ from django.utils.translation import ugettext_lazy as _
 
 # This app
 from .models import RawImage
+from .utils import supported_raw_formats
 
 class RawImageUploadForm(ModelForm):
     def clean_file(self):
         file = self.cleaned_data['file']
         if file:
             name, ext = os.path.splitext(file.name)
-            supported_types = ('.fit', '.fits', '.cr2')
-            if ext.lower() not in supported_types:
+            if ext.lower() not in supported_raw_formats():
                 raise ValidationError(_('File type is not supported'))
 
         return file
