9bc8a143d57f3c4fc6d250a39b1ec61437a47c46
Salvatore Iovene <salvatore@iovene.com>
2012-11-30 13:29:45 -0500

    (rawdata) Protects pages that need protection.

--- rawdata/mixins.py
+++ rawdata/mixins.py
@@ -2,7 +2,12 @@
 import simplejson as json
 
 # Django
+from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+
+# This app
+from .utils import user_has_active_subscription
 
 
 class AjaxableResponseMixin(object):
@@ -30,3 +35,8 @@ class AjaxableResponseMixin(object):
         else:
             return super(AjaxableResponseMixin, self).form_valid(form)
 
+
+class RestrictToSubscriberMixin(object):
+    @method_decorator(user_passes_test(lambda u: user_has_active_subscription(u)))
+    def dispatch(self, *args, **kwargs):
+        return super(RestrictToSubscriberMixin, self).dispatch(*args, **kwargs)
