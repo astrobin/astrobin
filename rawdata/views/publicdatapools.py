2f6aea39a591dd38550594f43e513921520005c4
Salvatore Iovene <salvatore@iovene.com>
2012-12-29 14:10:16 -0500

    Adds missing imports.

--- rawdata/views/publicdatapools.py
+++ rawdata/views/publicdatapools.py
@@ -1,7 +1,7 @@
 # Django
 from django.contrib import messages
 from django.contrib.contenttypes.models import ContentType
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext as _
 from django.views.generic import (
