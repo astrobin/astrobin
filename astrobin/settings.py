f7a75be1bf43ef43d3cfbea64580da9d1a6407cd
Salvatore Iovene <salvatore@iovene.com>
2013-01-06 14:07:04 -0500

    (api) Makes sure token auth works.

--- astrobin/settings.py
+++ astrobin/settings.py
@@ -419,6 +419,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     'PAGINATE_BY': 10,
     'FILTER_BACKEND': 'rest_framework.filters.DjangoFilterBackend',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
 
 # http://docs.celeryproject.org/en/latest/django/unit-testing.html
