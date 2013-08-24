23d8f1789dcb2c560558a5aded0c8d5b2b65e93d
Salvatore Iovene <salvatore@iovene.com>
2013-08-24 19:57:20 +0200

    IOTD in 7-day buffer, and 2 runner ups.

--- astrobin/management/commands/image_of_the_day.py
+++ astrobin/management/commands/image_of_the_day.py
@@ -4,10 +4,48 @@ from django.conf import settings
 from django.db.models import Q
 
 from astrobin.models import Image, Favorite, ImageOfTheDay
-from astrobin.image_utils import make_image_of_the_day
+from astrobin.image_utils import make_image_of_the_day, make_runnerup
 from nested_comments.models import NestedComment
 
 from datetime import date, datetime, timedelta
+
+
+def calculate_score(image):
+    author = image.user
+    votes = 0
+    for vote in image.votes.all():
+        votes += vote.score
+
+    times_favorited = Favorite.objects.filter(image = image).exclude(user = author).count()
+    comments = NestedComment.objects.filter(
+        deleted = False,
+        content_type__app_label = 'astrobin',
+        content_type__model = 'image',
+        object_id = image.id).exclude(author = author).count()
+
+    score = votes + (times_favorited * 3) + (comments * 5)
+    try:
+        iotd = ImageOfTheDay.objects.get(image = image)
+    except ImageOfTheDay.DoesNotExist:
+        iotd = None
+
+    if iotd:
+        score = -1
+
+    return score
+
+
+def compare_images(a, b):
+    score_a = calculate_score(a)
+    score_b = calculate_score(b)
+
+    if score_a > score_b:
+        return -1
+    if score_a < score_b:
+        return 1
+
+    return 0
+
 
 class Command(BaseCommand):
     help = "Calculates and saves the image of the day."
@@ -15,13 +53,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         coolest_image = None
         yesterday = date.today() - timedelta(1)
-        day_before_yesterday = yesterday - timedelta(1)
+        start = yesterday - timedelta(7)
 
-        while coolest_image is None:
-            print day_before_yesterday
-
-            candidate_images = Image.objects.filter(Q(uploaded__gte = day_before_yesterday) &
-                                                    Q(uploaded__lt = yesterday) &
+        candidate_images = []
+        while not candidate_images:
+            candidate_images = Image.objects.filter(Q(uploaded__gte = start) &
+                                                    Q(uploaded__lte = yesterday) &
                                                     Q(subject_type__lt = 500) &
                                                     Q(allow_rating = True) &
                                                     Q(user__userprofile__optout_rating = False) &
@@ -30,38 +67,23 @@ class Command(BaseCommand):
                                                     Q(is_stored = True) &
                                                     Q(is_wip = False) &
                                                     Q(original_ext__in = ['.jpg', '.jpeg', '.JPG', '.JPEG', '.png', '.PNG']))
+            start = start - timedelta(1)
 
-            if candidate_images:
-                coolest_image = candidate_images[0]
-                current_coolness = 0
-                for image in candidate_images:
-                    author = image.user
-                    score = 0
-                    for vote in image.votes.all():
-                        score += vote.score
+        sorted_images = sorted(candidate_images, cmp = compare_images)
 
-                    times_favorited = Favorite.objects.filter(image = image).count()
-                    comments = NestedComment.objects.filter(
-                        deleted = False,
-                        content_type__app_label = 'astrobin',
-                        content_type__model = 'image',
-                        object_id = image.id).exclude(author = author).count()
+        print "IOTD: %d - %s (%d)" % (
+            sorted_images[0].id, sorted_images[0].title.encode('utf-8'),
+            calculate_score(sorted_images[0]))
 
-                    coolness = score + (times_favorited * 3) + (comments * 5)
-                    try:
-                        iotd = ImageOfTheDay.objects.get(image = image)
-                    except ImageOfTheDay.DoesNotExist:
-                        iotd = None
-                    if iotd:
-                        coolness = -1
+        print "Runner up 1: %d - %s (%d)" % (
+            sorted_images[1].id, sorted_images[1].title.encode('utf-8'),
+            calculate_score(sorted_images[1]))
 
-                    print "Examining: [%s] [%d/%d]" % (image.title.encode('utf-8'), coolness, current_coolness)
-                    if coolness > current_coolness:
-                        coolest_image = image
-                        current_coolness = coolness
-                        print "\tCoolest image: [%s] [%d]" % (image.title.encode('utf-8'), coolness)
-            else:
-                day_before_yesterday = day_before_yesterday - timedelta(1)
+        print "Runner up 2: %d - %s (%d)" % (
+            sorted_images[2].id, sorted_images[2].title.encode('utf-8'),
+            calculate_score(sorted_images[2]))
 
-        make_image_of_the_day(coolest_image)
+        iotd = make_image_of_the_day(sorted_images[0])
+        make_runnerup(sorted_images[1], iotd, 1)
+        make_runnerup(sorted_images[2], iotd, 2)
 
