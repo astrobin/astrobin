d2a59c79f2e18e8e500821214707822e92a1ab7b
Salvatore Iovene <salvatore@iovene.com>
2013-10-15 06:37:45 +0000

    Submitting astrometry jobs and getting results.

--- astrobin_apps_platesolving/solver.py
+++ astrobin_apps_platesolving/solver.py
@@ -4,6 +4,10 @@ from django.conf import settings
 
 
 class Solver(object):
+    FAIL    = 0
+    SOLVING = 1
+    SUCCESS = 2
+
     def _backend(self):
         module_name, backend_name = \
             settings.ASTROBIN_PLATESOLVING_BACKEND.rsplit('.', 1)
@@ -15,5 +19,25 @@ class Solver(object):
 
     # Starts the job and returns an id for later reference
     def solve(self, image):
-        backend = self._backend()
-        return backend.start(image)
+        return self._backend().start(image)
+
+    def status(self, submission):
+        sub_status = self._backend().sub_status(submission)
+        status = sub_status.get('status')
+        if status == 'fail':
+            return self.FAIL
+
+        jobs = sub_status.get('jobs', [])
+        if not jobs:
+            return self.SOLVING
+
+        job = jobs[0]
+        job_result = self._backend().job_status(job)
+        status = job_result.get('status')
+        if status == 'solving':
+            return self.SOLVING
+        elif status == 'success':
+            return self.SUCCESS
+
+        return self.FAIL
+
