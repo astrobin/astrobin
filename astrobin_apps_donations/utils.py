959482caee4a5d2bce41315912491ff4e48904e8
Salvatore Iovene <salvatore@iovene.com>
2013-10-31 08:18:10 +0000

    Add way for user to cancel recurring donation.

--- astrobin_apps_donations/utils.py
+++
@@ -1,7 +0,0 @@
-from subscription.models import UserSubscription
-
-def user_is_donor(user):
-    if user.is_authenticated():
-        return UserSubscription.objects.filter(user = user, subscription__name = 'AstroBin Donor').count() > 0
-    return False
-
