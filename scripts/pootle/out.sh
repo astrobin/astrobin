e88773c1be6ffa3e96d33374d80c9a9c4cc99fc7
Salvatore Iovene <salvatore@iovene.com>
2013-11-10 21:29:45 +0200

    Update pootle scripts.

--- scripts/pootle/out.sh
+++ scripts/pootle/out.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-LANGS="ca de el es fi fr it nl pl pt pt-BR ro ru sk sq sr tr zh-CN"
-FROM='po/astrobin/'
-TO='../astrobin/astrobin/locale/'
+LANGS="ca cs de el es fa fi fr it hu nl pl pt pt-BR ro ru sk sq sr tr zh-CN"
+PROJECTS="astrobin nested_comments rawdata astrobin_apps_users astrobin_apps_images astrobin_apps_platesolving"
+FROM='po'
+TO='../astrobin'
 
 echo "Syncing Pootle files to disk..."
 ./manage.py sync_stores
 
-for lang in $LANGS
+for proj in $PROJECTS
 do
-  echo "Copying $lang..."
-  cp $FROM/$lang/django.po $TO/$lang/LC_MESSAGES/
+    echo "* $proj"
+    for lang in $LANGS
+    do
+        echo "  Copying $lang..."
+        cp $FROM/$proj/$lang/django.po $TO/$proj/locale/$lang/LC_MESSAGES/
+    done
 done
 
 echo "Done."
+
