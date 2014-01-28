388d5c37f1b42aea6abe23bf5467dd749a778424
Salvatore Iovene <salvatore@iovene.com>
2014-01-28 11:45:52 +0000

    Update languages.

--- scripts/makemessages.sh
+++ scripts/makemessages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apps="astrobin rawdata nested_comments astrobin_apps_users astrobin_apps_images astrobin_apps_platesolving astrobin_apps_donations"
-langs="ar ca cs de el en es fa fi fr hu it nl pl pt-BR pt ro ru sk sq sr tr zh-CN"
+langs="ar ca cs de el en es fa fi fr hu it nl pl pt-BR pt ro ru sk sq sr tr zh-CN zh-TW"
 
 for app in $apps; do
     echo "Processing app: $app"
