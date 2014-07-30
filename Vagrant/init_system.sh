bb641c1311b89cd475b405f2057050214a0596ed
Salvatore Iovene <salvatore.iovene@intel.com>
2014-07-30 10:00:02 +0300

    (vagrant) Add user vagrant to group astrobin.

--- Vagrant/init_system.sh
+++ Vagrant/init_system.sh
@@ -17,6 +17,10 @@ if ! id -u solr >/dev/null 2>&1; then
     useradd -MN -s /dev/null -g astrobin -u 2001 solr
 fi
 
+if id -u vagrant >/dev/null 2>&1; then
+    usermod -G astrobin vagrant
+fi
+
 chown -R astrobin:astrobin /venv
 chown -R astrobin:astrobin /webserver
 chown -R astrobin:astrobin /rawdata
