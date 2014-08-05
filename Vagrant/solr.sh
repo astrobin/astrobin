21b80bf71ceac792f25923b9493e6f9d1248732c
Salvatore Iovene <salvatore@iovene.com>
2014-08-05 09:49:59 +0300

    (vagrant) Fix solr schema *after* its generation.

--- Vagrant/solr.sh
+++ Vagrant/solr.sh
@@ -5,9 +5,6 @@ sudo -u solr /bin/bash - <<"EOF"
 curl https://archive.apache.org/dist/lucene/solr/4.4.0/solr-4.4.0.tgz > /opt/solr/solr.tgz
 tar xvfz /opt/solr/solr.tgz -C /opt/solr
 chmod g+w /opt/solr/ -R
-# TODO: see https://bitbucket.org/siovene/astrobin/issue/257/migrate-to-haystack-2x
-sed -i '/EnglishPorterFilterFactory/d' /opt/solr/solr-4.4.0/example/solr/collection1/conf/schema.xml
-sed -i '/<\/fields>/i<field name="_version_" type="slong" indexed="true" stored="true" multiValued="false"\/>' /opt/solr/solr-4.4.0/example/solr/collection1/conf/schema.xml
 EOF
 fi
 
@@ -16,4 +13,10 @@ sudo -u astrobin /bin/bash - <<"EOF"
 . /var/www/astrobin/env/dev
 
 /var/www/astrobin/manage.py build_solr_schema > /opt/solr/solr-4.4.0/example/solr/collection1/conf/schema.xml
+
+# TODO: see https://bitbucket.org/siovene/astrobin/issue/257/migrate-to-haystack-2x
+sed -i '/EnglishPorterFilterFactory/d' /opt/solr/solr-4.4.0/example/solr/collection1/conf/schema.xml
+sed -i '/<\/fields>/i<field name="_version_" type="slong" indexed="true" stored="true" multiValued="false"\/>' /opt/solr/solr-4.4.0/example/solr/collection1/conf/schema.xml
 EOF
+
+
