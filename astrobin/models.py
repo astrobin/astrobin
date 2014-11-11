astrobin/astrobin

commit c9dcface88da92eb247961c862f8aa426f11b12c

Fix #247, fix #289.

astrobin/models.py
@@ -1776,10 +1776,10 @@ class Location(models.Model):
     )
 
     def __unicode__(self):
-        if self.state:
-            return '%s, %s (%s), %s' % (self.name, self.city, self.state, get_country_name(self.country))
-        else:
-            return '%s, %s, %s' % (self.name, self.city, get_country_name(self.country))
+        return ', '.join(filter(None, [
+            self.name, self.city, self.state,
+            unicode(get_country_name(self.country))
+        ]))
 
     class Meta:
         app_label = 'astrobin'

astrobin/views.py
@@ -706,7 +706,6 @@ def image_detail(request, id, r):
         deep_sky_data = (
             (_('Resolution'), '%dx%d' % (image.w, image.h) if (image.w and image.h) else None),
             (_('Dates'), sorted(dsa_data['dates'])),
-            (_('Locations'), u', '.join([x.name for x in image.locations.all()])),
             (_('Frames'),
                 ('\n' if len(frames_list) > 1 else '') +
                 u'\n'.join("%s %s" % (
@@ -775,6 +774,7 @@ def image_detail(request, id, r):
         (6, 'cc/cc-by-nd.png',    LICENSE_CHOICES[6][1]),
     )
 
+    locations = u'; '.join(['%s' % (x) for x in image.locations.all()])
 
 
     ######################
@@ -862,6 +862,7 @@ def image_detail(request, id, r):
         'subject_type': [x[1] for x in Image.SUBJECT_TYPE_CHOICES if x[0] == image.subject_type][0] if image.subject_type else 0,
         'license_icon': licenses[image.license][1],
         'license_title': licenses[image.license][2],
+	'locations': locations,
         # Because of a regression introduced at
         # revision e1dad12babe5, now we have to
         # implement this ugly hack.

astrobin/templates/image/detail/image_card.html
@@ -83,6 +83,8 @@
                         <p><strong>{% trans "Orientation" %}</strong>: {{instance_to_platesolve.solution.orientation}} {% trans "degrees" %}</p>
                         <p><strong>{% trans "Field radius" %}</strong>: {{instance_to_platesolve.solution.radius}} {% trans "degrees" %}</p>
                     {% endif %}
+
+                    <p><strong>{% trans "Locations" %}</strong>: {{locations}}</p>
                 </div> <!-- acquisition-inner -->
             </div> <!-- acquisition -->
         </div> <!-- image-detail-inner -->

commit cc94d2aa3625fc3ee74884edf12c3d42b2a70173

Fix regression with unicode location data introduced in c9dcface88da92eb247961c862f8aa426f11b12c

astrobin/models.py
@@ -1776,7 +1776,7 @@ class Location(models.Model):
     )
 
     def __unicode__(self):
-        return ', '.join(filter(None, [
+        return u', '.join(filter(None, [
             self.name, self.city, self.state,
             unicode(get_country_name(self.country))
         ]))

astrobin/views.py
@@ -774,7 +774,7 @@ def image_detail(request, id, r):
         (6, 'cc/cc-by-nd.png',    LICENSE_CHOICES[6][1]),
     )
 
-    locations = u'; '.join(['%s' % (x) for x in image.locations.all()])
+    locations = '; '.join([u'%s' % (x) for x in image.locations.all()])
 
 
     ######################