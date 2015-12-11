c17c78ba7dd1b90a0f8b51e8edb40b974ce0b801
Salvatore Iovene <salvatore@iovene.com>
2015-12-11 21:28:37 +0200

    Add management command to reset Lite counters

---
+++ astrobin/management/commands/reset_lite_counters.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+
+from paypal.standard.ipn import models
+from subscription.models import UserSubscription
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for us in UserSubscription.objects.filter(subscription__name = 'AstroBin Lite'):
+            profile = us.user.userprofile
+            print "Processing user: %s" % profile
+            profile.premium_counter = 0
+            profile.save()
