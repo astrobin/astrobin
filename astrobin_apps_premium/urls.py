aefb19348923acaa27420c8890245bfaafb2f909
Salvatore Iovene <salvatore@iovene.com>
2015-12-31 20:18:19 +0200

    Migrate donations views require login

--- astrobin_apps_premium/urls.py
+++ astrobin_apps_premium/urls.py
@@ -9,10 +9,10 @@ from astrobin_apps_premium.views import *
 urlpatterns = patterns('',
     url(
         r'^migrate-donations/$',
-        MigrateDonationsView.as_view(),
+        login_required(MigrateDonationsView.as_view()),
         name = 'astrobin_apps_premium.migrate_donations'),
     url(
         r'^migrate-donations/success/$',
-        TemplateView.as_view(template_name="astrobin_apps_premium/migrate_donations_success.html"),
+        login_required(TemplateView.as_view(template_name="astrobin_apps_premium/migrate_donations_success.html")),
         name = 'astrobin_apps_premium.migrate_donations_success'),
 )
