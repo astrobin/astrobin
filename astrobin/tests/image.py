96edd107e2fcf7bd1d041bcd76e87cbe3aedc959
Salvatore Iovene <salvatore@iovene.com>
2016-01-02 16:42:22 +0200

    Add tests for image gear

--- astrobin/tests/image.py
+++ astrobin/tests/image.py
@@ -4,7 +4,16 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 # AstroBin
-from astrobin.models import Image
+from astrobin.models import (
+    Image,
+    Telescope,
+    Mount,
+    Camera,
+    FocalReducer,
+    Software,
+    Filter,
+    Accessory)
+
 
 class ImageTest(TestCase):
     def setUp(self):
@@ -110,3 +119,65 @@ class ImageTest(TestCase):
         self.assertEqual(image.locations.count(), 0)
         self.assertEqual(image.description, "Image description")
         self.assertEqual(image.allow_comments, True)
+
+        # Test gear
+        imaging_telescopes = [
+            Telescope.objects.create(
+                make = "Test make", name = "Test imaging telescope")]
+        guiding_telescopes = [
+            Telescope.objects.create(
+                make = "Test make", name = "Test guiding telescope")]
+        mounts = [
+            Mount.objects.create(
+                make = "Test make", name = "Test mount")]
+        imaging_cameras = [
+            Camera.objects.create(
+                make = "Test make", name = "Test imaging camera")]
+        guiding_cameras = [
+            Camera.objects.create(
+                make = "Test make", name = "Test guiding camera")]
+        focal_reducers = [
+            FocalReducer.objects.create(
+                make = "Test make", name = "Test focal reducer")]
+        software = [
+            Software.objects.create(
+                make = "Test make", name = "Test software")]
+        filters = [
+            Filter.objects.create(
+                make = "Test make", name = "Test filter")]
+        accessories = [
+            Accessory.objects.create(
+                make = "Test make", name = "Test accessory")]
+
+        profile = self.user.userprofile
+        profile.telescopes = imaging_telescopes + guiding_telescopes
+        profile.mounts = mounts
+        profile.cameras = imaging_cameras + guiding_cameras
+        profile.focal_reducers = focal_reducers
+        profile.software = software
+        profile.filters = filters
+        profile.accessories = accessories
+
+        response = self.client.post(
+            reverse('image_edit_save_gear'),
+            {
+                'image_id': 1,
+                'submit_acquisition': True,
+                'imaging_telescopes': ','.join(["%d" % x.pk for x in imaging_telescopes]),
+                'guiding_telescopes': ','.join(["%d" % x.pk for x in guiding_telescopes]),
+                'mounts': ','.join(["%d" % x.pk for x in mounts]),
+                'imaging_cameras': ','.join(["%d" % x.pk for x in imaging_cameras]),
+                'guiding_cameras': ','.join(["%d" % x.pk for x in guiding_cameras]),
+                'focal_reducers': ','.join(["%d" % x.pk for x in focal_reducers]),
+                'software': ','.join(["%d" % x.pk for x in software]),
+                'filters': ','.join(["%d" % x.pk for x in filters]),
+                'accessories': ','.join(["%d" % x.pk for x in accessories])
+            },
+            follow = True)
+        image = Image.objects.get(pk = 1)
+        self.assertRedirects(
+            response,
+            reverse('image_edit_acquisition', kwargs = {'id': 1}),
+            status_code = 302,
+            target_status_code = 200)
+
