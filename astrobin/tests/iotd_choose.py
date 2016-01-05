f32f873e1586f3ad36c71d09025ff43532784b6f
Salvatore Iovene <salvatore@iovene.com>
2016-01-05 12:21:03 +0200

    Add assert for non IOTD_Staff accessing iotd_choose

--- astrobin/tests/iotd_choose.py
+++ astrobin/tests/iotd_choose.py
@@ -10,10 +10,12 @@ class IOTDChooseTest(TestCase):
 
     def test_page_accessible_by_iotd_staff(self):
         user = User.objects.create_user('test', 'test@test.com', 'password')
+        self.client.login(username = 'test', password = 'password')
+        response = self.client.get(reverse('iotd_choose'))
+        self.assertEqual(response.status_code, 403)
+
+
         group = Group.objects.create(name = 'IOTD_Staff')
         user.groups.add(group)
-
-        self.client.login(username = 'test', password = 'password')
-
         response = self.client.get(reverse('iotd_choose'))
         self.assertEqual(response.status_code, 200)
