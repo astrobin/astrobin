47af283278082e6149856fdc5d3df06eedcb8c75
Salvatore Iovene <salvatore@iovene.com>
2016-01-05 08:49:20 +0200

    Make the test script nicer

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-coverage run ./manage.py test astrobin rawdata
-coverage report --omit="/venv/*" &&\
-coverage html --omit="/venv/*" -d cover
+
+echo "Running tests..."
+coverage run ./manage.py test astrobin rawdata --noinput --failfast
+if [ ${PIPESTATUS[0]} -eq 0 ]
+then
+    echo -n "Generating HTML report... "
+    coverage html --omit="/venv/*" -d cover >/dev/null 2>&1 && echo "OK"
+
+    echo -n "Total coverage: "
+    coverage report --omit="/venv/*" |& grep "TOTAL" | awk '{print $4}'
+
+    echo
+    echo "--------------------------------------------"
+    echo "See full coverage report at cover/index.html"
+    echo "All done. Thanks for testing!"
+    echo "--------------------------------------------"
+fi
