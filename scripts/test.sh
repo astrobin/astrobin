7c5d102ee4dcb7fe71bed21bf25b8aa99ef169a1
Salvatore Iovene <salvatore@iovene.com>
2016-01-06 16:14:42 +0200

    Add assert count to test report

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo "Running tests..."
 coverage run ./manage.py test \
     astrobin astrobin_apps_premium rawdata \
     --noinput --failfast
 if [ ${PIPESTATUS[0]} -eq 0 ]
 then
+    echo -n "Counting asserts... "
+    echo "~$(git grep "self.*assert" | wc -l)"
+
     echo -n "Generating HTML report... "
     coverage html --omit="/venv/*" -d cover >/dev/null 2>&1 && echo "OK"
 
