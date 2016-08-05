791ffc41a8720447879183b02d37a9e47a74527c
Salvatore Iovene <salvatore@iovene.com>
2016-08-05 20:14:46 +0000

    Improve the delete_spammers command

--- astrobin/management/commands/delete_spammers.py
+++ astrobin/management/commands/delete_spammers.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
 
 from astrobin.models import Image
 
@@ -6,7 +7,15 @@ class Command(BaseCommand):
     help = "Delete accounts of people with images in the spam list."
 
     def handle(self, *args, **options):
-        spam_images = Image.all_objects.filter(moderator_decision = 2)
-        for image in spam_images:
-            print "Deleting user %s" % image.user
-            image.user.delete()
+	spammers = []
+        spam = Image.all_objects.filter(moderator_decision = 2)
+        for image in spam:
+            if image.user not in spammers:
+                spammers.append(image.user)
+
+	for spammer in spammers:
+            print "Deleting spammer %s" % spammer
+            try:
+                spammer.delete()
+            except User.DoesNotExist:
+                pass
