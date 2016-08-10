29930f122bae20373b6c014b9452ca4d3cee26bf
Salvatore Iovene <salvatore@iovene.com>
2016-08-10 22:12:26 +0300

    All capty instances are handled in loadAstroBinImages

--- astrobin_apps_images/static/astrobin_apps_images/js/astrobin_apps_images.js
+++ astrobin_apps_images/static/astrobin_apps_images/js/astrobin_apps_images.js
@@ -1,36 +1,51 @@
 $(document).ready(function() {
     /* TODO: make this a jQuery plugin */
     window.loadAstroBinImages = function(fragment) {
-        $(fragment).find('img.astrobin-image[data-loaded=false]').each(function(index) {
+        $(fragment).find('img.astrobin-image').each(function(index) {
             var $img = $(this);
             var random_timeout = Math.floor(Math.random() * 100) + 100; // 100-200 ms
 
             setTimeout(function() {
-                var id = $img.attr('data-id');
-                var alias = $img.attr('data-alias');
-                var revision = $img.attr('data-revision');
-                var url = $img.attr('data-get-thumb-url');
+                var id = $img.data('id');
+                    alias = $img.data('alias'),
+                    revision = $img.data('revision'),
+                    url = $img.data('get-thumb-url'),
+                    loaded = $img.data('loaded'),
+                    capty = $img.hasClass('capty');
 
-                $.ajax({
-                    dataType: 'json',
-                    timeout: 0,
-                    cache: true,
-                    url: url,
-                    success: function(data, status, request) {
-                        var $img = $('img.astrobin-image[data-id=' + data.id + '][data-alias=' + alias + '][data-revision=' + revision +']');
+                function captify($img) {
+                    var height = $img.attr('height');
+                    $img.capty({animation: 'slide', speed: 200, height: height});
+                    $img.closest('.capty-wrapper').find('.capty-target').show();
+                }
 
-                        if ($img.hasClass('capty')) {
-                            $img.load(function() {
-                                $img.capty({animation: 'slide', speed: 200, height: $img.height()});
-                                $img.closest('.capty-wrapper').find('.capty-target').show();
-                            });
+                if (loaded && capty) {
+                    captify($img);
+                } else if (!loaded) {
+                    $.ajax({
+                        dataType: 'json',
+                        timeout: 0,
+                        cache: true,
+                        url: url,
+                        success: function(data, status, request) {
+                            var $img =
+                                $('img.astrobin-image[data-id=' + data.id +
+                                '][data-alias=' + alias +
+                                '][data-revision=' + revision +
+                                ']');
+
+                            if (capty) {
+                                $img.load(function() {
+                                    captify($img);
+                                });
+                            }
+
+                            $img
+                                .attr('src', data.url)
+                                .attr('data-loaded', 'true');
                         }
-
-                        $img
-                            .attr('src', data.url)
-                            .attr('data-loaded', 'true');
-                    }
-                });
+                    });
+                }
             }, random_timeout);
         });
     };
