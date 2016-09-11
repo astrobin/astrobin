48aea7ef45e3593915c2ea24b0f9474fd5401c2a
Salvatore Iovene <salvatore@iovene.com>
2016-09-11 11:56:18 +0300

    Groups: handle forum access via pybb permissions

--- astrobin/permissions.py
+++ astrobin/permissions.py
@@ -1,6 +1,56 @@
+from django.db.models import Q
 from pybb.permissions import DefaultPermissionHandler
 
-class PyBBMPermissions(DefaultPermissionHandler):
+class CustomForumPermissions(DefaultPermissionHandler):
     # Disable forum polls
     def may_create_poll(self, user):
         return False
+
+
+    def may_view_forum(self, user, forum):
+        may = super(CustomForumPermissions, self).may_view_forum(user, forum)
+
+        if forum.group is not None:
+            if user.is_authenticated():
+                return may and (
+                    forum.group.public or \
+                    user == forum.group.owner or \
+                    user in forum.group.members.all())
+            else:
+                return may and forum.group.public
+
+        return may
+
+
+    def filter_forums(self, user, qs):
+        f = super(CustomForumPermissions, self).filter_forums(user, qs)
+
+        if user.is_authenticated():
+            f = f.filter(
+                Q(group = None) |
+                Q(group__public = True) |
+                Q(group__owner = user) |
+                Q(group__members = user))
+        else:
+            f = f.filter(public = True)
+
+        return f
+
+
+    def may_view_topic(self, user, topic):
+        may = super(CustomForumPermissions, self).may_view_topic(user, topic)
+        return may and self.may_view_forum(user, topic.forum)
+
+    def filter_topics(self, user, qs):
+        f = super(CustomForumPermissions, self).filter_topics(user, qs)
+
+        if user.is_authenticated():
+            f = f.filter(
+                Q(forum__group = None) |
+                Q(forum__group__public = True) |
+                Q(forum__group__owner = user) |
+                Q(forum__group__members = user))
+        else:
+            f = f.filter(forum__group__public = True)
+
+        return f
