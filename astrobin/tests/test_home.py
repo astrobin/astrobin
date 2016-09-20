1f3bc4c19117ef4c675964c78ccaf0f6beb7ef77
Salvatore Iovene <salvatore@iovene.com>
2016-09-20 17:09:29 +0300

    Fix broken unit tests

--- astrobin/tests/test_home.py
+++ astrobin/tests/test_home.py
@@ -18,26 +18,26 @@ class HomeTest(TestCase):
         self.client.login(username = 'test', password = 'password')
 
         response = self.client.get(reverse('index'))
-        self.assertContains(response, '<h4><i class="icon-group"></i>Your activity stream</h4>', html = True)
+        self.assertContains(response, '<h4><i class="icon-group"></i><span class="hidden-phone">Your activity stream</span></h4>', html = True)
 
         response = self.client.post(url('global'), follow = True)
-        self.assertContains(response, '<h4><i class="icon-globe"></i>Global activity stream</h4>', html = True)
+        self.assertContains(response, '<h4><i class="icon-globe"></i><span class="hidden-phone">Global activity stream</span></h4>', html = True)
 
         response = self.client.post(url('images'), follow = True)
-        self.assertContains(response, '<h4><i class="icon-time"></i>Recently uploaded</h4>', html = True)
+        self.assertContains(response, '<h4><i class="icon-time"></i><span class="hidden-phone">Recently uploaded</span></h4>', html = True)
 
         response = self.client.post(url('followed'), follow = True)
-        self.assertContains(response, '<h4><i class="icon-eye-open"></i>Recent images from people you follow</h4>', html = True)
+        self.assertContains(response, '<h4><i class="icon-eye-open"></i><span class="hidden-phone">Recent images from people you follow</span></h4>', html = True)
 
         # Following two are unsupported with sqlite
 
         #response = self.client.post(url('liked'), follow = True)
-        #self.assertContains(response, '<h4><i class="icon-thumbs-up"></i>Recently liked</h4>', html = True)
+        #self.assertContains(response, '<h4><i class="icon-thumbs-up"></i><span class="hidden-phone">Recently liked</span></h4>', html = True)
 
         #response = self.client.post(url('bookmarked'), follow = True)
-        #self.assertContains(response, '<h4><i class="icon-bookmark"></i>Recently bookmarked</h4>', html = True)
+        #self.assertContains(response, '<h4><i class="icon-bookmark"></i><span class="hidden-phone">Recently bookmarked</span></h4>', html = True)
 
         response = self.client.post(url('fits'), follow = True)
-        self.assertContains(response, '<h4><i class="icon-archive"></i>Recent images with link to TIFF/FITS</h4>', html = True)
+        self.assertContains(response, '<h4><i class="icon-archive"></i><span class="hidden-phone">Recent images with link to TIFF/FITS</span></h4>', html = True)
 
         self.client.logout()
