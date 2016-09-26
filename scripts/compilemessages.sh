6cc1ba2b571cebbf71410138f5e62ccdfa02c963
Salvatore Iovene <salvatore@iovene.com>
2016-09-26 14:53:28 +0000

    Update languages

--- scripts/compilemessages.sh
+++ scripts/compilemessages.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 apps="astrobin rawdata nested_comments astrobin_apps_users astrobin_apps_images astrobin_apps_platesolving astrobin_apps_donations astrobin_apps_premium astrobin_apps_groups"
+langs="ar be ca cs de el en es fa fi fr hu it ja nl pl pt-BR pt ro ru sk sq sr tr zh-CN zh-TW"
 
 echo "Processing apps..."
 for app in $apps; do
     echo " * $app"
-    (cd $app; ../manage.py compilemessages >/dev/null 2>&1)
+    for lang in $langs; do
+        echo -n " $lang"
+        (cd $app; ../manage.py compilemessages -l $lang)
+    done
+    echo
 done
 
