a6fb4a15731b621a11765050f63edfe5f590ba3f
Salvatore Iovene <salvatore@iovene.com>
2016-10-06 21:10:46 +0200

    Log also in non debug mode

--- settings.py
+++ settings.py
@@ -537,65 +537,60 @@ RAWDATA_ROOT = os.environ['ASTROBIN_RAWDATA_ROOT']
 
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-if DEBUG:
-    LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': True,
-        'formatters': {
-            'standard': {
-                'format' : "[%(asctime)s] %(levelname)s [%(name)s] %(message)s",
-                'datefmt' : "%d/%b/%Y %H:%M:%S"
-            },
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
         },
-        'handlers': {
-            'null': {
-                'level':'DEBUG',
-                'class':'django.utils.log.NullHandler',
-            },
-            'console':{
-                'level':'DEBUG',
-                'class':'logging.StreamHandler',
-                'formatter': 'standard'
-            },
-            'logfile': {
-                'level':'DEBUG',
-                'class':'logging.handlers.RotatingFileHandler',
-                'filename': "/var/log/astrobin/debug.log",
-                'maxBytes': 50000,
-                'backupCount': 2,
-                'formatter': 'standard',
-            },
-            'mail_admins': {
-                'level': 'ERROR',
-                'class': 'django.utils.log.AdminEmailHandler'
-            }
+    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
         },
-        'loggers': {
-            'django': {
-                'handlers':['console'],
-                'propagate': True,
-                'level':'INFO',
-            },
-            'django.db.backends': {
-                'handlers': ['console'],
-                'level': 'ERROR',
-                'propagate': False,
-            },
-            'apps': {
-                'handlers': ['console', 'logfile'],
-                'level': 'DEBUG',
-            },
-            'django.request': {
-                'handlers': ['mail_admins'],
-                'level': 'ERROR',
-                'propagate': True,
-            },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+        'logfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': "/var/log/astrobin/debug.log",
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
         }
+    },
+    'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'INFO',
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'apps': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
     }
-else:
-    LOGGING = {
-        'version': 1,
-    }
+}
 
 if TESTING:
     import logging
