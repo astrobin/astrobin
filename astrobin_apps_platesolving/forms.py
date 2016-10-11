7d734ac54ee58a29ed6a623d6c2a389a7b4c4e0d
Salvatore Iovene <salvatore@iovene.com>
2016-10-11 13:21:12 +0200

    Instead of hiding plate-solving fields on blind form, disable them

--- astrobin_apps_platesolving/forms.py
+++ astrobin_apps_platesolving/forms.py
@@ -8,3 +8,11 @@ from .models import PlateSolvingSettings
 class PlateSolvingSettingsForm(forms.ModelForm):
     class Meta:
         model = PlateSolvingSettings
+
+    def __init__(self, *args, **kwargs):
+        super(PlateSolvingSettingsForm, self).__init__(*args, **kwargs)
+        instance = kwargs['instance']
+        if instance and instance.blind:
+            for field in self.fields:
+                if field != 'blind':
+                    self.fields[field].widget.attrs['disabled'] = 'disabled'
