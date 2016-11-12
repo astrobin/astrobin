69524f4cf1a303ee6537d5c27aa9c0f7c54c4a64
Salvatore Iovene <salvatore@iovene.com>
2016-11-12 14:16:55 +0000

    Add change_email to l18n scripts

--- scripts/pootle/out.sh
+++ scripts/pootle/out.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 LANGS="ar be ca cs de el es fa fi fr it hu ja nl pl pt pt-BR ro ru sk sq sr tr zh-CN zh-TW"
-PROJECTS="astrobin nested_comments rawdata astrobin_apps_users astrobin_apps_images astrobin_apps_platesolving astrobin_apps_donations astrobin_apps_premium astrobin_apps_groups"
+PROJECTS="astrobin nested_comments rawdata astrobin_apps_users astrobin_apps_images astrobin_apps_platesolving astrobin_apps_donations astrobin_apps_premium astrobin_apps_groups change_email"
 TO='/var/www/astrobin'
 FROM='/home/astrobin/venv/translate.astrobin.com/lib/python2.7/site-packages/pootle/po'
-
 
 
 echo "Syncing Pootle files to disk..."
