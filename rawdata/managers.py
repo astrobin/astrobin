9b8542323eeb0532a9f680e1dc410efe5f591437
Salvatore Iovene <salvatore@iovene.com>
2017-10-19 17:52:06 +0300

    (Upgrade) Rename get_query_set to get_queryset (removed in 1.8)

--- rawdata/managers.py
+++ rawdata/managers.py
@@ -5,11 +5,11 @@ from django.db import models
 from .folders import FOLDER_TYPE_LOOKUP
 
 class SoftDeleteManager(models.Manager):
-    def get_query_set(self):
-        return super(SoftDeleteManager, self).get_query_set().filter(active = True)
+    def get_queryset(self):
+        return super(SoftDeleteManager, self).get_queryset().filter(active = True)
 
     def all_with_inactive(self):
-        return super(SoftDeleteManager, self).get_query_set()
+        return super(SoftDeleteManager, self).get_queryset()
 
     def get(self, *args, **kwargs):
         ''' if a specific record was requested, return it even if it's inactive '''
@@ -19,11 +19,11 @@ class SoftDeleteManager(models.Manager):
         ''' if pk was specified as a kwarg, return even if it's inactive '''
         if 'pk' in kwargs:
             return self.all_with_inactive().filter(*args, **kwargs)
-        return self.get_query_set().filter(*args, **kwargs)
+        return self.get_queryset().filter(*args, **kwargs)
 
 class RawImageManager(SoftDeleteManager):
     def by_ids_or_params(self, ids, params):
-        qs = super(RawImageManager, self).get_query_set()
+        qs = super(RawImageManager, self).get_queryset()
         ids = ids.split(',') if ids else None
         if ids:
             qs = qs.filter(id__in = ids)
