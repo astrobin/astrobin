f29e990ceb1ed9169c6baf33c7deec162d959bdd
Salvatore Iovene <salvatore@iovene.com>
2017-11-03 12:15:35 +0000

    Add astrobin/celery.py to fix celery

--- astrobin/__init__.py
+++ astrobin/__init__.py
@@ -3,5 +3,7 @@ from __future__ import absolute_import
 # App initialization
 default_app_config = 'astrobin.apps.AstroBinAppConfig'
 
+from .celery import app as celery_app
+
 # Custom monkeypatches
 from .monkeypatch import *
---
+++ astrobin/celery.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+import os
+from celery import Celery
+
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
+from django.conf import settings
+app = Celery('astrobin')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+app.config_from_object('django.conf:settings')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
