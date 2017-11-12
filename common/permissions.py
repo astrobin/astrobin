c2f24e51540d433a92c5cbe7edf997934fb715c8
Salvatore Iovene <salvatore@iovene.com>
2017-11-12 18:15:12 +0200

    (api) Update permission API to latest DRF
    Closes #480

--- common/permissions.py
+++ common/permissions.py
@@ -1,13 +1,9 @@
 from rest_framework import permissions
 
 class ReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view, obj=None):
-        # Skip the check unless this is an object-level test
-        if obj is None:
-            return True
-
+    def has_permission(self, request, view):
         # Read permissions are allowed to any request
-        if request.method in permissions.SAFE_METHODS:            
+        if request.method in permissions.SAFE_METHODS:
             return True
 
         return False
