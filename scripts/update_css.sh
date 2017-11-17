00c4e05d26bf0334ff79a2232964ba13252327d1
Salvatore Iovene <salvatore@iovene.com>
2017-11-17 08:54:42 +0200

    (ui) Implement site wide breadcrumbs
    Closes #406

--- scripts/update_css.sh
+++ scripts/update_css.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 lessc astrobin/static/css/astrobin.less > astrobin/static/css/astrobin.css
+lessc astrobin/static/css/astrobin-mobile.less > astrobin/static/css/astrobin-mobile.css
 ./manage.py collectstatic --noinput
 echo 'flush_all' | netcat localhost 11211
 
