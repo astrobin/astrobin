c3f647e6bc9106701d9faff15535d591027545ad
Salvatore Iovene <salvatore@iovene.com>
2017-11-19 12:06:03 +0200

    (comments) Add admin interface
    Closes #499

--- nested_comments/admin.py
+++ nested_comments/admin.py
@@ -5,4 +5,18 @@ from django.contrib import admin
 from .models import NestedComment
 
 
-admin.site.register(NestedComment)
+class NestedCommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'content_object',
+        'author',
+        'text',
+        'created',
+        'updated',
+        'deleted',
+    )
+
+    search_fields = (
+        'author',
+    )
+
+admin.site.register(NestedComment, NestedCommentAdmin)
