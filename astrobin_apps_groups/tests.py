32dd467854c77cf6e7a0de989a2c7e1ba017cf2b
Salvatore Iovene <salvatore@iovene.com>
2017-11-23 20:49:07 +0100

    (tests) Fix broken test

--- astrobin_apps_groups/tests.py
+++ astrobin_apps_groups/tests.py
@@ -58,7 +58,7 @@ class GroupsTest(TestCase):
     def test_group_list_view(self):
         response = self.client.get(reverse('group_list'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, '<h1>Public groups</h1>', html = True)
+        self.assertContains(response, '<h1>Public groups<small>Open to the whole community.</small></h1>', html = True)
         self.assertContains(response, '<td class="group-name"><a href="' + reverse('group_detail', args = (self.group.pk,)) + '">Test group</a></td>', html = True)
         self.assertContains(response, '<td class="group-members hidden-phone">1</td>', html = True)
         self.assertContains(response, '<td class="group-images hidden-phone">0</td>', html = True)
