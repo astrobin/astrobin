7dd1d7ab2f7afdecf569ea964ac26a2bc8d4e532
Salvatore Iovene <salvatore@iovene.com>
2017-11-26 18:28:02 +0100

    (moderation) Fix 'mark_as_ham' view to allow 'image_moderators'

---
+++ astrobin/tests/test_moderation.py
@@ -0,0 +1,83 @@
+# Django
+from django.contrib.auth.models import Group, User
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+# AstroBin
+from astrobin.models import Image
+
+
+class ModerationTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('user', 'user@test.com', 'password')
+        self.moderator = User.objects.create_user('moderator', 'moderator@test.com', 'password')
+
+        self.content_moderators = Group.objects.create(name = 'content_moderators')
+        self.image_moderators = Group.objects.create(name = 'image_moderators')
+
+        self.moderator.groups.add(self.content_moderators, self.image_moderators)
+
+
+    def tearDown(self):
+        self.content_moderators.delete()
+        self.image_moderators.delete()
+        self.user.delete()
+        self.moderator.delete()
+
+
+    def _do_upload(self, filename, wip = False):
+        data = {'image_file': open(filename, 'rb')}
+        if wip:
+            data['wip'] = True
+
+        return self.client.post(
+            reverse('image_upload_process'),
+            data,
+            follow = True)
+
+
+    def _get_last_image(self):
+        return Image.all_objects.all().order_by('-id')[0]
+
+
+    def test_image_moderation_queue_view(self):
+        # Anon cannot access
+        response = self.client.get(reverse('image_moderation'))
+        self.assertEqual(response.status_code, 403)
+
+        # Non moderator cannot access
+        self.client.login(username = 'user', password = 'password')
+        response = self.client.get(reverse('image_moderation'))
+        self.assertEqual(response.status_code, 403)
+
+        # Moderator can access
+        self.client.login(username = 'moderator', password = 'password')
+        response = self.client.get(reverse('image_moderation'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "The moderation queue is empty")
+
+        # Upload an image that requires moderation
+        self.client.login(username = 'user', password = 'password')
+        self._do_upload('astrobin/fixtures/test.jpg')
+        image = self._get_last_image()
+        image.title = "Moderation test"
+        image.save()
+
+        # Moderator can see the image in the queue
+        self.client.login(username = 'moderator', password = 'password')
+        response = self.client.get(reverse('image_moderation'))
+        self.assertContains(response, "Moderation test")
+
+        # Moderator can approve it
+        response = self.client.post(reverse('image_moderation_mark_as_ham'), {'ids[]': [image.pk]})
+        self.assertEqual(response.status_code, 200)
+        image = Image.all_objects.get(pk = image.pk)
+        self.assertEqual(image.moderator_decision, 1)
+
+        # Moderator can mark it as spam
+        image.moderator_decision = 0
+        image.save()
+        response = self.client.post(reverse('image_moderation_mark_as_spam'), {'ids[]': [image.pk]})
+        self.assertEqual(response.status_code, 200)
+        image = Image.all_objects.get(pk = image.pk)
+        self.assertEqual(image.moderator_decision, 2)
