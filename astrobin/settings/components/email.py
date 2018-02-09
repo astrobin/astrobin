56204aef90f9c901a3bc26b8cf9a7fcc8f9ae122
Salvatore Iovene <salvatore@iovene.com>
2018-02-09 21:02:44 +0100

    Add support for optional dummy email backend

--- astrobin/settings/components/email.py
+++ astrobin/settings/components/email.py
@@ -1,11 +1,13 @@
 import os
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+if os.environ.get('SEND_EMAILS', 'true') != 'true':
+    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+
 DEFAULT_FROM_EMAIL = os.environ['DEFAULT_FROM_EMAIL']
 EMAIL_SUBJECT_PREFIX = os.environ['EMAIL_SUBJECT_PREFIX']
-
 SERVER_EMAIL = os.environ['SERVER_EMAIL']
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ['EMAIL_HOST']
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
