e2e6345ed7a30391a4bc64db1bd7d958060b26ce
Salvatore Iovene <salvatore@iovene.com>
2018-02-10 20:11:35 +0100

    Add django-celery-email

--- astrobin/celery.py
+++ astrobin/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ['DJANGO_SETTINGS_MODULE'])
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'astrobin.settings')
 
 from django.conf import settings
 app = Celery('astrobin')
--- astrobin/settings/components/email.py
+++ astrobin/settings/components/email.py
@@ -1,8 +1,16 @@
 import os
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-if os.environ.get('SEND_EMAILS', 'true') != 'true':
-    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
+CELERY_EMAIL_TASK_CONFIG = {
+    'queue': 'default',
+    'delivery_mode': 1,  # non persistent
+    'rate_limit': '50/m',  # 50 chunks per minute
+}
+
+if os.environ.get('SEND_EMAILS', 'true') == 'true':
+    CELERY_EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+else:
+    CELERY_EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
 DEFAULT_FROM_EMAIL = os.environ['DEFAULT_FROM_EMAIL']
 EMAIL_SUBJECT_PREFIX = os.environ['EMAIL_SUBJECT_PREFIX']
@@ -11,6 +19,6 @@ SERVER_EMAIL = os.environ['SERVER_EMAIL']
 EMAIL_HOST = os.environ['EMAIL_HOST']
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = os.environ['EMAIL_HOST_PORT']
+EMAIL_USE_SSL = os.environ['EMAIL_USE_SSL'] == 'true'
 
