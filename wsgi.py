8dac997e91513b6232b9cc0730f76945360d9338
Salvatore Iovene <salvatore@iovene.com>
2018-02-10 10:24:55 +0100

    Add wdb

--- wsgi.py
+++ wsgi.py
@@ -4,3 +4,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+if os.environ.get('DEBUG', "false") == "true":
+    from wdb.ext import WdbMiddleware
+    application = WdbMiddleware(application)
+
