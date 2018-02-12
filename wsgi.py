414ea1560bb714a3c9cedf290238230baa37d1a8
Salvatore Iovene <salvatore@iovene.com>
2018-02-12 08:32:04 +0100

    Fix path to settings file in wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "astrobin.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
