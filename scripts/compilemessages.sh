132bcbb3b0b647651d4cd59e3b6866268c2d0f73
Salvatore Iovene <salvatore@iovene.com>
2018-02-12 08:03:40 +0100

    Fix path of manage.py in compilemessages script

--- scripts/compilemessages.sh
+++ scripts/compilemessages.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 apps="astrobin rawdata nested_comments astrobin_apps_users astrobin_apps_images astrobin_apps_platesolving astrobin_apps_donations astrobin_apps_premium astrobin_apps_groups"
 langs="ar be ca cs de el en es fa fi fr hu it ja nl pl pt-BR pt ro ru sk sq sr tr zh-CN zh-TW"
-manage="/var/www/astrobin/manage.py"
+manage="../manage.py"
 
 echo "Processing apps..."
 for app in $apps; do
     echo " * $app"
     for lang in $langs; do
         echo -n " $lang"
-        (cd $app; $manage compilemessages -l $lang)
+        (cd $app; python $manage compilemessages -l $lang)
     done
     echo
 done
