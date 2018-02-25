273c571b174972b1ba60fa7e18e64f1b7d93889e
Salvatore Iovene <salvatore@iovene.com>
2018-02-25 10:39:04 +0100

    (settings) Fix serving of local static files in debug mode

--- astrobin/urls.py
+++ astrobin/urls.py
@@ -1,6 +1,6 @@
 # Django
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import url, include, patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -448,7 +448,10 @@ urlpatterns = [
     ###########################################################################
 
     url(r'^language/set/(?P<lang>[\w-]+)/$',set_language, name='set_language'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT);
+]
 
 urlpatterns += [url(r'^silk/', include('silk.urls', namespace='silk'))]
 
+if settings.DEBUG and settings.LOCAL_STATIC_STORAGE:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += patterns('', (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': True}))
