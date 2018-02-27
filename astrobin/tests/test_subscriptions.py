a672824c38e76a05dc5a0149eadc32628e3d2469
Salvatore Iovene <salvatore@iovene.com>
2018-02-27 22:54:16 +0100

    (tests) Fix when PREMIUM_ENABLED = False

--- astrobin/tests/test_subscriptions.py
+++ astrobin/tests/test_subscriptions.py
@@ -18,49 +18,51 @@ from astrobin.templatetags.tags import (
 
 class SubscriptionsTest(TestCase):
     def test_subscription_validity(self):
-        u = User.objects.create_user(
-            username = 'test', email='test@test.com', password = 'password')
-        g, created = Group.objects.get_or_create(name = "astrobin_premium")
-        s, created = Subscription.objects.get_or_create(
-            name = "Test subscription",
-            price = 1,
-            group = g,
-            category = "premium")
-        us, created = UserSubscription.objects.get_or_create(
-            user = u,
-            subscription = s)
+        with self.settings(PREMIUM_ENABLED = True):
+            u = User.objects.create_user(
+                username = 'test', email='test@test.com', password = 'password')
+            g, created = Group.objects.get_or_create(name = "astrobin_premium")
+            s, created = Subscription.objects.get_or_create(
+                name = "Test subscription",
+                price = 1,
+                group = g,
+                category = "premium")
+            us, created = UserSubscription.objects.get_or_create(
+                user = u,
+                subscription = s)
 
-        us.subscribe()
+            us.subscribe()
 
-        self.assertEqual(valid_subscriptions(u), [s])
-        self.assertEqual(has_valid_subscription(u, s.pk), True)
-        self.assertEqual(
-            has_valid_subscription_in_category(u, "premium"), True)
-        self.assertEqual(
-            get_premium_subscription_expiration(u), us.expires)
-        self.assertEqual(
-            has_subscription_by_name(u, "Test subscription"), True)
-        self.assertEqual(
-            get_subscription_by_name(u, "Test subscription"), us)
+            self.assertEqual(valid_subscriptions(u), [s])
+            self.assertEqual(has_valid_subscription(u, s.pk), True)
+            self.assertEqual(
+                has_valid_subscription_in_category(u, "premium"), True)
+            self.assertEqual(
+                get_premium_subscription_expiration(u), us.expires)
+            self.assertEqual(
+                has_subscription_by_name(u, "Test subscription"), True)
+            self.assertEqual(
+                get_subscription_by_name(u, "Test subscription"), us)
 
-        us.delete()
-        s.delete()
-        g.delete()
-        u.delete()
+            us.delete()
+            s.delete()
+            g.delete()
+            u.delete()
 
 
     def test_subscription_list_view(self):
-        g, created = Group.objects.get_or_create(name = "Test group")
-        s, created = Subscription.objects.get_or_create(
-            name = "Test subscription",
-            price = 1,
-            group = g,
-            category = "premium")
+        with self.settings(PREMIUM_ENABLED = True):
+            g, created = Group.objects.get_or_create(name = "astrobin_premium")
+            s, created = Subscription.objects.get_or_create(
+                name = "AstroBin Premium",
+                price = 1,
+                group = g,
+                category = "premium")
 
-        response = self.client.get(reverse('subscription_list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "<td>Test subscription</td>", html = True)
+            response = self.client.get(reverse('subscription_list'))
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, "<td>AstroBin Premium</td>", html = True)
 
-        s.delete()
-        g.delete()
+            s.delete()
+            g.delete()
 
