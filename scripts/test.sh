5c9d8b7601691c15970ee3afdfe62f7bfb6618a0
Salvatore Iovene <salvatore@iovene.com>
2018-03-03 23:49:03 +0100

    Add codecov support

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-coverage run ./manage.py test \
+coverage run --source=. ./manage.py test \
     astrobin astrobin_apps_groups \
     astrobin_apps_iotd \
     astrobin_apps_notifications \
     astrobin_apps_premium \
     rawdata \
     --noinput --failfast
-if [ ${PIPESTATUS[0]} -eq 0 ]
-then
-    echo -n "Counting asserts... "
-    echo "~$(git grep "self.*assert" | wc -l)"
 
-    echo -n "Generating HTML report... "
-    coverage html --omit="/venv/*" -d cover >/dev/null 2>&1 && echo "OK"
-
-    echo -n "Total coverage: "
-    coverage report --omit="/venv/*" | grep "TOTAL" | awk '{print $4}'
-
-    echo
-    echo "--------------------------------------------"
-    echo "See full coverage report at cover/index.html"
-    echo "All done. Thanks for testing!"
-    echo "--------------------------------------------"
+if [ $? -eq 0 ]; then
+    if [[ -z "${CODECOV_TOKEN}" ]]; then
+        echo "Please export CODECOV_TOKEN to upload coverage results to Codecov"
+    else
+        codecov -t ${CODECOV_TOKEN}
+    fi
 fi
