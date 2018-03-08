c6209999ad487aca9acbe1356385439914525166
Salvatore Iovene <salvatore@iovene.com>
2018-03-08 07:06:06 +0100

    (tasks) Delete inactive users with hard-bounced emails
    This issue addresses task 1 of #543

---
+++ astrobin/tests/test_tasks.py
@@ -0,0 +1,31 @@
+# Django
+from django.contrib.auth.models import User
+from django.test import TestCase, override_settings
+from django.utils import timezone
+
+# Third party
+from django_bouncy.models import Bounce
+
+# AstroBin
+from astrobin.tasks import *
+
+
+class TaskTest(TestCase):
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+    def test_delete_inactive_bounced_accounts(self):
+        user = User.objects.create_user(
+            'test', 'test@test.com', 'password')
+        bounce = Bounce.objects.create(
+            hard=True,
+            bounce_type="Permanent",
+            address="test@test.com",
+            mail_timestamp=timezone.now())
+
+        user.is_active = False
+        user.save()
+
+        delete_inactive_bounced_accounts.apply()
+
+        self.assertFalse(User.objects.filter(username="test").exists())
+        self.assertFalse(Bounce.objects.filter(address="test@test.com").exists())
+
