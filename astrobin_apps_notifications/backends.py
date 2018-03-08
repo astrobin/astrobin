88018cd43b2cd0e6217e9ba40f28615644a364b1
Salvatore Iovene <salvatore@iovene.com>
2018-03-08 17:07:01 +0100

    Add HTML transactional emails
    Closes #578

--- astrobin_apps_notifications/backends.py
+++ astrobin_apps_notifications/backends.py
@@ -1,3 +1,10 @@
+# Django
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import get_template, render_to_string
+from django.template import TemplateDoesNotExist
+from django.utils.translation import ugettext
+
 # Third party
 from django_bouncy.models import Bounce
 from gadjo.requestprovider.signals import get_request
@@ -35,3 +42,44 @@ class EmailBackend(BaseEmailBackend):
             bounce_type="Permanent",
             address=user.email)
         return can_send and not bounces.exists()
+
+    def deliver(self, recipient, sender, notice_type, extra_context):
+        context = self.default_context()
+        context.update({
+            "recipient": recipient,
+            "sender": sender,
+            "notice": ugettext(notice_type.display),
+        })
+        context.update(extra_context)
+
+        messages = self.get_formatted_messages((
+            "short.txt",
+            "full.txt",
+            "full.html"
+        ), notice_type.label, context)
+
+        subject = "".join(render_to_string("notification/email_subject.txt", {
+            "message": messages["short.txt"],
+        }, context).splitlines())
+
+        body = render_to_string("notification/email_body.txt", {
+            "message": messages["full.txt"],
+        }, context)
+
+        try:
+            html_template = get_template(
+                "notification/%s/full.html" % notice_type.label)
+            message = messages["full.html"]
+        except TemplateDoesNotExist:
+            message = messages["full.txt"]
+
+        html_body = render_to_string("notification/email_body.html", {
+            "message": message
+        }, context)
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [recipient.email],
+            html_message = html_body)
