d27e81805178f47ed71b4fe8c12bb909055770d3
Salvatore Iovene <salvatore@iovene.com>
2018-03-08 08:46:05 +0100

    (emails) Do not send emails to permanently hard bounced address
    Closes #543

--- astrobin/settings/components/notification.py
+++ astrobin/settings/components/notification.py
@@ -1,6 +1,6 @@
 NOTIFICATION_LANGUAGE_MODULE = "astrobin.UserProfile"
 NOTIFICATION_BACKENDS = (
     ("on-site", "astrobin_apps_notifications.backends.PersistentMessagesBackend"),
-    ("email", "notification.backends.email.EmailBackend"),
+    ("email", "astrobin_apps_notifications.backends.EmailBackend"),
 )
 
