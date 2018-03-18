213398dedf410cddbe58cb36e1d85116a6d0dd18
Paul Krizak <paul.krizak@gmail.com>
2018-03-18 11:54:28 -0700

    Eliminate more STATIC_URL usage

--- astrobin/settings/components/pagination.py
+++ astrobin/settings/components/pagination.py
@@ -1,3 +1,4 @@
+from django.contrib.staticfiles import storage
 EL_PAGINATION_PER_PAGE = 35
-EL_PAGINATION_LOADING = '<img src="' + STATIC_URL + 'common/images/ajax-loader-bar.gif" alt="..." />'
+EL_PAGINATION_LOADING = '<img src="%s" alt="..." />' % storage.ConfiguredStorage().url('common/images/ajax-loader-bar.gif')
 
