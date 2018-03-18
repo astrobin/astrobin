f74feabe83ed8b109c82b12c4474375092100b96
Paul Krizak <paul.krizak@gmail.com>
2018-03-17 17:59:52 -0700

    Look in astrobin subdir for static content

--- astrobin/settings/components/avatar.py
+++ astrobin/settings/components/avatar.py
@@ -1,4 +1,4 @@
-AVATAR_DEFAULT_URL = 'images/astrobin-default-avatar.png?v=1'
+AVATAR_DEFAULT_URL = 'astrobin/images/astrobin-default-avatar.png?v=1'
 AVATAR_STORAGE_DIR = 'images/avatars'
 AVATAR_AUTO_GENERATE_SIZES = (64, 80, 194)
 AVATAR_CACHE_ENABLED = not DEBUG
