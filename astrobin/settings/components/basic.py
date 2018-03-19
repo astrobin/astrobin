cad185dc24e94a303ca572a9f33a698c92af15c4
Salvatore Iovene <salvatore@iovene.com>
2018-03-19 22:38:44 +0100

    Remove settings that was preventing template debugging

--- astrobin/settings/components/basic.py
+++ astrobin/settings/components/basic.py
@@ -17,7 +17,6 @@ SITE_ID = 1
 
 TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
 DEBUG = os.environ.get('DEBUG', 'true') == 'true'
-DEBUG_PROPAGATE_EXCEPTIONS = DEBUG
 INTERNAL_IPS = ['127.0.0.1', '172.18.0.1'] # localhost and docker gateway
 
 MAINTENANCE_MODE = False
