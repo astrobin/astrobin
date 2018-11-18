59fe340381a1b06bb626d221ffc2e5d04f1465b9
Salvatore Iovene <salvatore@iovene.com>
2018-11-18 13:03:26 +0100

    Fix plate-solving on local development mode

--- astrobin_apps_platesolving/utils.py
+++ astrobin_apps_platesolving/utils.py
@@ -8,13 +8,19 @@ from django.core.files.temp import NamedTemporaryFile
 
 
 def getFromStorage(image, alias):
-    url = image.thumbnail(alias)
-    if "://" in url:
-        url = url.split('://')[1]
-    else:
-        url = settings.BASE_URL + url
+    def encoded(path):
+        return urllib2.quote(path.encode('utf-8'))
 
-    url = 'http://' + urllib2.quote(url.encode('utf-8'))
+    url = image.thumbnail(alias)
+
+    if "://" in url:
+        # We are getting the full path and must only encode the part after the protocol
+        # (we assume that the hostname is ASCII)
+        protocol, path = url.split("://")
+        url = protocol + encoded(path)
+    else:
+        url = settings.BASE_URL + encoded(url)
+
     headers = { 'User-Agent' : 'Mozilla/5.0' }
     req = urllib2.Request(url, None, headers)
     img = NamedTemporaryFile(delete = True)
