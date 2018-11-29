6e1203e6d9e75f0d590ea47d53b7f1f39ae27da2
Salvatore Iovene <salvatore@iovene.com>
2018-11-29 21:09:58 +0100

    (i18n) Fix path to manage.py in makemessages script

--- scripts/makemessages.sh
+++ scripts/makemessages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apps="astrobin rawdata nested_comments astrobin_apps_users astrobin_apps_images astrobin_apps_platesolving astrobin_apps_donations astrobin_apps_premium astrobin_apps_groups"
 langs="en-GB ar be ca cs de el en es fa fi fr hu it ja nl pl pt-BR pt ro ru sk sq sr tr zh-CN zh-TW"
-manage="/var/www/astrobin/manage.py"
+manage="../manage.py"
 
 echo "Processing apps..."
 for app in $apps; do
