e75627f473f17e8d763d8f22096936f559a6da87
Salvatore Iovene <salvatore@iovene.com>
2018-12-01 21:13:45 +0100

    (i18n) Update script to pull translations from translation server

--- scripts/pootle/in.sh
+++ scripts/pootle/in.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
-LANGS="ar be ca cs de el es fa fi fr it hu ja nl pt pl pt-BR ro ru sk sq sr tr zh-CN zh-TW"
-PROJECTS="astrobin nested_comments rawdata astrobin_apps_users astrobin_apps_images astrobin_apps_platesolving astrobin_apps_donations astrobin_apps_premium astrobin_apps_groups"
-FROM='/var/www/astrobin'
-TO='/home/astrobin/venv/translate.astrobin.com/lib/python2.7/site-packages/pootle/po'
 
-for proj in $PROJECTS
-do
-    echo "* $proj"
-    for lang in $LANGS
-    do
-        echo "   Copying $lang..."
-        cp $FROM/$proj/locale/$lang/LC_MESSAGES/django.po $TO/$proj/$lang/
+# Usage:
+#
+#    ./in.sh $ssh_cmd $pootle_host $remote_pootle_root
+#
+# e.g.:
+#    ./in.sh "ssh -i auth.pem" user@host.com /opt/pootle
+
+SSH=$1
+HOST=$2
+POOTLE_ROOT=$3
+
+ARCHIVE=/tmp/astrobin_i18n_files
+
+${SSH} ${HOST} /bin/bash <<-EOT
+    sudo python ${POOTLE_ROOT}/bin/pootle sync_stores
+
+    rm -rf ${ARCHIVE}
+    mkdir -p ${ARCHIVE}
+
+    for project in ${POOTLE_ROOT}/lib/pootle/translations/*; do
+      for language in \$project/*; do
+
+        project_name=\$(basename \$project)
+        language_code=\$(basename \$language)
+
+        src=\$language/\$project_name.po
+        dest=${ARCHIVE}/\$project_name/locale/\$language_code/LC_MESSAGES
+
+        if [[ "\${language_code}" == "templates" ]]; then
+          src=\$language/\$project_name.pot
+          dest=${ARCHIVE}/\$project_name/locale/en/LC_MESSAGES
+        fi
+
+        mkdir -p \$dest
+        cp \$src \$dest/django.po
+      done
     done
-done
+EOT
 
-echo "Updating Pootle..."
-pootle update_stores
-
-echo "Done."
-
+rsync -avz -e "${SSH}" ${HOST}:${ARCHIVE}/* .
