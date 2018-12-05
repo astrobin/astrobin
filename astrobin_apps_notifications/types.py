527447051d53e466d7e1fb078bb65b4197fdcc49
Salvatore Iovene <salvatore@iovene.com>
2018-12-05 20:54:13 +0100

    (i18n) Fix missing localizaion of notification settings
    Closes #652

--- astrobin_apps_notifications/types.py
+++ astrobin_apps_notifications/types.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+
 # Fields:
 # 0 - notice label
 # 1 - display name
@@ -12,31 +14,31 @@ NOTICE_TYPES = (
     ),
     (
         'new_follower',
-        'You have a new follower',
+        _('You have a new follower'),
         '',
         2,
     ),
     (
         'new_image',
-        'New image from a user you follow',
+        _('New image from a user you follow'),
         '',
         2,
     ),
     (
         'new_image_revision',
-        'New image revision from a user you follow',
+        _('New image revision from a user you follow'),
         '',
         2,
     ),
     (
         'image_solved',
-        'Your image was plate-solved',
+        _('Your image was plate-solved'),
         '',
         1,
     ),
     (
         'image_not_solved',
-        'Your image could not be plate-solved',
+        _('Your image could not be plate-solved'),
         '',
         1,
     ),
@@ -48,157 +50,157 @@ NOTICE_TYPES = (
     ),
     (
         'new_comment',
-        'Your image received a new comment',
+        _('Your image received a new comment'),
         '',
         2,
     ),
     (
         'new_comment_reply',
-        'There was a reply to your comment',
+        _('There was a reply to your comment'),
         '',
         2,
     ),
     (
         'new_bookmark',
-        'Your image was bookmarked',
+        _('Your image was bookmarked'),
         '',
         2,
     ),
     (
         'new_like',
-        'Your image was liked',
+        _('Your image was liked'),
         '',
         2,
     ),
     (
         'new_image_from_gear',
-        'New image with some gear you follow',
+        _('New image with some gear you follow'),
         '',
         2,
     ),
     (
         'new_image_of_subject',
-        'New image of a subject you follow',
+        _('New image of a subject you follow'),
         '',
         2,
     ),
     (
         'api_key_request_approved',
-        'Your API Key request has been approved',
+        _('Your API Key request has been approved'),
         '',
         2,
     ),
     (
         'new_gear_discussion',
-        'New discussion about a gear item you own',
+        _('New discussion about a gear item you own'),
         '',
         2,
     ),
     (
         'new_gear_review',
-        'New review of a gear item you own',
+        _('New review of a gear item you own'),
         '',
         2,
     ),
     (
         'rawdata_posted_to_pool',
-        'New data posted to public pool',
+        _('New data posted to public pool'),
         '',
         2,
     ),
     (
         'rawdata_posted_image_to_public_pool',
-        'New image posted to public data pool',
+        _('New image posted to public data pool'),
         '',
         2,
     ),
     (
         'rawdata_posted_to_private_folder',
-        'New data posted to shared private folder',
+        _('New data posted to shared private folder'),
         '',
         2,
     ),
     (
         'rawdata_posted_image_to_private_folder',
-        'New image posted to shared private folder',
+        _('New image posted to shared private folder'),
         '',
         2,
     ),
     (
         'rawdata_invited_to_private_folder',
-        'You are invited to a shared private folder',
+        _('You are invited to a shared private folder'),
         '',
         2,
     ),
     (
         'user_joined_public_group',
-        'A user you follow joined a public group',
+        _('A user you follow joined a public group'),
         '',
         2,
     ),
     (
         'new_group_invitation',
-        'You are invited to a group',
+        _('You are invited to a group'),
         '',
         2,
     ),
     (
         'new_group_join_request',
-        'A user requested to join a group you moderate',
+        _('A user requested to join a group you moderate'),
         '',
         2,
     ),
     (
         'group_join_request_approved',
-        'A group moderator approved your request to join',
+        _('A group moderator approved your request to join'),
         '',
         2,
     ),
     (
         'group_join_request_rejected',
-        'A group moderator rejected your request to join',
+        _('A group moderator rejected your request to join'),
         '',
         2,
     ),
     (
         'new_public_group_created',
-        'A user you follow created a new public group',
+        _('A user you follow created a new public group'),
         '',
         2,
     ),
     (
         'new_topic_in_group',
-        'New forum topic in a group you are a member of',
+        _('New forum topic in a group you are a member of'),
         '',
         2,
     ),
     (
         'received_email',
-        'You have received a new private message',
+        _('You have received a new private message'),
         '',
         2
     ),
     (
         'expiring_subscription',
-        'Your premium subscription will expire in one week',
+        _('Your premium subscription will expire in one week'),
         '',
         2
     ),
     (
         'expiring_subscription_autorenew',
-        'Your subscription will be renewed in one week',
+        _('Your subscription will be renewed in one week'),
         '',
         2
     ),
     (
         'missing_data_source',
-        'You have images with a missing "Data source"',
+        _('You have images with a missing "Data source"'),
         '',
         2
     ),
     (
         'missing_remote_source',
-        'You have images with a missing "Remote source"',
+        _('You have images with a missing "Remote source"'),
         '',
         2
     )
