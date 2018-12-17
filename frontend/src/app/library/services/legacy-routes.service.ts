5d80ce2566bbabeda2a1b7d46aee3d6923aee979
Salvatore Iovene <salvatore@iovene.com>
2018-12-17 21:45:36 +0100

    (frontend) Complete user menu

--- frontend/src/app/library/services/legacy-routes.service.ts
+++ frontend/src/app/library/services/legacy-routes.service.ts
@@ -15,4 +15,9 @@ export class LegacyRoutesService {
   BOOKMARKS = (profile: UserProfileModel) => `/users/${profile.userObject.username}/bookmarks/`;
   PLOTS = (profile: UserProfileModel) => `/users/${profile.userObject.username}/plots/`;
   RAWDATA = "/rawdata/";
+  RAWDATA_PRIVATE_SHARED_FOLDERS = "/rawdata/privatesharedfolders/";
+  INBOX = "/messages/inbox/";
+  API_KEYS = (profile: UserProfileModel) => `/users/${profile.userObject.username}/apikeys/`;
+  SETTINGS = "/profile/edit/basic/";
+  LOGOUT = "/accounts/logout/";
 }
