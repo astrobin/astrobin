c9483ad2610abb5a7f7de4b9e4a60f07d1c7c4a1
Salvatore Iovene <salvatore@iovene.com>
2018-12-17 22:47:52 +0100

    (frontend) Add Explore menu to header

--- frontend/src/app/library/services/legacy-routes.service.ts
+++ frontend/src/app/library/services/legacy-routes.service.ts
@@ -16,9 +16,16 @@ export class LegacyRoutesService {
   PLOTS = (profile: UserProfileModel) => `/users/${profile.userObject.username}/plots/`;
   RAWDATA = "/rawdata/";
   RAWDATA_PRIVATE_SHARED_FOLDERS = "/rawdata/privatesharedfolders/";
+  RAWDATA_PUBLIC_DATA_POOLS  = "/rawdata/publicdatapools/";
   INBOX = "/messages/inbox/";
   API_KEYS = (profile: UserProfileModel) => `/users/${profile.userObject.username}/apikeys/`;
   SETTINGS = "/profile/edit/basic/";
   LOGOUT = "/accounts/logout/";
   SET_LANGUAGE = (languageCode: string) => `/language/set/${languageCode}/`;
+  FORUM_HOME = "/forum/";
+  SEARCH = "/search/";
+  TOP_PICKS = "/explore/top-picks/";
+  IOTD = "/iotd/archive/";
+  GROUPS = "/groups/";
+  TRENDING_ASTROPHOTOGRAPHERS = "/trending-astrophotographers/";
 }
