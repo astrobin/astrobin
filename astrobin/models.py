a6d430ab229d42b0b543ecf01edb5e27442d8b0b
Filippo Nori <40722053+philoez98@users.noreply.github.com>
2019-02-10 14:54:04 +0100

    Update models.py

--- astrobin/models.py
+++ astrobin/models.py
@@ -692,7 +692,7 @@ class Image(HasSolutionMixin, SafeDeleteModel):
         'AMATEUR_HOSTING',
         'PUBLIC_AMATEUR_DATA',
         'PRO_DATA',
-        'RENT/SHARED',
+        'RENTED/SHARED',
         'MIX',
         'OTHER',
         'UNKNOWN'
