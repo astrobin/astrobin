afcfba7ae8fc625c2c267d76179fadc9aab3e9bd
Salvatore Iovene <salvatore@iovene.com>
2019-03-03 22:10:50 +0100

    Attempt to limit travis test pipeline to only starting the astrobin container

--- astrobin/settings/components/db.py
+++ astrobin/settings/components/db.py
@@ -1,9 +1,12 @@
 import os
 
+engine = os.environ.get('DB_ENGINE', 'django.db.backends.postgresql_psycopg2')
+SITE_ROOT = os.path.dirname(os.path.join(os.path.realpath(__file__)))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'astrobin',
+        'ENGINE': engine,
+        'NAME': os.path.join(SITE_ROOT, 'db.sqlite3') if 'sqlite' in engine else 'astrobin',
         'USER': 'astrobin',
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'astrobin'),
         'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
@@ -11,4 +14,3 @@ DATABASES = {
         'CONN_MAX_AGE': 60,
     }
 }
-
