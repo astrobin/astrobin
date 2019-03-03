0c6ee38c165fa4d5b06703702f5010ddc3c366e1
Salvatore Iovene <salvatore@iovene.com>
2019-03-03 18:10:19 +0100

    (platesolving) Prevent "placeholder" image from being plate-solved (#686)
    Closes #671

--- astrobin_apps_platesolving/utils.py
+++ astrobin_apps_platesolving/utils.py
@@ -6,9 +6,15 @@ from django.conf import settings
 from django.core.files import File
 from django.core.files.temp import NamedTemporaryFile
 
+class ThumbnailNotReadyException(Exception):
+    pass
 
 def getFromStorage(image, alias):
     url = image.thumbnail(alias)
+
+    if "placeholder" in url:
+        raise ThumbnailNotReadyException
+
     if "://" in url:
         url = url.split('://')[1]
     else:
@@ -21,4 +27,5 @@ def getFromStorage(image, alias):
     img.write(urllib2.urlopen(req).read())
     img.flush()
     img.seek(0)
+
     return File(img)
