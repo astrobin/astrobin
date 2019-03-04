35ef53768699bfc9c0a64c5b72afc60cd718bfab
Salvatore Iovene <salvatore@iovene.com>
2019-03-04 17:33:49 +0100

    (ci) Reduce Travis CI verbosity (#683)
      * Remove unneeded static files finder
      * Remove /dev/null redirection
      * Limit travis test pipeline to only starting the astrobin container
      * Set 0 verbosity for collectstatic

--- astrobin/settings/components/db.py
+++ astrobin/settings/components/db.py
@@ -1,14 +1,21 @@
 import os
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'astrobin',
-        'USER': 'astrobin',
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'astrobin'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
-        'PORT': '5432',
-        'CONN_MAX_AGE': 60,
+if os.environ.get("USE_SQLITE", 'false') == 'true':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'astrobin_test_db',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'astrobin',
+            'USER': 'astrobin',
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'astrobin'),
+            'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
+            'PORT': '5432',
+            'CONN_MAX_AGE': 60,
+        }
+    }
