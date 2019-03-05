816b28e865247ea38842afbe94dd4be9b1233f45
Salvatore Iovene <salvatore@iovene.com>
2019-03-05 22:38:41 +0100

    (infra) Add landing app to make/compile messages scripts

--- scripts/compilemessages.sh
+++ scripts/compilemessages.sh
@@ -8,9 +8,11 @@ astrobin_apps_users
 astrobin_apps_images
 astrobin_apps_platesolving
 astrobin_apps_donations
-astrobin_apps_premium astrobin_apps_groups
+astrobin_apps_premium
+astrobin_apps_groups
 astrobin_apps_notifications
 astrobin_apps_iotd
+astrobin_apps_landing
 "
 
 for APP in ${APPS}; do
