b656335c5b43a8935c7a29ba761001c16edebc8f
Salvatore Iovene <salvatore@iovene.com>
2019-06-15 18:59:26 +0200

    (ci) Fix geoip test

--- astrobin/tests/test_utils.py
+++ astrobin/tests/test_utils.py
@@ -9,7 +9,7 @@ from astrobin.utils import get_client_country_code
 
 class UtilsTest(TestCase):
     @patch("astrobin.utils.get_client_ip")
-    @patch("django.contrib.gis.geoip2.GeoIP2.country")
+    @patch("django.contrib.gis.geoip2.GeoIP2.country_code")
     def test_get_client_country_code(self, mock_country, mock_get_client_ip):
         mock_country.return_value = "CH"
         mock_get_client_ip.return_value = "123.123.123.123"
