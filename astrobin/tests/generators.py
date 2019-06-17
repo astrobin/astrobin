c2d3a4fa97d64013908b8b389adb7889d580687b
Salvatore Iovene <salvatore@iovene.com>
2019-06-17 23:04:43 +0200

    (images) Allow revision upload without notification
    Closes #699

---
+++ astrobin/tests/generators.py
@@ -0,0 +1,32 @@
+import random
+import string
+
+from django.contrib.auth.models import User
+
+from astrobin.models import Image, ImageRevision
+
+
+class Generators:
+
+    @staticmethod
+    def randomString(stringLength=10):
+        return ''.join(random.choice(string.ascii_lowercase) for i in range(stringLength))
+
+    @staticmethod
+    def user():
+        return User.objects.create(
+            username=Generators.randomString(),
+            password=Generators.randomString()
+        )
+
+    @staticmethod
+    def image():
+        return Image.objects.create(
+            user=Generators.user()
+        )
+
+    @staticmethod
+    def imageRevision():
+        return ImageRevision.objects.create(
+            image=Generators.image()
+        )
---
+++ astrobin/tests/test_signals.py
@@ -0,0 +1,62 @@
+from django.test import TestCase
+from mock import patch
+
+from astrobin.signals import imagerevision_post_save
+from astrobin.tests.generators import Generators
+
+
+class SignalsTest(TestCase):
+
+    @patch("astrobin.signals.push_notification")
+    @patch("astrobin.signals.add_story")
+    def test_imagerevision_post_save_wip_no_notifications(self, add_story, push_notification):
+        revision = Generators.imageRevision()
+        revision.image.is_wip = True
+
+        push_notification.reset_mock()
+        add_story.reset_mock()
+
+        imagerevision_post_save(None, revision, True)
+
+        self.assertFalse(push_notification.called)
+        self.assertFalse(add_story.called)
+
+    @patch("astrobin.signals.push_notification")
+    @patch("astrobin.signals.add_story")
+    def test_imagerevision_post_save_not_created_no_notifications(self, add_story, push_notification):
+        revision = Generators.imageRevision()
+
+        push_notification.reset_mock()
+        add_story.reset_mock()
+
+        imagerevision_post_save(None, revision, False)
+
+        self.assertFalse(push_notification.called)
+        self.assertFalse(add_story.called)
+
+    @patch("astrobin.signals.push_notification")
+    @patch("astrobin.signals.add_story")
+    def test_imagerevision_post_save_skip_notifications(self, add_story, push_notification):
+        revision = Generators.imageRevision()
+        revision.skip_notifications = True
+
+        push_notification.reset_mock()
+        add_story.reset_mock()
+
+        imagerevision_post_save(None, revision, True)
+
+        self.assertFalse(push_notification.called)
+        self.assertFalse(add_story.called)
+
+    @patch("astrobin.signals.push_notification")
+    @patch("astrobin.signals.add_story")
+    def test_imagerevision_post_save(self, add_story, push_notification):
+        revision = Generators.imageRevision()
+
+        push_notification.reset_mock()
+        add_story.reset_mock()
+
+        imagerevision_post_save(None, revision, True)
+
+        self.assertTrue(push_notification.called)
+        self.assertTrue(add_story.called)
