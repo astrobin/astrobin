cecafc68bf789688c5b7615bbfee714350a65311
Salvatore Iovene <salvatore@iovene.com>
2019-06-18 08:38:01 +0200

    (images) Allow revision upload without notification (#700)
    Closes #699

--- astrobin/forms.py
+++ astrobin/forms.py
@@ -351,9 +351,10 @@ class BringToAttentionForm(forms.Form):
 class ImageRevisionUploadForm(forms.ModelForm):
     class Meta:
         model = ImageRevision
-        fields = ('image_file', 'description',)
+        fields = ('image_file', 'description', 'skip_notifications')
         widgets = {
             'description': forms.Textarea(attrs={'rows': 4}),
+            'skip_notifications': forms.CheckboxInput()
         }
 
 
