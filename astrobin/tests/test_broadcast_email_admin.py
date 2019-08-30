e644416fcdb7d7e74ce56cf71fd0e6b946290b6e
Salvatore Iovene <salvatore@iovene.com>
2019-08-30 09:21:38 +0200

    (admin) Only send premium offers to people withoout premium who accept marketing emails

--- astrobin/tests/test_broadcast_email_admin.py
+++ astrobin/tests/test_broadcast_email_admin.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
 
 from django.contrib.admin import AdminSite
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.test import TestCase, RequestFactory
+from django.utils import timezone
 from mock import patch
+from subscription.models import Subscription, UserSubscription
 
 from astrobin.admin import BroadcastEmailAdmin
 from astrobin.models import BroadcastEmail
@@ -21,9 +23,29 @@ class BroadcastEmailAdminTest(TestCase):
 
         sendMock.assert_not_called()
 
-    @patch("django.core.mail.EmailMessage.send")
+    @patch("astrobin.tasks.send_broadcast_email.delay")
     @patch("django.contrib.admin.ModelAdmin.message_user")
-    def test_submit_premium_offer_discount(self, messageUserMock, sendMock):
+    def test_submit_premium_offer_discount(self, messageUserMock, taskMock):
+        request = RequestFactory().get("/")
+
+        email = BroadcastEmail.objects.create(subject="test")
+        admin = BroadcastEmailAdmin(model=BroadcastEmail, admin_site=AdminSite())
+
+        user = User.objects.create(username="discount", password="discount")
+        user.userprofile.premium_offer = "premium_offer_discount_20"
+        user.userprofile.premium_offer_expiration = datetime.now() + timedelta(days=1)
+        user.userprofile.receive_marketing_and_commercial_material = True
+        user.userprofile.save()
+
+        admin.submit_premium_offer_discount(request, BroadcastEmail.objects.filter(pk=email.pk))
+
+        taskMock.assert_called()
+        self.assertEqual(1, taskMock.call_count)
+        user.userprofile.refresh_from_db()
+        self.assertIsNotNone(user.userprofile.premium_offer_sent)
+
+    @patch("astrobin.tasks.send_broadcast_email.delay")
+    def test_submit_premium_offer_discount_no_marketing_emails(self, taskMock):
         request = RequestFactory().get("/")
 
         email = BroadcastEmail.objects.create(subject="test")
@@ -36,14 +58,41 @@ class BroadcastEmailAdminTest(TestCase):
 
         admin.submit_premium_offer_discount(request, BroadcastEmail.objects.filter(pk=email.pk))
 
-        sendMock.assert_called()
-        self.assertEqual(1, sendMock.call_count)
-        user.userprofile.refresh_from_db()
-        self.assertIsNotNone(user.userprofile.premium_offer_sent)
+        taskMock.assert_not_called()
 
-    @patch("django.core.mail.EmailMessage.send")
+    @patch("astrobin.tasks.send_broadcast_email.delay")
+    def test_submit_premium_offer_discount_already_premium(self, taskMock):
+        request = RequestFactory().get("/")
+
+        email = BroadcastEmail.objects.create(subject="test")
+        admin = BroadcastEmailAdmin(model=BroadcastEmail, admin_site=AdminSite())
+
+
+        user = User.objects.create(username="discount", password="discount")
+        user.userprofile.premium_offer = "premium_offer_discount_20"
+        user.userprofile.premium_offer_expiration = datetime.now() + timedelta(days=1)
+        user.userprofile.receive_marketing_and_commercial_material = True
+        user.userprofile.save()
+
+        group, created = Group.objects.get_or_create(name="astrobin_premium")
+        subscription, created = Subscription.objects.get_or_create(
+            name="AstroBin Premium",
+            price=1,
+            group=group,
+            category="premium")
+        user_subscription, created = UserSubscription.objects.get_or_create(
+            user=user,
+            subscription=subscription)
+
+        user_subscription.subscribe()
+
+        admin.submit_premium_offer_discount(request, BroadcastEmail.objects.filter(pk=email.pk))
+
+        taskMock.assert_not_called()
+
+    @patch("astrobin.tasks.send_broadcast_email.delay")
     @patch("django.contrib.admin.ModelAdmin.message_user")
-    def test_submit_premium_offer_discount_two_users(self, messageUserMock, sendMock):
+    def test_submit_premium_offer_discount_already_premium_but_expired(self, messageUserMock, taskMock):
         request = RequestFactory().get("/")
 
         email = BroadcastEmail.objects.create(subject="test")
@@ -52,26 +101,119 @@ class BroadcastEmailAdminTest(TestCase):
         user = User.objects.create(username="discount", password="discount")
         user.userprofile.premium_offer = "premium_offer_discount_20"
         user.userprofile.premium_offer_expiration = datetime.now() + timedelta(days=1)
+        user.userprofile.receive_marketing_and_commercial_material = True
+        user.userprofile.save()
+
+        group, created = Group.objects.get_or_create(name="astrobin_premium")
+        subscription, created = Subscription.objects.get_or_create(
+            name="AstroBin Premium",
+            price=1,
+            group=group,
+            category="premium")
+        user_subscription, created = UserSubscription.objects.get_or_create(
+            user=user,
+            subscription=subscription)
+
+        user_subscription.subscribe()
+
+        user_subscription.expires = timezone.now() - timedelta(days=1)
+        user_subscription.save()
+
+        admin.submit_premium_offer_discount(request, BroadcastEmail.objects.filter(pk=email.pk))
+
+        taskMock.assert_called()
+
+    @patch("astrobin.tasks.send_broadcast_email.delay")
+    def test_submit_premium_offer_discount_already_lite(self, taskMock):
+        request = RequestFactory().get("/")
+
+        email = BroadcastEmail.objects.create(subject="test")
+        admin = BroadcastEmailAdmin(model=BroadcastEmail, admin_site=AdminSite())
+
+        user = User.objects.create(username="discount", password="discount")
+        user.userprofile.premium_offer = "premium_offer_discount_20"
+        user.userprofile.premium_offer_expiration = datetime.now() + timedelta(days=1)
+        user.userprofile.receive_marketing_and_commercial_material = True
+        user.userprofile.save()
+
+        group, created = Group.objects.get_or_create(name="astrobin_premium")
+        subscription, created = Subscription.objects.get_or_create(
+            name="AstroBin Lite",
+            price=1,
+            group=group,
+            category="premium")
+        user_subscription, created = UserSubscription.objects.get_or_create(
+            user=user,
+            subscription=subscription)
+
+        user_subscription.subscribe()
+
+        admin.submit_premium_offer_discount(request, BroadcastEmail.objects.filter(pk=email.pk))
+
+        taskMock.assert_not_called()
+
+    @patch("astrobin.tasks.send_broadcast_email.delay")
+    @patch("django.contrib.admin.ModelAdmin.message_user")
+    def test_submit_premium_offer_discount_already_premium_with_discount(self, messageUserMock, taskMock):
+        request = RequestFactory().get("/")
+
+        email = BroadcastEmail.objects.create(subject="test")
+        admin = BroadcastEmailAdmin(model=BroadcastEmail, admin_site=AdminSite())
+
+        user = User.objects.create(username="discount", password="discount")
+        user.userprofile.premium_offer = "premium_offer_discount_20"
+        user.userprofile.premium_offer_expiration = datetime.now() + timedelta(days=1)
+        user.userprofile.receive_marketing_and_commercial_material = True
+        user.userprofile.save()
+
+        group, created = Group.objects.get_or_create(name="astrobin_premium")
+        subscription, created = Subscription.objects.get_or_create(
+            name="AstroBin Premium 30% discount",
+            price=1,
+            group=group,
+            category="premium_offer_discount_30")
+        user_subscription, created = UserSubscription.objects.get_or_create(
+            user=user,
+            subscription=subscription)
+
+        user_subscription.subscribe()
+
+        admin.submit_premium_offer_discount(request, BroadcastEmail.objects.filter(pk=email.pk))
+
+        taskMock.assert_not_called()
+
+    @patch("astrobin.tasks.send_broadcast_email.delay")
+    @patch("django.contrib.admin.ModelAdmin.message_user")
+    def test_submit_premium_offer_discount_two_users(self, messageUserMock, taskMock):
+        request = RequestFactory().get("/")
+
+        email = BroadcastEmail.objects.create(subject="test")
+        admin = BroadcastEmailAdmin(model=BroadcastEmail, admin_site=AdminSite())
+
+        user = User.objects.create(username="discount", password="discount")
+        user.userprofile.premium_offer = "premium_offer_discount_20"
+        user.userprofile.premium_offer_expiration = datetime.now() + timedelta(days=1)
+        user.userprofile.receive_marketing_and_commercial_material = True
         user.userprofile.save()
 
         user2 = User.objects.create(username="discount2", password="discount")
         user2.userprofile.premium_offer = "premium_offer_discount_50"
         user2.userprofile.premium_offer_expiration = datetime.now() + timedelta(days=1)
+        user2.userprofile.receive_marketing_and_commercial_material = True
         user2.userprofile.save()
 
         admin.submit_premium_offer_discount(request, BroadcastEmail.objects.filter(pk=email.pk))
 
-        sendMock.assert_called()
-        self.assertEqual(2, sendMock.call_count)
+        taskMock.assert_called()
 
         user.userprofile.refresh_from_db()
         user2.userprofile.refresh_from_db()
         self.assertIsNotNone(user.userprofile.premium_offer_sent)
         self.assertIsNotNone(user2.userprofile.premium_offer_sent)
 
-    @patch("django.core.mail.EmailMessage.send")
+    @patch("astrobin.tasks.send_broadcast_email.delay")
     @patch("django.contrib.admin.ModelAdmin.message_user")
-    def test_submit_premium_offer_discount_already_sent_long_ago(self, messageUserMock, sendMock):
+    def test_submit_premium_offer_discount_already_sent_long_ago(self, messageUserMock, taskMock):
         request = RequestFactory().get("/")
 
         email = BroadcastEmail.objects.create(subject="test")
@@ -81,18 +223,18 @@ class BroadcastEmailAdminTest(TestCase):
         user.userprofile.premium_offer = "premium_offer_discount_20"
         user.userprofile.premium_offer_expiration = datetime.now() + timedelta(days=1)
         user.userprofile.premium_offer_sent = datetime.now() - timedelta(days=31)
+        user.userprofile.receive_marketing_and_commercial_material = True
         user.userprofile.save()
 
         admin.submit_premium_offer_discount(request, BroadcastEmail.objects.filter(pk=email.pk))
 
-        sendMock.assert_called()
-        self.assertEqual(1, sendMock.call_count)
+        taskMock.assert_called()
         user.userprofile.refresh_from_db()
         self.assertTrue(user.userprofile.premium_offer_sent > datetime.now() - timedelta(minutes=1))
 
-    @patch("django.core.mail.EmailMessage.send")
+    @patch("astrobin.tasks.send_broadcast_email.delay")
     @patch("django.contrib.admin.ModelAdmin.message_user")
-    def test_submit_premium_offer_discount_already_sent_too_recently(self, messageUserMock, sendMock):
+    def test_submit_premium_offer_discount_already_sent_too_recently(self, messageUserMock, taskMock):
         request = RequestFactory().get("/")
 
         email = BroadcastEmail.objects.create(subject="test")
@@ -102,10 +244,11 @@ class BroadcastEmailAdminTest(TestCase):
         user.userprofile.premium_offer = "premium_offer_discount_20"
         user.userprofile.premium_offer_expiration = datetime.now() + timedelta(days=1)
         user.userprofile.premium_offer_sent = sent = datetime.now() - timedelta(days=20)
+        user.userprofile.receive_marketing_and_commercial_material = True
         user.userprofile.save()
 
         admin.submit_premium_offer_discount(request, BroadcastEmail.objects.filter(pk=email.pk))
 
-        sendMock.assert_not_called()
+        taskMock.assert_not_called()
         user.userprofile.refresh_from_db()
         self.assertEqual(sent, user.userprofile.premium_offer_sent)
