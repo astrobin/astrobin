435bacec29a551638664558bd8a65c36f795eb3b
Salvatore Iovene <salvatore@iovene.com>
2020-02-05 20:55:29 +0100

    (add new subscriptions to init script)

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,11 +5,94 @@ python manage.py migrate --noinput
 python manage.py migrate --run-syncdb --noinput
 python manage.py sync_translation_fields --noinput
 
-# Create initial data
+# Create Premium subsciptions
+python manage.py shell << EOF
+from subscription.models import Subscription
+from django.contrib.auth.models import Group
 
-# Create Premium groups
-echo "from django.contrib.auth.models import Group; Group.objects.get_or_create(name='astrobin_lite')" | python manage.py shell
-echo "from django.contrib.auth.models import Group; Group.objects.get_or_create(name='astrobin_premium')" | python manage.py shell
+Group.objects.get_or_create(name='astrobin_lite')
+Group.objects.get_or_create(name='astrobin_lite_2020')
+Group.objects.get_or_create(name='astrobin_premium')
+Group.objects.get_or_create(name='astrobin_premium_2020')
+Group.objects.get_or_create(name='astrobin_ultimate_2020')
+
+Subscription.objects.get_or_create(
+    name='AstroBin Lite',
+    currency="USD"
+    price=18,
+    trial_period=0,
+    trial_unit=None,
+    recurrence_period=0,
+    recurrence_unit=None,
+    group=Group.objects.get(name='astrobin_lite'),
+    category='premium')
+
+Subscription.objects.get_or_create(
+    name='AstroBin Lite (autorenew)',
+    currency="USD"
+    price=18,
+    trial_period=0,
+    trial_unit=None,
+    recurrence_period=1,
+    recurrence_unit='Y',
+    group=Group.objects.get(name='astrobin_lite'),
+    category='premium')
+
+Subscription.objects.get_or_create(
+    name='AstroBin Lite 2020+',
+    currency="CHF",
+    price=20,
+    trial_period=0,
+    trial_unit=None,
+    recurrence_period=0,
+    recurrence_unit=None,
+    group=Group.objects.get(name='astrobin_lite_2020'),
+    category='premium')
+
+Subscription.objects.get_or_create(
+    name='AstroBin Premium',
+    currency="USD",
+    price=36,
+    trial_period=0,
+    trial_unit=None,
+    recurrence_period=0,
+    recurrence_unit=None,
+    group=Group.objects.get(name='astrobin_premium'),
+    category='premium')
+
+Subscription.objects.get_or_create(
+    name='AstroBin Premium (autorenew)',
+    currency="USD"
+    price=36,
+    trial_period=0,
+    trial_unit=None,
+    recurrence_period=1,
+    recurrence_unit='Y',
+    group=Group.objects.get(name='astrobin_premium'),
+    category='premium')
+
+Subscription.objects.get_or_create(
+    name='AstroBin Premium 2020+',
+    currency="CHF",
+    price=40,
+    trial_period=0,
+    trial_unit=None,
+    recurrence_period=0,
+    recurrence_unit=None,
+    group=Group.objects.get(name='astrobin_premium_2020'),
+    category='premium')
+
+Subscription.objects.get_or_create(
+    name='AstroBin Ultimate 2020+',
+    currency="CHF",
+    price=60,
+    trial_period=0,
+    trial_unit=None,
+    recurrence_period=0,
+    recurrence_unit=None,
+    group=Group.objects.get(name='astrobin_ultimate_2020'),
+    category='premium')
+EOF
 
 # Create moderation groups
 echo "from django.contrib.auth.models import Group; Group.objects.get_or_create(name='content_moderators')" | python manage.py shell
