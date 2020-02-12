53fea6050ab723403ea27b14c73f7f68a0f8fbba
Salvatore Iovene <salvatore@iovene.com>
2020-02-12 22:25:58 +0100

    (add ra/dec tooltip to hd image too))

--- astrobin_apps_platesolving/static/astrobin_apps_platesolving/js/astrobin_apps_platesolving_mousemove.js
+++ astrobin_apps_platesolving/static/astrobin_apps_platesolving/js/astrobin_apps_platesolving_mousemove.js
@@ -4,14 +4,6 @@
 
         self.$image = $('.show-ra-dec-coordinates');
         self.$tooltip = $('#ra-dec-coordinates');
-
-        this.position = [
-            self.$image.offset().left,
-            self.$image.offset().top,
-            self.$image.offset().left + this.$image.outerWidth(),
-            self.$image.offset().top + this.$image.outerHeight()
-        ];
-
         self.enableCall = true;
 
         self.$image.mousemove(function (e) {
@@ -19,9 +11,8 @@
 
             self.enableCall = false;
 
-            var x = e.pageX - self.position[0];
-            var y = e.pageY - self.position[1];
-
+            var x = e.offsetX;
+            var y = e.offsetY;
             var scaledX = x * solvedSizeX / self.$image.outerWidth();
             var scaledY = y * solvedSizeY / self.$image.outerHeight();
 
@@ -38,8 +29,8 @@
             self.$tooltip
                 .text(interpolationText.alpha + ', ' + interpolationText.delta)
                 .css({
-                    left: x - self.$tooltip.width() / 2 - 10,
-                    top: y - 42
+                    left: x - self.$tooltip.outerWidth() / 2,
+                    top: y - 45
                 })
                 .show();
 
