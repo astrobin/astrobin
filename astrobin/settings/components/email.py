394bd2664f56b7c467702bc34872f44f161b6c12
Salvatore Iovene <salvatore@iovene.com>
2020-02-28 14:19:02 +0100

    (add support for no emails at all)

--- astrobin/settings/components/email.py
+++ astrobin/settings/components/email.py
@@ -1,7 +1,8 @@
 import os
 
+SEND_EMAILS = os.environ.get('SEND_EMAILS', 'false')
 
-if os.environ.get('SEND_EMAILS', 'false') == 'true':
+if  SEND_EMAILS == 'true':
     EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
     CELERY_EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
@@ -16,7 +17,7 @@ if os.environ.get('SEND_EMAILS', 'false') == 'true':
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
     EMAIL_PORT = os.environ.get('EMAIL_HOST_PORT', 25)
     EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'false') == 'true'
-else:
+elif SEND_EMAILS == 'dummy':
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
     EMAIL_HOST = 'debug_email'
@@ -24,6 +25,8 @@ else:
     EMAIL_HOST_PASSWORD = ''
     EMAIL_PORT = 25
     EMAIL_USE_SSL = False
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
 SERVER_EMAIL = DEFAULT_FROM_EMAIL = os.environ.get('SERVER_EMAIL', 'noreply@astrobin.com')
 EMAIL_SUBJECT_PREFIX = os.environ.get('EMAIL_SUBJECT_PREFIX', '[AstroBin]')
