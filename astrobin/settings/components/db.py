7a9a19db60fbf45bb29cd997e49808aa4c99a3ee
Salvatore Iovene <salvatore@iovene.com>
2020-03-04 23:19:24 +0100

    (infra) Add support for read replica database

--- astrobin/settings/components/db.py
+++ astrobin/settings/components/db.py
@@ -19,3 +19,17 @@ else:
             'CONN_MAX_AGE': 60,
         }
     }
+
+    if os.environ.get('POSTGRES_READ_REPLICA_HOST'):
+        DATABASES['reader'] = {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'astrobin',
+            'USER': 'astrobin',
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'astrobin'),
+            'HOST': os.environ.get('POSTGRES_READ_REPLICA_HOST', 'postgres'),
+            'PORT': '5432',
+            'CONN_MAX_AGE': 60,
+        }
+
+        DATABASE_ROUTERS = ['astrobin.db.CustomRouter']
+
