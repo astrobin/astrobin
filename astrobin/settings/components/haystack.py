131ee981f72a608decc99e5c66dca7600db0157d
Salvatore Iovene <salvatore@iovene.com>
2020-03-06 23:27:45 +0100

    (search) Fix issue with search objects having UTF-8 characters not getting indexed
    Closes #814

--- astrobin/settings/components/haystack.py
+++ astrobin/settings/components/haystack.py
@@ -3,6 +3,29 @@ import os
 from elasticsearch import RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 
+
+class AWS4AuthEncodingFix(AWS4Auth):
+    def __call__(self, request):
+        request = super(AWS4AuthEncodingFix, self).__call__(request)
+
+        for header_name in request.headers:
+            self._encode_header_to_utf8(request, header_name)
+
+        return request
+
+    def _encode_header_to_utf8(self, request, header_name):
+        value = request.headers[header_name]
+
+        if isinstance(value, unicode):
+            value = value.encode('utf-8')
+
+        if isinstance(header_name, unicode):
+            del request.headers[header_name]
+            header_name = header_name.encode('utf-8')
+
+        request.headers[header_name] = value
+
+
 HAYSTACK_DEFAULT_OPERATOR = 'AND'
 HAYSTACK_SEARCH_RESULTS_PER_PAGE = 70
 
@@ -21,7 +44,7 @@ HAYSTACK_CONNECTIONS = {
 if 'es.amazonaws.com' in HAYSTACK_CONNECTIONS['default']['URL']:
     HAYSTACK_CONNECTIONS['default']['KWARGS'] = {
         'port': 443,
-        'http_auth': AWS4Auth(
+        'http_auth': AWS4AuthEncodingFix(
             os.environ.get('AWS_ACCESS_KEY_ID'),
             os.environ.get('AWS_SECRET_ACCESS_KEY'),
             os.environ.get('ELASTIC_SEARCH_AWS_REGION', 'us-east-1'),
