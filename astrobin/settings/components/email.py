64632ed5c50690cc9b95d43361ed4786e9419c3b
Salvatore Iovene <salvatore@iovene.com>
2020-03-07 13:48:44 +0100

    (infra) Strip envorinment variables
    Closes #815

--- astrobin/settings/components/email.py
+++ astrobin/settings/components/email.py
@@ -1,7 +1,7 @@
 import os
 
 
-if os.environ.get('SEND_EMAILS', 'false') == 'true':
+if os.environ.get('SEND_EMAILS', 'false').strip() == 'true':
     EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
     CELERY_EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
@@ -11,11 +11,11 @@ if os.environ.get('SEND_EMAILS', 'false') == 'true':
         'rate_limit': '30/m',  # 50 chunks per minute
     }
 
-    EMAIL_HOST = os.environ.get('EMAIL_HOST', 'localhost')
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-    EMAIL_PORT = os.environ.get('EMAIL_HOST_PORT', 25)
-    EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'false') == 'true'
+    EMAIL_HOST = os.environ.get('EMAIL_HOST', 'localhost').strip()
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '').strip()
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '').strip()
+    EMAIL_PORT = int(os.environ.get('EMAIL_HOST_PORT', '25').strip())
+    EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'false').strip() == 'true'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
@@ -25,7 +25,7 @@ else:
     EMAIL_PORT = 25
     EMAIL_USE_SSL = False
 
-SERVER_EMAIL = DEFAULT_FROM_EMAIL = os.environ.get('SERVER_EMAIL', 'noreply@astrobin.com')
-EMAIL_SUBJECT_PREFIX = os.environ.get('EMAIL_SUBJECT_PREFIX', '[AstroBin]')
+SERVER_EMAIL = DEFAULT_FROM_EMAIL = os.environ.get('SERVER_EMAIL', 'noreply@astrobin.com').strip()
+EMAIL_SUBJECT_PREFIX = os.environ.get('EMAIL_SUBJECT_PREFIX', '[AstroBin]').strip()
 
 
