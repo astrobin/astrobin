d04e9c57e057bde5ea456b0f7f11ab2e983c58d3
Salvatore Iovene <salvatore@iovene.com>
2020-03-14 10:24:00 +0100

    (images) Hide corrupted revisions to visitors
    Closes #833

--- astrobin_apps_images/tests/test_image_service.py
+++ astrobin_apps_images/tests/test_image_service.py
@@ -24,3 +24,24 @@ class TestImageService(TestCase):
         Generators.imageRevision(image=image, is_final=True, label='C')
 
         self.assertFalse(ImageService(image).is_corrupted())
+
+    def test_get_revisions_excludes_corrupted(self):
+        image = Generators.image()
+        Generators.imageRevision(image=image)
+        Generators.imageRevision(image=image, corrupted=True, label='C')
+
+        self.assertEquals(ImageService(image).get_revisions().count(), 1)
+
+    def test_get_revisions_includes_corrupted(self):
+        image = Generators.image()
+        Generators.imageRevision(image=image)
+        Generators.imageRevision(image=image, corrupted=True, label='C')
+
+        self.assertEquals(ImageService(image).get_revisions(include_corrupted=True).count(), 2)
+
+    def test_get_revisions_with_description(self):
+        image = Generators.image()
+        Generators.imageRevision(image=image)
+        Generators.imageRevision(image=image, label='C', description='Foo')
+
+        self.assertEquals(ImageService(image).get_revisions_with_description().count(), 1)
