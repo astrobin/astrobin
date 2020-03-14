9bc6cd113032c3739cb1322c1e830a18a15ccf4a
Salvatore Iovene <salvatore@iovene.com>
2020-03-14 15:43:57 +0100

    (gallery) Improve performance of filtering by corrupted images

--- astrobin_apps_images/tests/test_image_service.py
+++ astrobin_apps_images/tests/test_image_service.py
@@ -5,26 +5,6 @@ from astrobin_apps_images.services import ImageService
 
 
 class TestImageService(TestCase):
-    def test_is_corrupted(self):
-        image = Generators.image()
-        corrupted = Generators.image(corrupted=True)
-
-        self.assertFalse(ImageService(image).is_corrupted())
-        self.assertTrue(ImageService(corrupted).is_corrupted())
-
-    def test_is_corrupted_when_final_revision_is(self):
-        image = Generators.image(is_final=False)
-        Generators.imageRevision(image=image, corrupted=True, is_final=True)
-
-        self.assertTrue(ImageService(image).is_corrupted())
-
-    def test_is_not_corrupted_when_non_final_revision_is(self):
-        image = Generators.image(is_final=False)
-        Generators.imageRevision(image=image, corrupted=True, is_final=False)
-        Generators.imageRevision(image=image, is_final=True, label='C')
-
-        self.assertFalse(ImageService(image).is_corrupted())
-
     def test_get_revisions_excludes_corrupted(self):
         image = Generators.image()
         Generators.imageRevision(image=image)
