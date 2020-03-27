ab36ad5253c95bb2f8bb7910dfdbee9667133aa8
Salvatore Iovene <salvatore@iovene.com>
2020-03-27 13:38:12 +0100

    (remove unused file)

--- astrobin/forms/fits_upload_form.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from astrobin.models import Image
-
-
-class ImageFitsUploadForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        fields = ('fits_file',)
