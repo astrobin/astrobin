0bfb839a4235d59e74235ec10c2643ade1d917f1
Salvatore Iovene <salvatore@iovene.com>
2020-04-07 20:46:24 +0200

    (images) Fix incorrect permissions to view real size images
    Closes #901

--- astrobin_apps_premium/templatetags/astrobin_apps_premium_tags.py
+++ astrobin_apps_premium/templatetags/astrobin_apps_premium_tags.py
@@ -182,8 +182,12 @@ def can_perform_advanced_platesolving(user):
 
 
 @register.filter
-def can_see_real_resolution(user):
-    return not is_free(user)
+def can_see_real_resolution(user, image):
+    return not is_free(user) or \
+           user == image.user or \
+           is_any_ultimate(image.user) or \
+           is_premium(image.user) or \
+           is_lite(image.user)
 
 
 @register.filter
