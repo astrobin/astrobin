a16d89d9c780d616466d472198cddc51c7ad5aec
Salvatore Iovene <salvatore@iovene.com>
2020-04-15 21:11:03 +0200

    (images) Add timeout to request to purge file from CloudFlare

--- astrobin/services/cloudflare_service.py
+++ astrobin/services/cloudflare_service.py
@@ -1,15 +1,16 @@
 import logging
-import urllib2
 
+import requests
 import simplejson
 from django.conf import settings
+
+log = logging.getLogger('apps')
 
 
 class CloudflareService:
     base_url = "https://api.cloudflare.com/client/v4"
 
     def __init__(self):
-        self.log = logging.getLogger('apps')
         self.token = settings.CLOUDFLARE_TOKEN
         self.zone_id = settings.CLOUDFLARE_ZONE_ID
 
@@ -20,7 +21,7 @@ class CloudflareService:
         """
 
         if not self.token or not self.zone_id:
-            self.log.debug("CloudflareService cannot work without token and zone_id")
+            log.debug("CloudflareService cannot work without token and zone_id")
             return
 
         headers = {
@@ -36,12 +37,8 @@ class CloudflareService:
             ]
         })
 
-        request = urllib2.Request(url, data, headers=headers)
-
         try:
-            response = urllib2.urlopen(request).read()
-
-            self.log.debug("Requested to purge Cloudflare cache for file %s" % path)
-            self.log.debug(response)
-        except urllib2.HTTPError as e:
-            self.log.debug("Unable to purge Cloudflare cache for file %s: %s" % (path, e.message))
+            log.debug("Requesting to purge Cloudflare cache for file %s" % path)
+            requests.get(url, data, verify=False, headers=headers, timeout=0.5)
+        except Exception as e:
+            log.warning("Unable to purge Cloudflare cache for file %s: %s" % (path, e.message))
