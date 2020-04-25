9f8d8e3307628afa6b44a6763307fc25ea8df870
Salvatore Iovene <salvatore@iovene.com>
2020-04-25 23:41:21 +0200

    (api) Fix i18n API to be able to get composite langs too (en-GB, pt-BR, etc)

--- astrobin_apps_json_api/i18n/urls.py
+++ astrobin_apps_json_api/i18n/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from astrobin_apps_json_api.i18n.views import I18nMessages
 
 urlpatterns = (
-    url(r'^i18n/messages/(?P<code>\w+)/$', I18nMessages.as_view()),
+    url(r'^i18n/messages/(?P<code>\w+(-\w+)?)/$', I18nMessages.as_view()),
 )
