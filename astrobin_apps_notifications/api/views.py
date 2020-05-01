24d11f8d88fd410262c4f88d75adab1571503857
Salvatore Iovene <salvatore@iovene.com>
2020-05-01 19:01:45 +0200

    (api/v2) Use django_filters backend and camelCase notification API

--- astrobin_apps_notifications/api/views.py
+++ astrobin_apps_notifications/api/views.py
@@ -1,6 +1,8 @@
+from djangorestframework_camel_case.render import CamelCaseJSONRenderer
 from persistent_messages.models import Message
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import list_route
+from rest_framework.renderers import BrowsableAPIRenderer
 from rest_framework.response import Response
 
 from astrobin_apps_notifications.api.serializers import NotificationSerializer
@@ -9,6 +11,7 @@ from astrobin_apps_notifications.api.serializers import NotificationSerializer
 class NotificationViewSet(viewsets.ModelViewSet):
     serializer_class = NotificationSerializer
     permission_classes = [permissions.IsAuthenticated]
+    renderer_classes = [BrowsableAPIRenderer, CamelCaseJSONRenderer]
 
     def get_queryset(self):
         return Message.objects.filter(user=self.request.user).order_by('-created')
