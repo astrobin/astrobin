66da3eec8fb9c339c155e3ee7541a5c4f425405d
Salvatore Iovene <salvatore@iovene.com>
2020-05-01 20:20:57 +0200

    (api/v2) Use django_filters backend and camelCase notification API (#922)

--- astrobin/settings/components/rest.py
+++ astrobin/settings/components/rest.py
@@ -1,7 +1,7 @@
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     'PAGE_SIZE': 50,
-    'FILTER_BACKEND': 'rest_framework.filters.DjangoFilterBackend',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication', # Useful for unit tests
