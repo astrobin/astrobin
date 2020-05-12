634cdf16548ffd3165337bbfd488fac667e0f65b
Salvatore Iovene <salvatore@iovene.com>
2020-05-12 08:47:34 +0200

    Release v1.67.0

--- astrobin/settings/__init__.py
+++ astrobin/settings/__init__.py
@@ -3,7 +3,7 @@ import os
 from split_settings.tools import optional, include
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-VERSION = "v1.66.1"
+VERSION = "v1.67.0"
 
 include(
     # Begin
