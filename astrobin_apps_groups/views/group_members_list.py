37372c3643839a707dd4302ed506cf50b9059f5c
Salvatore Iovene <salvatore@iovene.com>
2020-06-12 22:27:45 +0200

    (general) Fix pagination issue (#1224)
    Downgrade el-pagination back to version that supports Python 2 and
    fix API changes.
    
    Closes #1222

--- astrobin_apps_groups/views/group_members_list.py
+++ astrobin_apps_groups/views/group_members_list.py
@@ -8,3 +8,9 @@ from astrobin_apps_groups.views.mixins import RestrictPrivateGroupToMembersMixin
 class GroupMembersListView(LoginRequiredMixin, RestrictPrivateGroupToMembersMixin, DetailView):
     model = Group
     template_name = 'astrobin_apps_groups/group_members_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(GroupMembersListView, self).get_context_data(**kwargs)
+        group = self.get_object()
+        context['members'] = group.members.all().order_by('username')
+        return context
