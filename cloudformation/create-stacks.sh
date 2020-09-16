4f473ca11ce01d2a46aae849d08c558298855804
Salvatore Iovene <salvatore@iovene.com>
2020-09-16 21:29:45 +0200

    (pass certificate to create script)

--- cloudformation/create-stacks.sh
+++ cloudformation/create-stacks.sh
@@ -3,6 +3,7 @@
 REGION=$(cat ./region.txt)
 PREFIX=$(cat ./stack-prefix.txt)
 CF="aws --region ${REGION} cloudformation"
+CERTIFICATE_ARN=$1
 
 for STACK_FILE in $(find ./sequential -name '*.yml' | sort | xargs)
 do
@@ -25,10 +26,17 @@ done
 for STACK_FILE in $(find ./parallel -name '*.yml' | sort | xargs)
 do
     STACK_NAME=$(basename ${STACK_FILE%.*})
+    PARAMS=()
+
+    if [[ $STACK_NAME == "15-astrobin-app-service" ]]; then
+        PARAMS+=(--parameters ParameterKey=CertificateArnParameter,ParameterValue=${CERTIFICATE_ARN})
+    fi
+
     echo Crating stack ${PREFIX}-${STACK_NAME}...
     ${CF} create-stack \
         --stack-name ${PREFIX}-${STACK_NAME} \
         --template-body file://${PWD}/${STACK_FILE} \
+        "${PARAMS[@]}" \
         --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM >/dev/null
 done
 
