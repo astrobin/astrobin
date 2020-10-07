52de200e361612d45564f76996bb1b9b27191a7b
Salvatore Iovene <salvatore@iovene.com>
2020-10-07 14:26:27 +0200

    (order by django_ct)

--- astrobin/search.py
+++ astrobin/search.py
@@ -82,7 +82,7 @@ class AstroBinSearchForm(SearchForm):
             d = "i"
 
         if d == "i":
-            results = results.models(Image, EquipmentBrandListing)
+            results = results.models(Image, EquipmentBrandListing).order_by('-django_ct')
         elif d == "u":
             results = results.models(User)
         elif d == "cf":
