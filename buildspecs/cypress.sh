41b69ab7f9ba8504cb1537b12838bb26f9cfeab0
Salvatore Iovene <salvatore@iovene.com>
2020-10-13 23:51:19 +0200

    (snooze 5)

--- buildspecs/cypress.sh
+++ buildspecs/cypress.sh
@@ -4,4 +4,5 @@ export NGINX_MODE=dev
 export ASTROBIN_BUILD=${CODEBUILD_RESOLVED_SOURCE_VERSION}
 
 docker-compose -f docker/docker-compose.yml -f docker/docker-compose.build.yml up -d &
+sleep 300
 npm ci && CYPRESS_baseUrl=http://localhost $(npm bin)/cypress run
