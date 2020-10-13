a55456ea1b1a24b2c7a2cf0bb11ac9b58dcebbe9
Salvatore Iovene <salvatore@iovene.com>
2020-10-13 22:54:15 +0200

    wip

--- buildspecs/cypress.sh
+++ buildspecs/cypress.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export NGINX_MODE=dev
+export ASTROBIN_BUILD=${CODEBUILD_RESOLVED_SOURCE_VERSION}
+
 exec 3< <(docker-compose -f docker/docker-compose.yml -f docker/docker-compose.build.yml up) &&
 sed '/[INFO] Listening at/q' <&3 &&
 docker exec -t docker_astrobin_1 bash -c "npm ci && CYPRESS_baseUrl=http://localhost:8083 $(npm bin)/cypress run"
--- buildspecs/push-image.sh
+++ buildspecs/push-image.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker tag astrobin:$CODEBUILD_RESOLVED_SOURCE_VERSION $ECR_URL/astrobin:$CODEBUILD_RESOLVED_SOURCE_VERSION &&
+docker tag astrobin:$CODEBUILD_RESOLVED_SOURCE_VERSION $DOCKER_REGISTRY/astrobin:$CODEBUILD_RESOLVED_SOURCE_VERSION &&
 aws ecr get-login-password --region us-east-1 | docker login \
     --username AWS \
     --password-stdin \
-    $ECR_URL &&
-docker push $ECR_URL/astrobin:$CODEBUILD_RESOLVED_SOURCE_VERSION
+    $DOCKER_REGISTRY &&
+docker push $DOCKER_REGISTRY/astrobin:$CODEBUILD_RESOLVED_SOURCE_VERSION
