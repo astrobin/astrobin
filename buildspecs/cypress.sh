82e0a353a84146d00eba77815c2550927f305f98
Salvatore Iovene <salvatore@iovene.com>
2020-10-13 23:14:01 +0200

    docker-compose up is fast, no need to be fancy

--- buildspecs/cypress.sh
+++ buildspecs/cypress.sh
@@ -3,6 +3,5 @@
 export NGINX_MODE=dev
 export ASTROBIN_BUILD=${CODEBUILD_RESOLVED_SOURCE_VERSION}
 
-exec 3< <(docker-compose -f docker/docker-compose.yml -f docker/docker-compose.build.yml up) &&
-sed '/[INFO] Listening at/q' <&3 &&
+docker-compose -f docker/docker-compose.yml -f docker/docker-compose.build.yml up -d &
 npm ci && CYPRESS_baseUrl=http://localhost $(npm bin)/cypress run
