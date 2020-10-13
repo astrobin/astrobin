365d53e0e5a63bc5f40972ede4917bf75d83fdd6
Salvatore Iovene <salvatore@iovene.com>
2020-10-13 22:40:19 +0200

    wip

--- buildspecs/cypress.sh
+++ buildspecs/cypress.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-exec 3< <(docker-compose -f docker/docker-compose.yml -f docker/docker-compose.build.yml) &&
+exec 3< <(docker-compose -f docker/docker-compose.yml -f docker/docker-compose.build.yml up) &&
 sed '/[INFO] Listening at/q' <&3 &&
 docker exec -t docker_astrobin_1 bash -c "npm ci && CYPRESS_baseUrl=http://localhost:8083 $(npm bin)/cypress run"
