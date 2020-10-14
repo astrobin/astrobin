a92a7f90f3afa551c9eb42bf111abbf13399369b
Salvatore Iovene <salvatore@iovene.com>
2020-10-14 08:40:43 +0200

    try port 8083

--- buildspecs/cypress.sh
+++ buildspecs/cypress.sh
@@ -3,6 +3,7 @@
 export NGINX_MODE=dev
 export ASTROBIN_BUILD=${CODEBUILD_RESOLVED_SOURCE_VERSION}
 
+npm ci &
 docker-compose -f docker/docker-compose.yml -f docker/docker-compose.build.yml up -d &
-sleep 300
-npm ci && CYPRESS_baseUrl=http://localhost $(npm bin)/cypress run
+sleep 180
+$(npm bin)/cypress run
