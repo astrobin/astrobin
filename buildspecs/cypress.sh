78c465f70f0fb6a35baed4c9c9f07d6fbae6b937
Salvatore Iovene <salvatore@iovene.com>
2020-10-14 10:53:18 +0200

    (wait for 200)

--- buildspecs/cypress.sh
+++ buildspecs/cypress.sh
@@ -5,5 +5,6 @@ export ASTROBIN_BUILD=${CODEBUILD_RESOLVED_SOURCE_VERSION}
 
 npm ci &
 docker-compose -f docker/docker-compose.yml -f docker/docker-compose.build.yml up -d &
-sleep 300
-$(npm bin)/cypress run
+
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost/accounts/login/)" != "200" ]]; do sleep 5; done
+$(npm bin) test
