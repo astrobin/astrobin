670d8bf6ee43e293caa550ae6a90e752eaab3c61
Salvatore Iovene <salvatore@iovene.com>
2020-10-14 09:15:05 +0200

    sleep 300

--- buildspecs/cypress.sh
+++ buildspecs/cypress.sh
@@ -5,5 +5,5 @@ export ASTROBIN_BUILD=${CODEBUILD_RESOLVED_SOURCE_VERSION}
 
 npm ci &
 docker-compose -f docker/docker-compose.yml -f docker/docker-compose.build.yml up -d &
-sleep 180
+sleep 300
 $(npm bin)/cypress run
