466125954fd76b59deb5a6db43bbe37d7e995a60
Salvatore Iovene <salvatore@iovene.com>
2020-10-14 09:02:11 +0200

    ignore cypress exceptions

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,6 +1,6 @@
 describe("Login", () => {
     it("should display the page", () => {
-        cy.visit("/accounts/login");
+        cy.visit("/accounts/login/");
         cy.get("input[name='username']").should("exist");
         cy.get("input[name='password']").should("exist");
     });
