188ff1d2700e90d9960a553534c3fec3922bb26c
Salvatore Iovene <salvatore@iovene.com>
2020-10-23 13:58:06 +0200

    (add vars)

--- scripts/deployment/userdata-scheduler.sh
+++ scripts/deployment/userdata-scheduler.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
 
+export ASTROBIN_BUILD=${RELEASE_TAG}
+
 docker swarm init
 docker stack deploy -c docker/docker-compose-scheduler.yml scheduler
--- scripts/deployment/userdata-worker.sh
+++ scripts/deployment/userdata-worker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
 
+export ASTROBIN_BUILD=${RELEASE_TAG}
+
 docker swarm init
 docker stack deploy -c docker/docker-compose-worker.yml worker
