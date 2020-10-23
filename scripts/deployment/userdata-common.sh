3c7ab053877e4033f572a67bbead75d652cf2aa0
Salvatore Iovene <salvatore@iovene.com>
2020-10-23 11:48:06 +0200

    (infra) Add EC2 launch template user data script to the repository

---
+++ scripts/deployment/userdata-common.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -ex
+
+# Variables needed by this script:
+#  - AWS_ACCESS_KEY_ID
+#  - AWS_SECRET_ACCESS_KEY
+#  - AWS_REGION
+#  - RELEASE_TAG
+#  - DOCKER_REGISTRY
+#  - EFS_FILE_SYSTEM
+
+# Assumptions:
+#  - The astrobin repository is checked out at the root directory.
+#  - There is a file called /astrobin.env with the needed environment variables.
+
+export ASTROBIN_TEMPORARY_FILES=/astrobin-temporary-files
+export USER=ubuntu
+export GROUP=ubuntu
+export NGINX_MODE=prod
+export ASTROBIN_BUILD=$RELEASE_TAG
+
+# Get initial packages:
+
+apt-get update -y && apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common \
+    awscli \
+    binutils
+
+# Get docker:
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+apt-get install -y docker-compose
+usermod -aG docker ${USER}
+
+# Get docker image:
+
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+aws configure set aws_region ${AWS_REGION}
+rm /usr/bin/docker-credential-secretservice
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${DOCKER_REGISTRY}
+docker pull ${DOCKER_REGISTRY}/astrobin:${RELEASE_TAG}
+
+# Install efs-utils:
+
+git clone https://github.com/aws/efs-utils && (cd efs-utils && ./build-deb.sh && apt-get -y install ./build/amazon-efs-utils*deb)
+
+# Mount EFS:
+
+mkdir ${ASTROBIN_TEMPORARY_FILES}
+mount -t efs -o tls ${EFS_FILE_SYSTEM}:/ ${ASTROBIN_TEMPORARY_FILES}
+mkdir -p ${ASTROBIN_TEMPORARY_FILES}/files
+chown ${USER}:${USER} ${ASTROBIN_TEMPORARY_FILES}/files
+
+# Create docker stack:
+
+docker swarm init
+NODE_ID=$(docker node ls --format "{{.ID}}")
+docker node update --label-add default=true ${NODE_ID}
+docker node update --label-add app=true ${NODE_ID}
+cp /astrobin.env /astrobin/docker/astrobin.env
+docker stack deploy --compose-file docker/docker-compose.yml --compose-file docker/docker-compose.deploy.yml docker
