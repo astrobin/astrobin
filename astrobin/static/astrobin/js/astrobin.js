62b0eaccb3e88157b5c9a5e3f704079d081c1e5a
Salvatore Iovene <salvatore@iovene.com>
2020-11-15 17:25:21 +0100

    (text editor) Add support for pasting via button or context menu
    Closes #1621

--- astrobin/static/astrobin/js/astrobin.js
+++ astrobin/static/astrobin/js/astrobin.js
@@ -321,6 +321,17 @@ astrobin_common = {
                 on: {
                     change: function () {
                         this.updateElement();
+                    },
+                    beforeCommandExec: function(event) {
+                        // Show the paste dialog for the paste buttons and right-click paste
+                        if (event.data.name === "paste") {
+                            event.editor._.forcePasteDialog = true;
+                        }
+
+                        // Don't show the paste dialog for Ctrl+Shift+V
+                        if (event.data.name === "pastetext" && event.data.commandData.from === "keystrokeHandler") {
+                            event.cancel();
+                        }
                     }
                 }
             }
