30a2e64d789cbfafd9900b67dc844ad8a1d1152a
Salvatore Iovene <salvatore@iovene.com>
2020-11-23 22:34:10 +0100

    (images) Implement click & drag to zoom to 100% on full view (#1653)

--- astrobin_apps_json_api/user/views/__init__.py
+++ astrobin_apps_json_api/user/views/__init__.py
@@ -1,5 +1,6 @@
 from .confirm_image_recovery import ConfirmImageRecovery
 from .delete_images import DeleteImages
+from .mark_click_and_drag_toast_as_seen import MarkClickAndDragToastAsSeen
 from .mark_corrupted_images_banner_as_seen import MarkCorruptedImagesBannerAsSeen
 from .plate_solution_overlay_on_full_disabled import PlateSolutionOverlayOnFullDisabled
 from .restore_deleted_images import RestoreDeletedImages
