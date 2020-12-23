04cfe696a37ecbdbabc181938bda3cd02cfc5385
Hunter Harling <hunterharling@gmail.com>
2020-12-22 22:31:57 -0800

    generate random username

--- cypress/support/commands/register.js
+++ cypress/support/commands/register.js
@@ -3,15 +3,17 @@ Cypress.Commands.add('register', (opts={}) => {
 
     cy.acceptCookies();
 
-    cy.get(".form-horizontal input[name='username']").type(opts.username || "astrobin_dev1");
-    cy.get(".form-horizontal input[name='email']").type(opts.email || "dev1@astrobin.com");
-    cy.get(".form-horizontal input[name='password1']").type(opts.password || "astrobin_dev1");
-    cy.get(".form-horizontal input[name='password2']").type(opts.password || "astrobin_dev1");
+    let rand_id = Math.random().toString(36).substring(7);
+
+    cy.get(".form-horizontal input[name='username']").type(opts.username || `astrobin_dev_${rand_id}`);
+    cy.get(".form-horizontal input[name='email']").type(opts.email || `dev_${rand_id}@astrobin.com`);
+    cy.get(".form-horizontal input[name='password1']").type(opts.password || `astrobin_dev_${rand_id}`);
+    cy.get(".form-horizontal input[name='password2']").type(opts.password || `astrobin_dev_${rand_id}`);
     cy.get("#uniform-id_tos").click();
 
     cy.bypassReCAPTCHA();
     cy.wait(1500);
-    
+
     cy.get(".form-horizontal button").click();
 
     cy.url().should("contain", "/accounts/register/complete/");
