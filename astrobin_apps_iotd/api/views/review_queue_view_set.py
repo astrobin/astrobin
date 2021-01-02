9390dab5797441879c5310193a5eb9c1715eca11
Salvatore Iovene <salvatore@iovene.com>
2021-01-02 14:31:11 +0100

    (use right serializer)

--- astrobin_apps_iotd/api/views/review_queue_view_set.py
+++ astrobin_apps_iotd/api/views/review_queue_view_set.py
@@ -5,13 +5,13 @@ from djangorestframework_camel_case.render import CamelCaseJSONRenderer
 from rest_framework import viewsets
 from rest_framework.renderers import BrowsableAPIRenderer
 
-from astrobin_apps_iotd.api.serializers.submission_queue_serializer import SubmissionQueueSerializer
+from astrobin_apps_iotd.api.serializers.review_queue_serializer import ReviewQueueSerializer
 from astrobin_apps_iotd.services import IotdService
 from common.permissions import ReadOnly
 
 
 class ReviewQueueViewSet(viewsets.ModelViewSet):
-    serializer_class = SubmissionQueueSerializer
+    serializer_class = ReviewQueueSerializer
     renderer_classes = [BrowsableAPIRenderer, CamelCaseJSONRenderer]
     permission_classes = [ReadOnly]
     http_method_names = ['get', 'head']
