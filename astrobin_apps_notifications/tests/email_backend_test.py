c6f71f69435b5619dca92df6dfb2b17e7b2f8089
Salvatore Iovene <salvatore@iovene.com>
2021-01-12 22:35:09 +0100

    (emails) If there's a successful delivery after a bounce, keep sending emails to that address
    Closes #1817

--- astrobin_apps_notifications/tests/email_backend_test.py
+++ astrobin_apps_notifications/tests/email_backend_test.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.contrib.auth.models import User
 from django.test import TransactionTestCase
-from django_bouncy.models import Bounce, Complaint
+from django_bouncy.models import Bounce, Complaint, Delivery
 from notification.models import NoticeType
 
 from astrobin_apps_notifications.backends import EmailBackend
@@ -28,6 +28,22 @@ class EmailBackendTest(TransactionTestCase):
         )
 
         self.assertFalse(EmailBackend(1).can_send(self.user, self.notice_type))
+
+    def test_can_send_with_hard_bounce_followed_by_delivery(self):
+        Bounce.objects.create(
+            hard=True,
+            bounce_type="Permanent",
+            address=self.user.email,
+            mail_timestamp=datetime.datetime.now() - datetime.timedelta(days=2)
+        )
+
+        Delivery.objects.create(
+            address=self.user.email,
+            mail_timestamp=datetime.datetime.now() - datetime.timedelta(days=1),
+            delivered_time=datetime.datetime.now() - datetime.timedelta(days=1)
+        )
+
+        self.assertTrue(EmailBackend(1).can_send(self.user, self.notice_type))
 
     def test_can_send_with_two_soft_bounces(self):
         Bounce.objects.create(
@@ -69,6 +85,37 @@ class EmailBackendTest(TransactionTestCase):
         )
 
         self.assertFalse(EmailBackend(1).can_send(self.user, self.notice_type))
+
+
+    def test_can_send_with_three_soft_bounces_but_one_of_them_is_after_a_delivery(self):
+        Bounce.objects.create(
+            hard=False,
+            bounce_type="Transient",
+            address=self.user.email,
+            mail_timestamp=datetime.datetime.now() - datetime.timedelta(days=4)
+        )
+
+        Bounce.objects.create(
+            hard=False,
+            bounce_type="Transient",
+            address=self.user.email,
+            mail_timestamp=datetime.datetime.now() - datetime.timedelta(days=3)
+        )
+
+        Delivery.objects.create(
+            address=self.user.email,
+            mail_timestamp=datetime.datetime.now() - datetime.timedelta(days=2),
+            delivered_time=datetime.datetime.now() - datetime.timedelta(days=2)
+        )
+
+        Bounce.objects.create(
+            hard=False,
+            bounce_type="Transient",
+            address=self.user.email,
+            mail_timestamp=datetime.datetime.now() - datetime.timedelta(days=1)
+        )
+
+        self.assertTrue(EmailBackend(1).can_send(self.user, self.notice_type))
 
     def test_can_send_with_complaint(self):
         Complaint.objects.create(
