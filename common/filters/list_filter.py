5ff9e120fda06a036936cb4683665b529a7f2efd
Salvatore Iovene <salvatore@iovene.com>
2021-02-01 22:01:04 +0100

    (api/v2) Allow image search by list of hashes

--- common/filters/list_filter.py
+++ common/filters/list_filter.py
@@ -4,6 +4,6 @@ import django_filters
 class ListFilter(django_filters.Filter):
     def filter(self, qs, value):
         if value not in (None, ''):
-            integers = [int(v) for v in value.split(',')]
-            return qs.filter(**{'%s__%s' % (self.name, self.lookup_expr): integers})
+            values = [v for v in value.split(',')]
+            return qs.filter(**{'%s__%s' % (self.name, self.lookup_expr): values})
         return qs
