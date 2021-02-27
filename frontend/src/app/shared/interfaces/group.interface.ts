07e9e2baa933739be2da1ee205f70dd5fe3674da
Salvatore Iovene <salvatore@iovene.com>
2021-02-27 22:07:38 +0100

    (infra) Move frontend Angular app into main repo, for ease of operations

--- frontend/src/app/shared/interfaces/group.interface.ts
+++
@@ -1,7 +0,0 @@
-import { PermissionInterface } from "@shared/interfaces/permission.interface";
-
-export interface GroupInterface {
-  id: number;
-  name: string;
-  permissions?: PermissionInterface[];
-}
