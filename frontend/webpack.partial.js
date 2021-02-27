7747778447bda863dd25487cf3280b10db136321
Salvatore Iovene <salvatore@iovene.com>
2021-02-27 22:46:16 +0100

    webpack ignore node_modules

--- frontend/webpack.partial.js
+++ frontend/webpack.partial.js
@@ -1,6 +1,9 @@
 const webpack = require("webpack");
 
 module.exports = {
+  watchOptions: {
+    ignored: /node_modules/
+  },
   plugins: [
     new webpack.DefinePlugin({
       VERSION: new Date().getTime()
