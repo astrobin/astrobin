e8bd198685aec3a7a98e36215e7acfebd67f25ff
Salvatore Iovene <salvatore@iovene.com>
2021-02-28 22:29:45 +0100

    (checkout astrobin-ng and run it)

--- buildspecs/cypress.sh
+++ buildspecs/cypress.sh
@@ -12,8 +12,20 @@ docker-compose \
    -f docker/docker-compose-local.yml \
    up -d &
 
+(
+    git clone https://github.com/astrobin/astrobin-ng.git &&
+    cd astrobin-ng &&
+    npm ci &&
+    npm start
+) &
+
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost/accounts/login/)" != "200" ]]; do
-    echo "Waiting for AstroBin..."
+    echo "Waiting for astrobin..."
+    sleep 5
+done
+
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:4400/accounts/login/)" != "200" ]]; do
+    echo "Waiting for astrobin-ng..."
     sleep 5
 done
 
