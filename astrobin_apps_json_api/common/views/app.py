487620e73ae5dc4c0af6c8349f15422c6f410432
Salvatore Iovene <salvatore@iovene.com>
2021-03-01 17:41:41 +0100

    (change revision upload endpoint)

--- astrobin_apps_json_api/common/views/app.py
+++ astrobin_apps_json_api/common/views/app.py
@@ -42,7 +42,7 @@ class AppConfig(JSONResponseMixin, View):
             u'IOTD_REVIEW_MAX_PER_DAY': settings.IOTD_REVIEW_MAX_PER_DAY,
             u'IOTD_QUEUES_PAGE_SIZE': settings.IOTD_QUEUES_PAGE_SIZE,
             u'IMAGE_UPLOAD_ENDPOINT': '/api/v2/images/image-upload/',
-            u'IMAGE_REVISION_UPLOAD_ENDPOINT': '/api/v2/images/image-revision/',
+            u'IMAGE_REVISION_UPLOAD_ENDPOINT': '/api/v2/images/image-revision-upload/',
         })
 
     def __get_i18n_hash__(self):
