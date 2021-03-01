339a28a97c5dc88ed8b8f13cccacd69efe4f46e6
Salvatore Iovene <salvatore@iovene.com>
2021-03-01 11:47:19 +0100

    (fix starting angular app)

--- buildspecs/cypress.sh
+++ buildspecs/cypress.sh
@@ -16,7 +16,7 @@ docker-compose \
     git clone https://github.com/astrobin/astrobin-ng.git &&
     cd astrobin-ng &&
     npm ci &&
-    npm start:cypress
+    npm run serve:cypress
 ) &
 
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost/accounts/login/)" != "200" ]]; do
