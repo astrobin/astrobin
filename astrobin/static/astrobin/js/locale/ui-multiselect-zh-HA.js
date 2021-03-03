1db117e52f5225e47f96345980a335b6fda2570a
Salvatore Iovene <salvatore@iovene.com>
2021-03-03 09:21:57 +0100

    (add some frontend zh-hans files)

---
+++ astrobin/static/astrobin/js/locale/ui-multiselect-zh-HA.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale zh
+ */
+
+$.extend($.ui.multiselect.locale, {
+    addAll:'加入所有',
+    removeAll:'移除所有',
+    itemsCount:'项目数'
+});
