13a6149fa44ec389de50050d37c62e20bb6abd23
Salvatore Iovene <salvatore@iovene.com>
2021-03-25 00:45:13 +0100

    (images) Add buttons to search around the same field
    Closes #2094

--- astrobin_apps_platesolving/services/solution_service.py
+++ astrobin_apps_platesolving/services/solution_service.py
@@ -1,30 +1,12 @@
 from django.db.models import QuerySet
+from django.urls import reverse
 
 from astrobin.models import ImageRevision, Image
 from astrobin_apps_platesolving.models import Solution, PlateSolvingAdvancedSettings
 
 
-class SolutionService:    
-    def __init__(self, solution):
-        # type: (Solution) -> None
-        
-        self.solution = solution
-        
-    def get_objects_in_field(self):
-        # type: () -> List[str]
-
-        objects = []
-        if self.solution.objects_in_field:
-            objects = [x.strip() for x in self.solution.objects_in_field.split(',')]
-        if self.solution.advanced_annotations:
-            advanced_annotations_lines = self.solution.advanced_annotations.split('\n')
-            for line in advanced_annotations_lines:
-                advanced_annotation = line.split(',')[-1]
-                header = line.split(',')[0]
-                if header == "Label" and advanced_annotation not in objects and advanced_annotation != '':
-                    objects.append(advanced_annotation)
-                    
-        return sorted(objects)
+class SolutionService:
+    solution = None  # type: Solution
 
     @staticmethod
     def get_or_create_advanced_settings(target):
@@ -46,3 +28,46 @@ class SolutionService:
             return latest_settings, False
 
         return PlateSolvingAdvancedSettings.objects.create(), True
+
+    def __init__(self, solution):
+        # type: (Solution) -> None
+
+        self.solution = solution
+
+    def get_objects_in_field(self):
+        # type: () -> List[str]
+
+        objects = []
+        if self.solution.objects_in_field:
+            objects = [x.strip() for x in self.solution.objects_in_field.split(',')]
+        if self.solution.advanced_annotations:
+            advanced_annotations_lines = self.solution.advanced_annotations.split('\n')
+            for line in advanced_annotations_lines:
+                advanced_annotation = line.split(',')[-1]
+                header = line.split(',')[0]
+                if header == "Label" and advanced_annotation not in objects and advanced_annotation != '':
+                    objects.append(advanced_annotation)
+
+        return sorted(objects)
+
+    def get_search_query_around(self, degrees):
+        # type: (int) -> str
+        ra = float(self.solution.advanced_ra or self.solution.ra)
+        dec = float(self.solution.advanced_dec or self.solution.dec)
+
+        ra_min = ra - degrees * .5
+        ra_max = ra + degrees * .5
+
+        dec_min = dec - degrees * .5
+        dec_max = dec + degrees * .5
+
+        field_min = 0
+        field_max = degrees
+
+        base_search_url = reverse('haystack_search')
+
+        return base_search_url + \
+               "?q=&d=i&t=all" + \
+               "&coord_ra_min=%.2f&coord_ra_max=%.2f" % (ra_min, ra_max) + \
+               "&coord_dec_min=%.2f&coord_dec_max=%.2f" % (dec_min, dec_max) + \
+               "&field_radius_min=%.2f&field_radius_max=%.2f" % (field_min, field_max)
