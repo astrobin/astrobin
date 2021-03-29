635e699a39d08300e4ae81137361b1c94247cae4
Salvatore Iovene <salvatore@iovene.com>
2021-03-29 10:58:20 +0200

    (notifications) Add logging for issues with sending new image/revision notifications

--- astrobin_apps_notifications/tasks.py
+++ astrobin_apps_notifications/tasks.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import timedelta
 
 from celery import shared_task
@@ -13,6 +14,8 @@ from common.services import DateTimeService
 from toggleproperties.models import ToggleProperty
 
 
+logger = logging.getLogger('apps')
+
 @shared_task(time_limit=300)
 def purge_old_notifications():
     call_command("purge_old_notifications")
@@ -23,9 +26,11 @@ def push_notification_for_new_image(user_pk, image_pk):
     try:
         image = Image.objects_including_wip.get(pk=image_pk)
     except Image.DoesNotExist:
+        logger.error('push_notification_for_new_image called for image not found: %d' % image_pk)
         return
 
     if image.is_wip:
+        logger.error('push_notification_for_new_image called for image that is wip: %d' % image_pk)
         return
 
     followers = [
@@ -41,6 +46,10 @@ def push_notification_for_new_image(user_pk, image_pk):
             'image': image,
             'image_thumbnail': thumb.url if thumb else None
         })
+    else:
+        logger.error('push_notification_for_new_image called for image %d whose author %d has no followes' % (
+            image_pk, user_pk)
+        )
 
 
 @shared_task(time_limit=1200)
@@ -48,9 +57,11 @@ def push_notification_for_new_image_revision(revision_pk):
     try:
         revision = ImageRevision.objects.get(pk=revision_pk)
     except ImageRevision.DoesNotExist:
+        logger.error('push_notification_for_new_image called for revision not found: %d' % revision_pk)
         return
 
     if revision.skip_notifications or revision.image.is_wip:
+        logger.error('push_notification_for_new_image called for revision of image that is wip: %d' % revision_pk)
         return
 
     followers = [x.user for x in ToggleProperty.objects.filter(
@@ -58,10 +69,15 @@ def push_notification_for_new_image_revision(revision_pk):
         content_type=ContentType.objects.get_for_model(User),
         object_id=revision.image.user.pk)]
 
-    push_notification(followers, revision.image.user, 'new_image_revision', {
-        'object_url': build_notification_url(settings.BASE_URL + revision.get_absolute_url(), revision.image.user),
-        'originator': revision.image.user.userprofile.get_display_name(),
-    })
+    if len(followers) > 0:
+        push_notification(followers, revision.image.user, 'new_image_revision', {
+            'object_url': build_notification_url(settings.BASE_URL + revision.get_absolute_url(), revision.image.user),
+            'originator': revision.image.user.userprofile.get_display_name(),
+        })
+    else:
+        logger.error(
+            'push_notification_for_new_image called for revision %d whose author has no followers' % revision_pk
+        )
 
 
 @shared_task(time_limit=300)
