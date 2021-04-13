eb739f72638a5b4ff20a28424dba155483f251a8
Salvatore Iovene <salvatore@iovene.com>
2021-04-13 15:15:46 +0200

    (api/v2/iotd) Restrict queues to authenticated users (#2166)

--- astrobin_apps_iotd/api/views/review_queue_view_set.py
+++ astrobin_apps_iotd/api/views/review_queue_view_set.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from djangorestframework_camel_case.render import CamelCaseJSONRenderer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import BrowsableAPIRenderer
 
 from astrobin_apps_iotd.api.queue_pagination import QueuePagination
@@ -15,7 +16,7 @@ class ReviewQueueViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewQueueSerializer
     renderer_classes = [BrowsableAPIRenderer, CamelCaseJSONRenderer]
     pagination_class = QueuePagination
-    permission_classes = [ReadOnly]
+    permission_classes = [IsAuthenticated, ReadOnly]
     http_method_names = ['get', 'head']
 
     def get_queryset(self):
