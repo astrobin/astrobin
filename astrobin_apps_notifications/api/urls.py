95355452c0d0360c5c87982832b4eef8b12ee278
Salvatore Iovene <salvatore@iovene.com>
2021-05-24 19:18:47 +0200

    (api/v2) Add API to get notification types, and to get/set notification settings (#2300)

--- astrobin_apps_notifications/api/urls.py
+++ astrobin_apps_notifications/api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 
-from astrobin_apps_notifications.api.views import NotificationViewSet
+from astrobin_apps_notifications.api.views import NotificationViewSet, NoticeSettingViewSet, NoticeTypeViewSet
 
 router = routers.DefaultRouter()
 router.register(r'notification', NotificationViewSet, base_name='notification')
+router.register(r'type', NoticeTypeViewSet, base_name='type')
+router.register(r'setting', NoticeSettingViewSet, base_name='setting')
 
 urlpatterns = [
     url('', include(router.urls)),
