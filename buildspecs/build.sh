1786b39d590e630fcb2e7cd7363d0e6ab06fb027
Salvatore Iovene <salvatore@iovene.com>
2021-05-26 10:40:44 +0200

    (infra) Bring nginx build into the CI/CD system

--- buildspecs/build.sh
+++ buildspecs/build.sh
@@ -4,9 +4,22 @@ docker login --username ${DOCKER_USERNAME} --password ${DOCKER_PASSWORD} || exit
 
 docker pull ubuntu:16.04 || exit 1
 
+# Build nginx
+
+docker build \
+    -t astrobin-nginx:$CODEBUILD_RESOLVED_SOURCE_VERSION \
+    -t $DOCKER_REGISTRY/astrobin-nginx:$CODEBUILD_RESOLVED_SOURCE_VERSION \
+    --build-arg ENV=prod \
+    -f docker/nginx.prod.dockerfile . || exit 1
+
+docker tag astrobin-nginx:$CODEBUILD_RESOLVED_SOURCE_VERSION $DOCKER_REGISTRY/astrobin-nginx:$CODEBUILD_RESOLVED_SOURCE_VERSION || exit 1
+
+# Build AstroBin
+
 docker build \
     -t astrobin:$CODEBUILD_RESOLVED_SOURCE_VERSION \
     -t $DOCKER_REGISTRY/astrobin:$CODEBUILD_RESOLVED_SOURCE_VERSION \
     -f docker/astrobin.dockerfile . || exit 1
 
 docker tag astrobin:$CODEBUILD_RESOLVED_SOURCE_VERSION $DOCKER_REGISTRY/astrobin:$CODEBUILD_RESOLVED_SOURCE_VERSION || exit 1
+
--- buildspecs/push-image.sh
+++ buildspecs/push-image.sh
@@ -4,4 +4,5 @@ aws ecr get-login-password --region us-east-1 | docker login \
     --username AWS \
     --password-stdin \
     $DOCKER_REGISTRY &&
+docker push $DOCKER_REGISTRY/astrobin-nginx:$CODEBUILD_RESOLVED_SOURCE_VERSION &&
 docker push $DOCKER_REGISTRY/astrobin:$CODEBUILD_RESOLVED_SOURCE_VERSION
