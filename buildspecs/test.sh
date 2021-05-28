702e23714d32cc8c03098bddc173953de8cdfe6e
Salvatore Iovene <salvatore@iovene.com>
2021-05-28 12:17:20 +0200

    (infra) Include docker registry in test script

--- buildspecs/test.sh
+++ buildspecs/test.sh
@@ -2,7 +2,7 @@
 
 export ARCH=$(uname -m)
 
-docker run astrobin-${ARCH}:$CODEBUILD_RESOLVED_SOURCE_VERSION bash -c "\
+docker run $DOCKER_REGISTRY/astrobin-${ARCH}:$CODEBUILD_RESOLVED_SOURCE_VERSION bash -c "\
     ./scripts/test.sh && \
     coverage xml && \
     curl -s https://codecov.io/bash | bash -s - -C $CODEBUILD_RESOLVED_SOURCE_VERSION -t ${CODECOV_TOKEN}"
