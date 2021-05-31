f0d9a30c42fc6ceb03818788d6032a40c93fd079
Salvatore Iovene <salvatore@iovene.com>
2021-05-31 09:47:15 +0200

    (images) Remove limitation on max number of fields, allowing for arbitrarily large acquisition sets (#2321)

--- astrobin/settings/components/basic.py
+++ astrobin/settings/components/basic.py
@@ -28,6 +28,7 @@ LONGPOLL_ENABLED = False
 
 ALLOWED_HOSTS = ['*']
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
+DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 
 ADS_ENABLED = os.environ.get('ADS_ENABLED', 'false').strip() == 'true'
 ADSENSE_ENABLED = os.environ.get('ADSENSE_ENABLED', 'false').strip() == 'true'
