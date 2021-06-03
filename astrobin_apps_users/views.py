b62ae99ee3f36f9733abc38ee6ab521699652f72
Salvatore Iovene <salvatore@iovene.com>
2021-06-03 16:21:00 +0200

    (performance) Fix performance issue of the "these people like this" modal

--- astrobin_apps_users/views.py
+++ astrobin_apps_users/views.py
@@ -1,38 +1,33 @@
-from braces.views import JSONResponseMixin, LoginRequiredMixin
+import json
+
+from braces.views import JSONResponseMixin, LoginRequiredMixin, JsonRequestResponseMixin
 from django.contrib import messages
 from django.contrib.contenttypes.models import ContentType
+from django.core.serializers.json import DjangoJSONEncoder
 from django.core.urlresolvers import reverse
 from django.db.models import Q
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.utils.translation import gettext
 from django.views import View
 from django.views.generic import base
 from django_bouncy.models import Bounce, Complaint
 
+from astrobin.models import UserProfile
 from toggleproperties.models import ToggleProperty
 
-from astrobin.models import UserProfile
 
-
-class TogglePropertyUsersAjaxView(base.View):
+class TogglePropertyUsersAjaxView(JsonRequestResponseMixin, base.View):
     def get(self, request, *args, **kwargs):
         property_type = kwargs.pop('property_type')
         object_id = kwargs.pop('object_id')
         content_type = ContentType.objects.get_for_id(kwargs.pop('content_type_id'))
 
-        users = [x.user for x in ToggleProperty.objects.filter(
-            property_type = property_type,
-            object_id = object_id,
-            content_type = content_type)]
+        likes = ToggleProperty.objects.filter(
+            property_type=property_type,
+            object_id=object_id,
+            content_type=content_type).values_list('user__username', 'user__userprofile__real_name', 'created_on')
 
-        context = {
-            'user_list': users,
-            'view': 'table',
-            'layout': 'compact',
-        }
-
-        return render(request, 'astrobin_apps_users/inclusion_tags/user_list.html', context)
+        return self.render_json_response(json.dumps(list(likes), cls=DjangoJSONEncoder))
 
 
 class UserSearchView(JSONResponseMixin, base.View):
@@ -42,9 +37,9 @@ class UserSearchView(JSONResponseMixin, base.View):
         term = request.GET.get('term')
 
         profiles = UserProfile.objects.filter(
-            Q(user__first_name__icontains=term)|
-            Q(user__last_name__icontains=term)|
-            Q(user__username__icontains=term)|
+            Q(user__first_name__icontains=term) |
+            Q(user__last_name__icontains=term) |
+            Q(user__username__icontains=term) |
             Q(real_name__icontains=term))
 
         if request.is_ajax():
@@ -54,7 +49,7 @@ class UserSearchView(JSONResponseMixin, base.View):
                     'id': profile.user.pk,
                     'username': profile.user.username,
                     'display_name': profile.get_display_name(),
-                    'url': reverse('user_page', args = (profile.user.username,)),
+                    'url': reverse('user_page', args=(profile.user.username,)),
                 })
 
             return self.render_json_response(matches)
