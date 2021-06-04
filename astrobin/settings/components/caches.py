44ad21c35795d3e3665d3647266af8a1b456ca3b
Salvatore Iovene <salvatore@iovene.com>
2021-06-04 23:07:23 +0200

    (infra) Add CACHE_TYPE environment variable to control which cache is used

--- astrobin/settings/components/caches.py
+++ astrobin/settings/components/caches.py
@@ -1,10 +1,25 @@
 import os
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '%s:11211' % os.environ.get('MEMCACHED_HOST', 'memcached').strip(),
-    },
-}
+CACHE_TYPE = os.environ.get('CACHE_TYPE', 'memcached').strip()
+
+if CACHE_TYPE == 'memcached':
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '%s:11211' % os.environ.get('MEMCACHED_HOST', 'memcached').strip(),
+        },
+    }
+elif CACHE_TYPE == 'locmem':
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        }
+    }
+else:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
+    }
 
 CACHALOT_ENABLED = os.environ.get('CACHALOT_ENABLED', 'false').strip() == 'true'
