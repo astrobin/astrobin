9701e9ed04bc16acbd520844456d6152ffa4272e
Salvatore Iovene <salvatore@iovene.com>
2021-06-10 11:18:10 +0200

    (plate-solving) Fix off-image annotations and add HD catalog support (#2360)

--- astrobin_apps_platesolving/annotate.py
+++ astrobin_apps_platesolving/annotate.py
@@ -59,7 +59,14 @@ class Annotator:
         def computeTextCoordinates(annotation):
             x, y = annotation['pixelx'], annotation['pixely']
             w, h = computeTextSize(annotation)
-            return int(round(x - w / 2)), int(round(y - annotation['radius'] - h * 2))
+
+            coordinates = int(round(x - w / 2)), int(round(y - annotation['radius'] - h * 2))
+
+            # If the object's radius extends beyond the image, the text would be outside of the image's bound.
+            if coordinates[1] < 0:
+                coordinates = int(round(x - w / 2)), int(round(y - h * 2))
+
+            return coordinates
 
         def computeTextBoundingBox(annotation):
             x, y = computeTextCoordinates(annotation)
@@ -74,6 +81,25 @@ class Annotator:
 
             return box
 
+        def computeColors(annotation, shape):
+            semi_opaque_types = ['hd']
+
+            white = (255, 255, 255)
+            black = (0, 0, 0)
+            semi_opaque_white = (255, 255, 255, 128)
+            semi_opaque_black = (0, 0, 0, 128)
+
+            if shape in ('circle', 'rectangle'):
+                # Shape with a fill are half as opaque.
+                white = (255, 255, 255, 128)
+                black = (0, 0, 0, 128)
+                semi_opaque_white = (255, 255, 255, 64)
+                semi_opaque_black = (0, 0, 0, 64)
+
+            if annotation['type'] in semi_opaque_types:
+                return semi_opaque_white, semi_opaque_black
+            return white, black
+
         # Unused for now
         def collisionDetected(annotation, others):
             box = computeTextBoundingBox(annotation)
@@ -87,7 +113,7 @@ class Annotator:
 
             return False
 
-        supported_types = ['m', 'ic', 'ngc', 'ugc', 'abel', 'bright']
+        supported_types = ['m', 'ic', 'ngc', 'ugc', 'abel', 'bright', 'hd']
         annotations.sort(key=lambda x: x['radius'])
         for annotation in annotations:
             if annotation['type'] in supported_types:
@@ -95,41 +121,39 @@ class Annotator:
                 y = annotation['pixely'] = annotation['pixely'] * self.resampling_factor
                 radius = annotation['radius'] = \
                     annotation['radius'] * self.resampling_factor + 10 * self.resampling_factor
-                white = (255, 255, 255)
-                black = (0, 0, 0)
 
                 font = getAnnotationFont(annotation)
                 text = getAnnotationText(annotation)
                 text_x, text_y = computeTextCoordinates(annotation)
-                text_w, text_h = computeTextSize(annotation)
                 box = computeTextBoundingBox(annotation)
 
                 for i in range(0, self.line_thickness):
                     # Circle
                     draw.ellipse(
                         [x - radius + 1 + i, y - radius + 1 + i, x + radius - 1 - i, y + radius - 1 - i],
-                        outline=black)
+                        outline=computeColors(annotation, 'circle')[1])
                     draw.ellipse(
                         [x - radius - i, y - radius - i, x + radius + i, y + radius + i],
-                        outline=white)
+                        outline=computeColors(annotation, 'circle')[0])
 
                     # Line to text
-                    draw.line(
-                        [x + 1 + i, y - radius, x + 1 + i, box['y'] + box['h']],
-                        black)
-                    draw.line(
-                        [x + i, y - radius, x + i, box['y'] + box['h']],
-                        white)
+                    if x > 0 and y - radius > 0 and box['y'] + box['h'] > 0:
+                        draw.line(
+                            [x + 1 + i, y - radius, x + 1 + i, box['y'] + box['h']],
+                            computeColors(annotation, 'line')[1])
+                        draw.line(
+                            [x + i, y - radius, x + i, box['y'] + box['h']],
+                            computeColors(annotation, 'line')[0])
 
                 # Text box
                 draw.rectangle(
                     [box['x'], box['y'], box['x'] + box['w'], box['y'] + box['h']],
-                    outline=(255, 255, 255, 128),
-                    fill=(0, 0, 0, 128))
+                    outline=computeColors(annotation, 'rectangle')[0],
+                    fill=computeColors(annotation, 'rectangle')[1])
 
                 # Text
-                draw.text((text_x + 1, text_y + 1), text, black, font)
-                draw.text((text_x, text_y), text, white, font)
+                draw.text((text_x + 1, text_y + 1), text, computeColors(annotation, 'text')[1], font)
+                draw.text((text_x, text_y), text, computeColors(annotation, 'text')[0], font)
 
     def annotate(self):
         if self.solution.annotations is None:
