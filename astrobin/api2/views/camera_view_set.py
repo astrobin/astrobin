05638dd7bb38e5a42cb989a3331af6aae43a911b
Salvatore Iovene <salvatore@iovene.com>
2021-06-23 22:08:04 +0200

    (add API to get non-migrated camera)

--- astrobin/api2/views/camera_view_set.py
+++ astrobin/api2/views/camera_view_set.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from djangorestframework_camel_case.render import CamelCaseJSONRenderer
 from rest_framework import viewsets
+from rest_framework.decorators import list_route
 from rest_framework.renderers import BrowsableAPIRenderer
+from rest_framework.response import Response
 
 from astrobin.api2.serializers.camera_serializer import CameraSerializer
 from astrobin.models import Camera
@@ -18,3 +20,9 @@ class CameraViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Camera.objects.all()
+
+    @list_route(url_path='random-non-migrated')
+    def random_non_migrated(self, request):
+        queryset = self.get_queryset().filter(migration_flag__isnull=True).order_by('?')[:1]
+        serializer = CameraSerializer(queryset, many=True, context=self.get_serializer_context())
+        return Response(serializer.data)
