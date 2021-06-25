eb331fb68ae29ae055e5a26446fbe89256d3186a
Salvatore Iovene <salvatore@iovene.com>
2021-06-25 18:41:58 +0200

    (forum) Fix display of quoted users and send mention notification

--- common/services/mentions_service.py
+++ common/services/mentions_service.py
@@ -5,12 +5,13 @@ class MentionsService(object):
     @staticmethod
     def get_mentions(text):
         # type: (unicode) -> list[unicode]
-        regex = r'\[url=.*?\/users\/(.*?)\/\]@(.*?)\[\/url\]'
+        regex = r'\[url=.*?\/users\/(.*?)\/\]@.*?\[\/url\]|\[quote="(.*?)"\].*?\[\/quote\]'
         matches = re.finditer(regex, text, re.MULTILINE)
         mentions = []
 
         for matchNum, match in enumerate(matches, start=1):
-            mention = match.groups(1)[0]
-            mentions.append(mention)
+            for group in match.groups():
+                if group is not None:
+                    mentions.append(group)
 
         return list(set(mentions))
