12fe53eee7461da877ff7ebfe49df816172d854c
Salvatore Iovene <salvatore@iovene.com>
2021-06-27 21:28:21 +0200

    (forums) Use unidecode instead of smart_str to deal with unicode in quote blocks

--- astrobin/bbcode_tags.py
+++ astrobin/bbcode_tags.py
@@ -1,8 +1,8 @@
 from annoying.functions import get_object_or_None
 from django.core.exceptions import MultipleObjectsReturned
-from django.utils.encoding import smart_str
 from precise_bbcode.bbcode.tag import BBCodeTag
 from precise_bbcode.tag_pool import tag_pool
+from unidecode import unidecode
 
 from astrobin.models import UserProfile
 
@@ -14,27 +14,27 @@ class QuoteBBCodeTag(BBCodeTag):
         strip = True
 
     def render(self, value, option=None, parent=None):
-        content = smart_str(value)
+        content = unidecode(value)
 
         if option:
-            username = smart_str(option).replace('"', '')
+            username = unidecode(option).replace('"', '')
             profile = get_object_or_None(UserProfile, user__username=username)
             if not profile:
                 try:
                     profile = get_object_or_None(UserProfile, real_name=username)
                 except MultipleObjectsReturned:
-                    return '<blockquote><em>{}</em>:<br/>{}</blockquote>'.format(
+                    return u'<blockquote><em>{}</em>:<br/>{}</blockquote>'.format(
                         username,
                         content
                     )
             if profile:
-                return '<blockquote><a href="/users/{}">{}</a>:<br/>{}</blockquote>'.format(
+                return u'<blockquote><a href="/users/{}">{}</a>:<br/>{}</blockquote>'.format(
                     profile.user.username,
-                    smart_str(profile.get_display_name()),
+                    unidecode(profile.get_display_name()),
                     content
                 )
 
-        return '<blockquote>{}</blockquote>'.format(content)
+        return u'<blockquote>{}</blockquote>'.format(content)
 
 
 tag_pool.register_tag(QuoteBBCodeTag)
