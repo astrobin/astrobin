b801c3d34de509958947f1e8d5cfb770552c86d9
Salvatore Iovene <salvatore@iovene.com>
2021-07-01 11:47:39 +0200

    (general) Remove unused autocomplete functions (#2412)

--- astrobin/urls.py
+++ astrobin/urls.py
@@ -331,8 +331,6 @@ urlpatterns += [
     ### AUTOCOMPLETE VIEWS                                                 ###
     ###########################################################################
 
-    url(r'^autocomplete/(?P<what>\w+)/$', lookups.autocomplete, name='autocomplete'),
-    url(r'^autocomplete_user/(?P<what>\w+)/$', lookups.autocomplete_user, name='autocomplete_user'),
     url(r'^autocomplete_usernames/$', lookups.autocomplete_usernames, name='autocomplete_usernames'),
     url(r'^autocomplete_images/$', lookups.autocomplete_images, name='autocomplete_images'),
 
