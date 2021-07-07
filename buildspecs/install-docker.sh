002e6840bda09dce6c6b2839182a68066c14d683
Salvatore Iovene <salvatore@iovene.com>
2021-07-07 22:35:37 +0200

    (try bionic)

--- buildspecs/install-docker.sh
+++ buildspecs/install-docker.sh
@@ -12,7 +12,7 @@ else
 fi
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=${UBUNTU_ARCH}] https://download.docker.com/linux/ubuntu focal stable"
+add-apt-repository "deb [arch=${UBUNTU_ARCH}] https://download.docker.com/linux/ubuntu bionic stable"
 apt-cache policy docker-ce
 apt-get install -y docker-ce
 apt-get install -y docker-compose
