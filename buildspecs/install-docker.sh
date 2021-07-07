66e8dd9040c9664dbe60af3a82ce5550d7d3e265
Salvatore Iovene <salvatore@iovene.com>
2021-07-07 22:42:47 +0200

    (upgrade docker only on amd64, as it's needed to run cypress tests)

--- buildspecs/install-docker.sh
+++ buildspecs/install-docker.sh
@@ -5,19 +5,15 @@ export ASTROBIN_GUNICORN_WORKERS=1
 export ARCH=$(uname -m)
 export USER=$(whoami)
 
-if [ "$ARCH" == "aarch64" ]; then
-    UBUNTU_ARCH=arm64
-else
-    UBUNTU_ARCH=amd64
+if [ "$ARCH" == "x86_64" ]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    apt-get update
+    apt-cache policy docker-ce
+    apt-get install -y docker-ce
+    apt-get install -y docker-compose
+    usermod -aG docker ${USER}
 fi
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=${UBUNTU_ARCH}] https://download.docker.com/linux/ubuntu bionic stable"
-apt-get update
-apt-cache policy docker-ce
-apt-get install -y docker-ce
-apt-get install -y docker-compose
-usermod -aG docker ${USER}
 
 docker --version
 docker-compose --version
