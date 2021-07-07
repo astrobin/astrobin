1804c5390cf3f516a21d5a619f0f5b9953ce108c
Salvatore Iovene <salvatore@iovene.com>
2021-07-07 22:09:32 +0200

    (pre-pull correct ubuntu image, upgrade docker for cypress)

--- buildspecs/cypress.sh
+++ buildspecs/cypress.sh
@@ -13,6 +13,18 @@ fi
 docker login --username ${DOCKER_USERNAME} --password ${DOCKER_PASSWORD} || exit 1
 
 npm ci &
+
+# Upgrade docker because the standard CodeBuild image comes with Docker Engine 19
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+apt-get install -y docker-compose
+usermod -aG docker ${USER}
+
+docker --version
+docker-compose --version
+
 docker-compose \
    -f docker/docker-compose-app.yml \
    -f docker/docker-compose-worker.yml \
