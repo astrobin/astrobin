5f34a74fbfefafec36902a41eae92376a09e7d70
Salvatore Iovene <salvatore@iovene.com>
2021-07-07 22:16:08 +0200

    (install docker manually during build)

---
+++ buildspecs/install-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -ex
+
+export ASTROBIN_BUILD=${CODEBUILD_RESOLVED_SOURCE_VERSION}
+export ASTROBIN_GUNICORN_WORKERS=1
+export ARCH=$(uname -m)
+
+if [ "$ARCH" == "aarch64" ]; then
+    UBUNTU_ARCH=arm64
+else
+    UBUNTU_ARCH=amd64
+fi
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=${UBUNTU_ARCH}] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+apt-get install -y docker-compose
+usermod -aG docker ${USER}
+
+docker --version
+docker-compose --version
