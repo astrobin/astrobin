a519813b8657e02d57a2f08df2002c6f180a2ed5
Salvatore Iovene <salvatore@iovene.com>
2021-08-20 19:47:27 +0200

    (fix test)

--- nested_comments/tests/test_comment_post_save.py
+++ nested_comments/tests/test_comment_post_save.py
@@ -2,6 +2,7 @@ import mock
 from django.test import TestCase
 from mock import patch
 
+from astrobin.signals import nested_comment_post_save
 from astrobin.tests.generators import Generators
 from nested_comments.tests.nested_comments_generators import NestedCommentsGenerators
 
@@ -53,3 +54,23 @@ class CommentPostSaveTest(TestCase):
         comment = NestedCommentsGenerators.comment()
 
         push_notification.assert_called_with([user], comment.author, 'new_comment_mention', mock.ANY)
+
+    @patch("astrobin.signals.MentionsService.get_mentions")
+    @patch("astrobin.signals.CommentNotificationsService.send_notifications")
+    @patch("astrobin.signals.CommentNotificationsService.send_moderation_required_notification")
+    def test_nested_comment_post_save_sends_moderation_required_notification(
+            self, send_moderation_required_notification, send_notifications, get_mentions):
+        comment = NestedCommentsGenerators.comment()
+        comment.pending_moderation = True
+
+        nested_comment_post_save(None, comment, True)
+
+        self.assertTrue(get_mentions.called)
+        self.assertFalse(send_notifications.called)
+        self.assertTrue(send_moderation_required_notification.called)
+
+        comment.pending_moderation = False
+
+        nested_comment_post_save(None, comment, True)
+
+        self.assertTrue(send_moderation_required_notification.called)
