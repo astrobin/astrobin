c07ed33a183c252d0bd589a469d7412ab3651e54
Salvatore Iovene <salvatore@iovene.com>
2021-08-24 12:57:20 +0200

    (auth) Allow case-insensitive login if there's only one match (#2489)

--- astrobin/auth.py
+++ astrobin/auth.py
@@ -1,32 +1,29 @@
+from annoying.functions import get_object_or_None
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 
 
-# Class to permit the authentication using email or username
+# Class to permit the authentication using email or username, with case sensitive and insensitive matches.
 class CustomBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         UserModel = get_user_model()
+        case_sensitive = UserModel.objects.filter(Q(username__exact=username) | Q(email__iexact=username)).distinct()
+        case_insensitive = UserModel.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).distinct()
+        user = None
 
-        try:
-            users = UserModel.objects.filter(
-                Q(username__exact=username) |
-                Q(email__iexact=username)
-            ).distinct()
-        except UserModel.DoesNotExist:
-            return None
+        if case_sensitive.exists():
+            user = case_sensitive.first()
+        elif case_insensitive.exists():
+            count = case_insensitive.count()
 
-        if users.exists():
-            user_obj = users.first()
-            if user_obj.check_password(password):
-                return user_obj
-            return None
-        else:
-            return None
+            if count == 1:
+                user = case_insensitive.first()
+
+        if user and user.check_password(password):
+            return user
+
+        return None
 
     def get_user(self, user_id):
-        UserModel = get_user_model()
-        try:
-            return UserModel.objects.get(pk=user_id)
-        except UserModel.DoesNotExist:
-            return None
+        return get_object_or_None(get_user_model(), pk=user_id)
