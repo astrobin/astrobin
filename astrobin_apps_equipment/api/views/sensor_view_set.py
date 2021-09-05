93d4587d4a0e47dcfdbd201a6c36a078aeaf74dd
Salvatore Iovene <salvatore@iovene.com>
2021-09-05 18:43:50 +0200

    (add api for sensor image upload)

--- astrobin_apps_equipment/api/views/sensor_view_set.py
+++ astrobin_apps_equipment/api/views/sensor_view_set.py
@@ -1,6 +1,19 @@
+from rest_framework.decorators import action
+from rest_framework.parsers import MultiPartParser, FormParser
+
+from astrobin_apps_equipment.api.serializers.sensor_image_serializer import SensorImageSerializer
 from astrobin_apps_equipment.api.serializers.sensor_serializer import SensorSerializer
 from astrobin_apps_equipment.api.views.equipment_item_view_set import EquipmentItemViewSet
 
 
 class SensorViewSet(EquipmentItemViewSet):
     serializer_class = SensorSerializer
+
+    @action(
+        detail=True,
+        methods=['PUT'],
+        serializer_class=SensorImageSerializer,
+        parser_classes=[MultiPartParser, FormParser],
+    )
+    def image(self, request, pk):
+        return super(SensorViewSet, self).image_upload(request, pk)
