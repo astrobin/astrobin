6a2da9a37e1dbd914062efaab7741dafc0eb14aa
Salvatore Iovene <salvatore@iovene.com>
2021-09-08 17:46:15 +0200

    (try nohup)

--- buildspecs/cypress.sh
+++ buildspecs/cypress.sh
@@ -4,6 +4,13 @@ export ASTROBIN_BUILD=${CODEBUILD_RESOLVED_SOURCE_VERSION}
 export ASTROBIN_GUNICORN_WORKERS=1
 export ARCH=$(uname -m)
 
+alias compose="docker-compose \
+    -f docker/docker-compose-app.yml \
+    -f docker/docker-compose-worker.yml \
+    -f docker/docker-compose-scheduler.yml \
+    -f docker/docker-compose-local.yml \
+    up"
+
 if [ $ARCH == "aarch64" ]; then
     # https://docs.cypress.io/guides/getting-started/installing-cypress#Download-URLs
     echo "Skipping Cypress tests on aarch64 because Cypress does not support it yet."
@@ -12,28 +19,18 @@ fi
 
 docker login --username ${DOCKER_USERNAME} --password ${DOCKER_PASSWORD} || exit 1
 
-npm ci &
+npm ci || exit 2
 
-docker-compose \
-   -f docker/docker-compose-app.yml \
-   -f docker/docker-compose-worker.yml \
-   -f docker/docker-compose-scheduler.yml \
-   -f docker/docker-compose-local.yml \
-   up &
+nohup compose up &
 
 sleep 5
 
-docker-compose \
-   -f docker/docker-compose-app.yml \
-   -f docker/docker-compose-worker.yml \
-   -f docker/docker-compose-scheduler.yml \
-   -f docker/docker-compose-local.yml \
-   logs -f &
+compose logs -f 2>&1 &
 
 astrobin_attempts=0
 astrobin_max_attempts=24
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://127.0.0.1:8083/accounts/login/)" != "200" ]]; do
-    [[ $astrobin_attempts -eq $astrobin_max_attempts  ]] && echo "Failed!" && exit 2
+    [[ $astrobin_attempts -eq $astrobin_max_attempts ]] && echo "Failed!" && exit 3
     echo "Waiting for astrobin..."
     sleep 5
     ((astrobin_attempts++))
@@ -49,10 +46,12 @@ done
 astrobin_ng_attempts=0
 astrobin_ng_max_attempts=24
 while [[ "$(curl -s -o /dev/null http://127.0.0.1:4400)" ]]; do
-    [[ $astrobin_ng_attempts -eq $astrobin_ng_max_attempts  ]] && echo "Failed!" && exit 3
+    [[ $astrobin_ng_attempts -eq $astrobin_ng_max_attempts  ]] && echo "Failed!" && exit 4
     echo "Waiting for astrobin-ng..."
     sleep 5
     ((astrobin_ng_attempts++))
 done
 
-CYPRESS_baseUrl=http://127.0.0.1:8083 $(npm bin)/cypress run
+CYPRESS_baseUrl=http://127.0.0.1:8083 $(npm bin)/cypress run || exit 5
+
+compose down || exit 6
