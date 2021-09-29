05d63e8870bcceb09ee047371c71d6e9a4b12f2a
Salvatore Iovene <salvatore@iovene.com>
2021-09-29 14:34:14 +0200

    (implement camera edit proposal approval)

--- astrobin_apps_equipment/api/views/camera_edit_proposal_view_set.py
+++ astrobin_apps_equipment/api/views/camera_edit_proposal_view_set.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser, FormParser
 
@@ -6,6 +7,7 @@ from astrobin_apps_equipment.api.serializers.camera_edit_proposal_image_serializ
     CameraEditProposalImageSerializer
 from astrobin_apps_equipment.api.serializers.camera_edit_proposal_serializer import CameraEditProposalSerializer
 from astrobin_apps_equipment.api.views.equipment_item_edit_proposal_view_set import EquipmentItemEditProposalViewSet
+from astrobin_apps_equipment.models import CameraEditProposal
 
 
 class CameraEditProposalViewSet(EquipmentItemEditProposalViewSet):
@@ -23,7 +25,22 @@ class CameraEditProposalViewSet(EquipmentItemEditProposalViewSet):
 
     @action(detail=True, methods=['POST'])
     def approve(self, request, pk):
-        pass
+        edit_proposal: CameraEditProposal = get_object_or_404(CameraEditProposal, pk=pk)
+
+        check_permissions, response = self.check_edit_proposal_permissions(request, edit_proposal)
+        if not check_permissions:
+            return response
+
+        camera = edit_proposal.edit_proposal_target
+        camera.type = edit_proposal.type
+        camera.sensor = edit_proposal.sensor
+        camera.cooled = edit_proposal.cooled
+        camera.max_cooling = edit_proposal.max_cooling
+        camera.back_focus = edit_proposal.back_focus
+
+        camera.save()
+
+        return super().approve(request, pk)
 
     class Meta(EquipmentItemEditProposalViewSet.Meta):
         abstract = False
