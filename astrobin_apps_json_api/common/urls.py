c7e8f3ff0104d8fb1f7e32a21dc3a48ff9a605f3
Salvatore Iovene <salvatore@iovene.com>
2021-10-18 21:46:41 +0200

    (add url-is-available and endure protocol)

--- astrobin_apps_json_api/common/urls.py
+++ astrobin_apps_json_api/common/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from django.views.decorators.cache import never_cache
 
 from astrobin_apps_json_api.common.views import AppConfig, CkEditorUpload
+from astrobin_apps_json_api.common.views.url_is_available import UrlIsAvailable
 
 urlpatterns = (
     url(r'^common/app-config/$', AppConfig.as_view()),
     url(r'^common/ckeditor-upload/$', never_cache(CkEditorUpload.as_view())),
+    url(r'^common/url-is-available/$', never_cache(UrlIsAvailable.as_view()))
 )
