65186a5b39bf779b118dc81afba5987f15e22849
Salvatore Iovene <salvatore@iovene.com>
2021-10-19 15:55:33 +0200

    (add fields to legacy telescope_serializer.py)

--- astrobin/api2/serializers/telescope_serializer.py
+++ astrobin/api2/serializers/telescope_serializer.py
@@ -5,4 +5,8 @@ from astrobin.models import Telescope
 class TelescopeSerializer(GearSerializer):
     class Meta:
         model = Telescope
-        fields = GearSerializer.Meta.fields
+        fields = GearSerializer.Meta.fields + (
+            'aperture',
+            'focal_length',
+            'type',
+        )
