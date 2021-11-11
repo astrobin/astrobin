47e81b01f5e06184c4114a5a14bc3fabc474cdde
Salvatore Iovene <salvatore@iovene.com>
2021-11-11 22:25:46 +0100

    (add mounts to api/v1)

--- astrobin/api2/urls.py
+++ astrobin/api2/urls.py
@@ -5,11 +5,13 @@ from astrobin.api2.views.camera_rename_proposal_view_set import CameraRenameProp
 from astrobin.api2.views.camera_view_set import CameraViewSet
 from astrobin.api2.views.gear_view_set import GearViewSet
 from astrobin.api2.views.location_view_set import LocationViewSet
+from astrobin.api2.views.mount_view_set import MountViewSet
 from astrobin.api2.views.telescope_view_set import TelescopeViewSet
 
 router = routers.DefaultRouter()
 router.register(r'gear', GearViewSet, basename='gear')
 router.register(r'telescope', TelescopeViewSet, basename='telescope')
+router.register(r'mount', MountViewSet, basename='mount')
 router.register(r'camera', CameraViewSet, basename='camera')
 router.register(r'camera-rename-proposal', CameraRenameProposalViewSet, basename='camera-rename-proposal')
 router.register(r'location', LocationViewSet, basename='location')
