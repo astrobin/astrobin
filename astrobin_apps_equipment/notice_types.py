654d2d52c6dc10c32ff4b8f96770e4a93386f7f3
Salvatore Iovene <salvatore@iovene.com>
2021-11-11 13:51:13 +0100

    (equipment) Add notifications for edit proposal created/approved/rejected

--- astrobin_apps_equipment/notice_types.py
+++ astrobin_apps_equipment/notice_types.py
@@ -1,4 +1,7 @@
 EQUIPMENT_NOTICE_TYPES = (
     ('equipment-item-approved', '', '', 2),
     ('equipment-item-rejected', '', '', 2),
+    ('equipment-edit-proposal-created', '', '', 2),
+    ('equipment-edit-proposal-approved', '', '', 2),
+    ('equipment-edit-proposal-rejected', '', '', 2),
 )
