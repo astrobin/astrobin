1bbda308629727b2b13463e235d2f1952e2c1741
Salvatore Iovene <salvatore@iovene.com>
2021-11-21 09:18:31 +0100

    (api/v2) Avoid potentially costly pk__in query in RestoreDeletedImages

--- astrobin_apps_json_api/user/views/restore_deleted_images.py
+++ astrobin_apps_json_api/user/views/restore_deleted_images.py
@@ -1,3 +1,4 @@
+from annoying.functions import get_object_or_None
 from braces.views import JsonRequestResponseMixin, LoginRequiredMixin
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
@@ -9,12 +10,13 @@ from astrobin.models import Image
 class RestoreDeletedImages(JsonRequestResponseMixin, LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         pks = self.request_json["images"]
-        images = Image.all_objects.filter(user=request.user, pk__in=pks)
 
-        if len(pks) != images.count():
-            return self.render_bad_request_response()
+        for pk in pks:
+            image: Image = get_object_or_None(Image.all_objects, pk=pk)
+            if image is None or image.user != request.user:
+                return self.render_bad_request_response()
 
-        images.undelete()
+            image.undelete()
 
         messages.success(request, _("%(number)s image(s) restored." % {"number": len(pks)}))
         return self.render_json_response({"status": "OK"})
