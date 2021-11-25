fc508f0e3a3cb149547f1539880413b1e5ce3ba8
Salvatore Iovene <salvatore@iovene.com>
2021-11-25 22:37:40 +0100

    (images) Add backend check to prevent liking one's own images

--- toggleproperties/views.py
+++ toggleproperties/views.py
@@ -22,27 +22,41 @@ def ajax_add_toggleproperty(request):
         obj = content_type.get_object_for_this_type(pk=object_id)
         response_dict = {}
 
-        # check if it was created already
-        if ToggleProperty.objects.filter(content_type=content_type, object_id=object_id, \
-                                         property_type=property_type, user=request.user):
-            # return conflict response code if already satisfied
-            return HttpResponse(status=409)
+        if ToggleProperty.objects.filter(
+                content_type=content_type,
+                object_id=object_id,
+                property_type=property_type,
+                user=request.user
+        ):
+            return HttpResponse(
+                f'User {request.user.username} already toggled the property {property_type} for object '
+                f'{object_id}/{content_type.pk}.',
+                status=409
+            )
 
-        # if not create it
-        tp = ToggleProperty.objects.create_toggleproperty(property_type, obj, request.user)
+        if content_type.model == 'imagerevision':
+            obj = obj.image
+
+        if property_type == 'like' and (content_type.model == 'image' and request.user == obj.user):
+            return HttpResponse(
+                'Sorry, but you cannot like your own image.',
+                status=400
+            )
+
+        ToggleProperty.objects.create_toggleproperty(property_type, obj, request.user)
 
         if content_type.model == 'image':
             ImageService(obj).record_hit(request)
-        elif content_type.model == 'imagerevision':
-            ImageService(obj.image).record_hit(request)
 
         if settings.TOGGLEPROPERTIES.get('show_count'):
             count = ToggleProperty.objects.toggleproperties_for_object(property_type, obj).count()
             response_dict['count'] = count
 
-        return HttpResponse(simplejson.dumps(response_dict),
-                            'application/javascript',
-                            status=200)
+        return HttpResponse(
+            simplejson.dumps(response_dict),
+            'application/javascript',
+            status=200
+        )
     else:
         return HttpResponse(status=405)
 
@@ -51,23 +65,29 @@ def ajax_add_toggleproperty(request):
 def ajax_remove_toggleproperty(request):
     if request.method == "POST":
         object_id = request.POST.get("object_id")
-        content_type = get_object_or_404(ContentType,
-                                         pk=request.POST.get("content_type_id"))
+        content_type = get_object_or_404(
+            ContentType,
+            pk=request.POST.get("content_type_id")
+        )
         property_type = request.POST.get("property_type")
         response_dict = {}
 
-        tp = get_object_or_404(ToggleProperty, object_id=object_id,
-                               content_type=content_type,
-                               property_type=property_type,
-                               user=request.user)
+        tp = get_object_or_404(
+            ToggleProperty, object_id=object_id,
+            content_type=content_type,
+            property_type=property_type,
+            user=request.user
+        )
         tp.delete()
         obj = content_type.get_object_for_this_type(pk=object_id)
         if settings.TOGGLEPROPERTIES.get('show_count'):
             count = ToggleProperty.objects.toggleproperties_for_object(property_type, obj).count()
             response_dict['count'] = count
 
-        return HttpResponse(simplejson.dumps(response_dict),
-                            'application/javascript',
-                            status=200)
+        return HttpResponse(
+            simplejson.dumps(response_dict),
+            'application/javascript',
+            status=200
+        )
     else:
         return HttpResponse(status=405)
