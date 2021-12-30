8eec9ce717a37bc02779384c2d902a9c95960e6e
Salvatore Iovene <salvatore@iovene.com>
2021-12-30 20:33:00 +0100

    (iotd/queues) Retire classic judgement queue

--- astrobin_apps_iotd/urls.py
+++ astrobin_apps_iotd/urls.py
@@ -1,40 +1,17 @@
 from django.conf.urls import url
-from django.views.decorators.cache import never_cache
 
-from astrobin_apps_iotd.feeds.iotd import IotdFeed, IotdAtomFeed
-from astrobin_apps_iotd.feeds.top_picks import TopPickFeed, TopPickAtomFeed
-from astrobin_apps_iotd.feeds.top_picks_instagram_story import TopPickInstagramStoryFeed, TopPickInstagramStoryAtomFeed
-from astrobin_apps_iotd.views import IotdToggleJudgementAjaxView, IotdJudgementQueueView, IotdArchiveView, \
-    IotdSubmissionQueueView, IotdReviewQueueView
+from astrobin_apps_iotd.feeds.iotd import IotdAtomFeed, IotdFeed
+from astrobin_apps_iotd.feeds.top_picks import TopPickAtomFeed, TopPickFeed
+from astrobin_apps_iotd.feeds.top_picks_instagram_story import TopPickInstagramStoryAtomFeed, TopPickInstagramStoryFeed
+from astrobin_apps_iotd.views import IotdArchiveView
 
 urlpatterns = (
-    # Submissions
-    url(
-        r'^submission-queue/$',
-        never_cache(IotdSubmissionQueueView.as_view()),
-        name='iotd_submission_queue'),
-
-    # Votes
-    url(
-        r'^review-queue/$',
-        never_cache(IotdReviewQueueView.as_view()),
-        name='iotd_review_queue'),
-
-    # Judgements
-    url(
-        r'^toggle-iotd-judgement-ajax/(?P<pk>\d+)/$',
-        never_cache(IotdToggleJudgementAjaxView.as_view()),
-        name='iotd_toggle_judgement_ajax'),
-    url(
-        r'^judgement-queue/$',
-        never_cache(IotdJudgementQueueView.as_view()),
-        name='iotd_judgement_queue'),
-
     # Archive
     url(
         r'^archive/$',
         IotdArchiveView.as_view(),
-        name='iotd_archive'),
+        name='iotd_archive'
+    ),
 
     # Feeds
     url(r'rss/iotd$', IotdFeed(), name='iotd_rss_iotd'),
