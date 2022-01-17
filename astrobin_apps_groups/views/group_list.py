e5a2e24b0d9b220ffe2399b487da33b1f90c8e32
Salvatore Iovene <salvatore@iovene.com>
2022-01-17 17:17:05 +0100

    (groups) Improve sorting and searching on groups list page

--- astrobin_apps_groups/views/group_list.py
+++ astrobin_apps_groups/views/group_list.py
@@ -1,4 +1,4 @@
-from django.db.models import Q
+from django.db.models import Count, Q
 from django.views.generic import ListView
 
 from astrobin_apps_groups.models import Group
@@ -19,28 +19,91 @@ class GroupListView(ListView):
                 'category': 'category',
                 'created': '-date_created',
                 'activity': '-date_updated',
+                'members': '-num_members',
+                'images': '-num_images',
                 'posts': '-forum__post_count',
             }[sort]
         except KeyError:
             sort = '-date_updated'
 
         q = self.request.GET.get('q')
+        queryset = self.get_queryset().annotate(num_images=Count('images'), num_members=Count('members'))
 
         if self.request.user.is_authenticated:
-            context['private_groups'] = \
-                self.get_queryset() \
-                    .filter(public=False) \
-                    .filter(
-                    Q(owner=self.request.user) |
-                    Q(members=self.request.user) |
-                    Q(invited_users=self.request.user)) \
-                    .distinct() \
-                    .order_by(sort)
+            context['owned_private_groups'] = queryset.filter(
+                Q(public=False) &
+                Q(owner=self.request.user)
+            ).order_by(
+                sort
+            )
 
-        context['public_groups'] = \
-            self.get_queryset().filter(public=True).order_by(sort)
+            context['member_private_groups'] = queryset.filter(
+                Q(public=False) &
+                ~Q(owner=self.request.user) &
+                Q(members=self.request.user) &
+                ~Q(invited_users=self.request.user)
+            ).order_by(
+                sort
+            )
+
+            context['invited_private_groups'] = queryset.filter(
+                Q(public=False) &
+                ~Q(owner=self.request.user) &
+                ~Q(members=self.request.user) &
+                Q(invited_users=self.request.user)
+            ).order_by(
+                sort
+            )
+
+            context['owned_public_groups'] = queryset.filter(
+                Q(public=True) &
+                Q(owner=self.request.user)
+            ).order_by(
+                sort
+            )
+
+            context['member_public_groups'] = queryset.filter(
+                Q(public=True) &
+                ~Q(owner=self.request.user) &
+                Q(members=self.request.user) &
+                ~Q(invited_users=self.request.user)
+            ).order_by(
+                sort
+            )
+
+            context['invited_public_groups'] = queryset.filter(
+                Q(public=True) &
+                ~Q(owner=self.request.user) &
+                ~Q(members=self.request.user) &
+                Q(invited_users=self.request.user)
+            ).order_by(
+                sort
+            )
+
+            context['public_groups'] = \
+                queryset.filter(
+                    Q(public=True) &
+                    ~Q(owner=self.request.user) &
+                    ~Q(members=self.request.user) &
+                    ~Q(invited_users=self.request.user)
+                ).order_by(
+                    sort
+                )
+        else:
+            context['public_groups'] = \
+                queryset.filter(
+                    public=True
+                ).order_by(
+                    sort
+                )
 
         if q:
-            context['public_groups'] = context['public_groups'].filter(name__icontains=q)
+            for x in (
+                    'owned_private_groups', 'member_private_groups', 'invited_private_groups',
+                    'owned_public_groups', 'member_public_groups', 'invited_public_groups',
+                    'public_groups',
+            ):
+                if x in context:
+                    context[x] = context[x].filter(name__icontains=q)
 
         return context
