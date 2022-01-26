1bd5e91d1cff68b3f2bbf9ea254f364c07dcdedd
Salvatore Iovene <salvatore@iovene.com>
2022-01-26 23:05:40 +0100

    (general) Do not strip tags when highlighting, fix escaping JS on image navigation tooltip

--- common/templatetags/common_tags.py
+++ common/templatetags/common_tags.py
@@ -336,12 +336,6 @@ def highlight_text(parser, token):
 
     return HighlightTextNode(text, query, **kwargs)
 
-
-@register.filter
-def highlight_text_filter(text, terms):
-    return HighlightingService(text, terms).render_html()
-
-
 @register.simple_tag
 def get_verbose_field_name(instance, field_name):
     return instance._meta.get_field(field_name).verbose_name.title()
