18214efe31be4767efb4a8062ca934bf3a866a58
Salvatore Iovene <salvatore@iovene.com>
2022-01-27 12:29:25 +0100

    (images) Fix rendering of description bbcode thru highlighting filter

--- common/services/highlighting_service.py
+++ common/services/highlighting_service.py
@@ -3,6 +3,7 @@ import regex
 import string
 
 from django.utils.html import strip_tags
+from django.utils.safestring import mark_safe
 from precise_bbcode.templatetags.bbcode_tags import bbcode
 
 from astrobin.settings.components.forum import SANITIZER_ALLOWED_TAGS
@@ -31,55 +32,53 @@ class HighlightingService:
             self.dialect = kwargs['dialect']
 
     def render_html(self) -> str:
-        if self.terms in (None, ''):
-            return self.text
-
         if self.dialect == 'bbcode':
             result = bbcode(self.text)
         else:
             result = self.text
 
-        text_words = [
-            word.translate(str.maketrans('', '', string.punctuation))
-            for word in list(dict.fromkeys(strip_tags(result).split()))
-        ]
+        if self.terms not in (None, ''):
+            text_words = [
+                word.translate(str.maketrans('', '', string.punctuation))
+                for word in list(dict.fromkeys(strip_tags(result).split()))
+            ]
 
-        terms_words = [
-            word.lower()
-            for word in self.terms.split()
-            if not word.startswith("-") and len(word) > 2 and word not in SANITIZER_ALLOWED_TAGS
-        ]
+            terms_words = [
+                word.lower()
+                for word in self.terms.split()
+                if not word.startswith("-") and len(word) > 2 and word not in SANITIZER_ALLOWED_TAGS
+            ]
 
-        half_length = self.max_length / 2
-        padding = max(self.max_length / 10, 20)
-        first_match_index = int(max(
-            min([result.lower().find(x.lower()) for x in self.terms.split() if not x.startswith('-')]) - padding,
-            0
-        ))
-        last_match_index = int(min(
-            max(
-                [result.lower().find(x.lower()) for x in self.terms.split() if not x.startswith('-')]
-            ) + padding,
-            len(self.text)
-        ))
+            half_length = self.max_length / 2
+            padding = max(self.max_length / 10, 20)
+            first_match_index = int(max(
+                min([result.lower().find(x.lower()) for x in self.terms.split() if not x.startswith('-')]) - padding,
+                0
+            ))
+            last_match_index = int(min(
+                max(
+                    [result.lower().find(x.lower()) for x in self.terms.split() if not x.startswith('-')]
+                ) + padding,
+                len(self.text)
+            ))
 
-        middle = (first_match_index + last_match_index) / 2
+            middle = (first_match_index + last_match_index) / 2
 
-        if self.max_length > 0:
-            start = max(min(math.floor(middle - half_length) - 1, first_match_index), 0)
-            end = min(math.ceil(middle + max(half_length, last_match_index - first_match_index)), last_match_index)
-        else:
-            start = 0
-            end = len(result)
+            if self.max_length > 0:
+                start = max(min(math.floor(middle - half_length) - 1, first_match_index), 0)
+                end = min(math.ceil(middle + max(half_length, last_match_index - first_match_index)), last_match_index)
+            else:
+                start = 0
+                end = len(result)
 
-        result = f'{"..." if start > 0 else ""}{result[start:end]}{"..." if end < len(result) else ""}'
+            result = f'{"..." if start > 0 else ""}{result[start:end]}{"..." if end < len(result) else ""}'
 
-        for word in text_words:
-            if word.lower() in terms_words:
-                result = regex.sub(
-                    f'\<(?:[^<>]++|(?0))++\>(*SKIP)(*F)|{word}',
-                    f'<{self.html_tag} class="{self.css_class}">{word}</{self.html_tag}>',
-                    result
-                )
+            for word in text_words:
+                if word.lower() in terms_words:
+                    result = regex.sub(
+                        f'\<(?:[^<>]++|(?0))++\>(*SKIP)(*F)|{word}',
+                        f'<{self.html_tag} class="{self.css_class}">{word}</{self.html_tag}>',
+                        result
+                    )
 
-        return result
+        return mark_safe(result)
