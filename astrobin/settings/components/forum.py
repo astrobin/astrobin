e44806119c836f06e1bc356305705acdc156a56f
Salvatore Iovene <salvatore@iovene.com>
2022-02-16 21:24:15 +0100

    (general) Allow alt/width/height in sanitization of input HTML because of smilies

--- astrobin/settings/components/forum.py
+++ astrobin/settings/components/forum.py
@@ -100,6 +100,6 @@ def pybb_premoderation(user, post_content, forum):
 PYBB_PREMODERATION = pybb_premoderation
 
 SANITIZER_ALLOWED_TAGS = ['b', 'i', 'strong', 'em', 'a', 'img', 'br', 'span', 'p']
-SANITIZER_ALLOWED_ATTRIBUTES = ['href', 'target', 'src', 'class']
+SANITIZER_ALLOWED_ATTRIBUTES = ['href', 'target', 'src', 'class', 'alt', 'width', 'height']
 
 
