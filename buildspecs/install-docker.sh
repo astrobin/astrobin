630499161b2c44286491738f409c140764840977
Salvatore Iovene <salvatore@iovene.com>
2022-03-02 10:23:13 +0100

    (infra) Update docker repository

--- buildspecs/install-docker.sh
+++ buildspecs/install-docker.sh
@@ -7,7 +7,7 @@ export USER=$(whoami)
 
 if [ "$ARCH" == "x86_64" ]; then
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
     apt-get update
     apt-cache policy docker-ce
     apt-get install -y docker-ce
