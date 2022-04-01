6b4b49f49a96aecff15e26dc3269df6c81076fb3
Salvatore Iovene <salvatore@iovene.com>
2022-04-01 08:57:57 +0200

    (general) To avoid the potential for abuse, always put content from Chinese members on a moderation queue (#2750)

--- common/services/moderation_service.py
+++ common/services/moderation_service.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 
 class ModerationService(object):
     @staticmethod
+    def auto_enqueue_for_moderation(user: User) -> bool:
+        moderate_countries = ['ru', 'cn']
+        return hasattr(
+            user, 'userprofile'
+        ) and user.userprofile.last_seen_in_country and user.userprofile.last_seen_in_country in moderate_countries
+
+    @staticmethod
     def auto_approve(user: User) -> bool:
         for domain in settings.AUTO_APPROVE_DOMAINS:
             if user.email.endswith(domain):
