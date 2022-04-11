a3b70389be1787f6fc835595f48c51d46384b05c
Salvatore Iovene <salvatore@iovene.com>
2022-04-11 11:04:54 +0200

    (fix setting language from profile)

--- astrobin/middleware/locale_middleware.py
+++ astrobin/middleware/locale_middleware.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import django
 from django.utils import translation
 from pybb.compat import is_authenticated
 
 from astrobin.middleware.mixins import MiddlewareParentClass
 
 
-class RequestLanguageMiddleware(MiddlewareParentClass):
+class LocaleMiddleware(MiddlewareParentClass):
     def process_request(self, request):
         if is_authenticated(request.user) and \
                 hasattr(request.user, 'userprofile') and \
                 request.user.userprofile.language:
-            request.LANGUAGE_CODE = translation.get_language()
-        else:
-            request.LANGUAGE_CODE = "en"
+            translation.activate(request.user.userprofile.language)
+            request.LANGUAGE_CODE = request.user.userprofile.language
