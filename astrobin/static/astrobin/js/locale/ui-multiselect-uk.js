8ee0ac60b61889f44162374ec8a4ed5867b64940
Salvatore Iovene <salvatore@iovene.com>
2022-04-14 18:14:05 +0200

    (i18n) Add missing Ukrainian files (pending translation)

---
+++ astrobin/static/astrobin/js/locale/ui-multiselect-uk.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale uk, uk-UA
+ */
+
+$.extend($.ui.multiselect.locale, {
+	addAll: 'Add all',
+	removeAll: 'Remove all',
+	itemsCount: 'items selected'
+});
