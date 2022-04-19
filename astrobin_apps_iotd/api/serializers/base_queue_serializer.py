999f0c9002625d569ccfeea5d3e7cb65b7704390
Salvatore Iovene <salvatore@iovene.com>
2022-04-19 17:38:35 +0200

    (add support to iotd queue api)

--- astrobin_apps_iotd/api/serializers/base_queue_serializer.py
+++ astrobin_apps_iotd/api/serializers/base_queue_serializer.py
@@ -3,14 +3,20 @@ from rest_framework import serializers
 from astrobin.api2.serializers.camera_serializer import CameraSerializer
 from astrobin.api2.serializers.telescope_serializer import TelescopeSerializer
 from astrobin.models import Image
+from astrobin_apps_equipment.api.serializers.camera_serializer import CameraSerializer as CameraSerializer2
+from astrobin_apps_equipment.api.serializers.telescope_serializer import TelescopeSerializer as TelescopeSerializer2
 
 
 class BaseQueueSerializer(serializers.ModelSerializer):
     hash = serializers.PrimaryKeyRelatedField(read_only=True)
     w = serializers.IntegerField()
     h = serializers.IntegerField()
+
     imaging_telescopes = TelescopeSerializer(many=True, read_only=True)
     imaging_cameras = CameraSerializer(many=True, read_only=True)
+
+    imaging_telescopes_2 = TelescopeSerializer2(many=True, read_only=True)
+    imaging_cameras_2 = CameraSerializer2(many=True, read_only=True)
 
     def to_representation(self, instance: Image):
         representation = super().to_representation(instance)
@@ -40,5 +46,7 @@ class BaseQueueSerializer(serializers.ModelSerializer):
             'h',
             'imaging_telescopes',
             'imaging_cameras',
+            'imaging_telescopes_2',
+            'imaging_cameras_2',
             'published',
         )
