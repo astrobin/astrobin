e0abdbac34bc4d3e5f8156e2076ee697e2d5dd5e
Salvatore Iovene <salvatore@iovene.com>
2022-05-03 12:47:51 +0200

    (equipment) Add labels to notice types, to be used in notification settings

--- astrobin_apps_equipment/notice_types.py
+++ astrobin_apps_equipment/notice_types.py
@@ -1,10 +1,12 @@
+from django.utils.translation import ugettext_lazy as _
+
 EQUIPMENT_NOTICE_TYPES = (
-    ('equipment-item-approved', '', '', 2),
-    ('equipment-item-rejected', '', '', 2),
-    ('equipment-item-rejected-affected-image', '', '', 2),
-    ('equipment-edit-proposal-created', '', '', 2),
-    ('equipment-edit-proposal-approved', '', '', 2),
-    ('equipment-edit-proposal-rejected', '', '', 2),
-    ('equipment-item-migration-approved', '', '', 2),
-    ('equipment-item-migration-rejected', '', '', 2),
+    ('equipment-item-approved', _('Equipment item you created was approved'), '', 2),
+    ('equipment-item-rejected', _('Equipment item you created was rejected'), '', 2),
+    ('equipment-item-rejected-affected-image', _('Rejected equipment item removed from your image'), '', 2),
+    ('equipment-edit-proposal-created', _('Equipment item received an edit proposal'), '', 2),
+    ('equipment-edit-proposal-approved', _('Your edit proposal was approved'), '', 2),
+    ('equipment-edit-proposal-rejected', _('Your edit proposal was rejected'), '', 2),
+    ('equipment-item-migration-approved', _('Approved migration proposal of an equipment item'), '', 2),
+    ('equipment-item-migration-rejected', _('Rejected migration proposal of an equipment item'), '', 2),
 )
