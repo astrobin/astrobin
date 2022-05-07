97f111a030dc1aa15f51bf69f06d596d6b7c3d1b
Salvatore Iovene <salvatore@iovene.com>
2022-05-07 16:06:59 +0200

    (notifications) Make profile always have language so that users get notifications in right language

--- astrobin/middleware/locale_middleware.py
+++ astrobin/middleware/locale_middleware.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from typing import Optional
 
 from django.utils import translation
+from django.utils.translation import get_language_from_request
 from pybb.compat import is_authenticated
 
 from astrobin.middleware.mixins import MiddlewareParentClass
@@ -8,9 +10,17 @@ from astrobin.middleware.mixins import MiddlewareParentClass
 
 class LocaleMiddleware(MiddlewareParentClass):
     def process_request(self, request):
-        if is_authenticated(request.user) and \
-                hasattr(request.user, 'userprofile') and \
-                request.user.userprofile.language:
-            lang: str = request.user.userprofile.language.lower()
-            translation.activate(lang)
-            request.LANGUAGE_CODE = lang
+        language: Optional[str] = None
+        request_language: Optional[str] = get_language_from_request(request)
+
+        if is_authenticated(request.user) and hasattr(request.user, 'userprofile'):
+            if request.user.userprofile.language:
+                language = request.user.userprofile.language.lower()
+            else:
+                request.user.userprofile.language = request_language
+                request.user.userprofile.save(keep_deleted=True)
+
+        if language:
+            request.LANGUAGE_CODE = language
+            if language != request_language:
+                translation.activate(language)
