4b5d3df9fb3faf564e1296cae7358255a94ee0b1
Salvatore Iovene <salvatore@iovene.com>
2022-05-26 17:53:21 +0200

    (include variants in camera serializer)

--- astrobin_apps_equipment/api/serializers/camera_serializer.py
+++ astrobin_apps_equipment/api/serializers/camera_serializer.py
@@ -1,11 +1,28 @@
+from django.db.models import Q
 from rest_framework import serializers
 
 from astrobin_apps_equipment.api.serializers.equipment_item_serializer import EquipmentItemSerializer
 from astrobin_apps_equipment.models import Camera
+from astrobin_apps_equipment.models.camera_base_model import CameraType
+from astrobin_apps_equipment.services.camera_service import CameraService
 
 
 class CameraSerializer(EquipmentItemSerializer):
     modified = serializers.BooleanField(required=False, default=False)
+    variants = serializers.SerializerMethodField()
+
+    def get_variants(self, camera):
+        if camera.modified or (camera.type == CameraType.DSLR_MIRRORLESS and camera.cooled):
+            return []
+
+        variants = Camera.objects.filter(
+            ~Q(pk=camera.pk) &
+            Q(brand=camera.brand) &
+            Q(name=camera.name) &
+            CameraService.variant_inclusion_query()
+        )
+
+        return CameraSerializer(variants, many=True).data
 
     class Meta(EquipmentItemSerializer.Meta):
         model = Camera
--- astrobin_apps_equipment/services/camera_service.py
+++ astrobin_apps_equipment/services/camera_service.py
@@ -5,6 +5,15 @@ from astrobin_apps_equipment.models.camera_base_model import CameraType
 
 class CameraService:
     @staticmethod
+    def variant_inclusion_query() -> Q:
+        return Q(
+            Q(type=CameraType.DSLR_MIRRORLESS) &
+            Q(
+                 Q(modified=True) | Q(cooled=True)
+            )
+        )
+
+    @staticmethod
     def variant_exclusion_query() -> Q:
         return Q(
             Q(
