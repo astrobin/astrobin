6d2506671369441f1c4e260a13a5cdb43a0cd7f0
Salvatore Iovene <salvatore@iovene.com>
2022-05-27 14:44:10 +0200

    (fix string representation of camera)

--- astrobin_apps_equipment/models/camera.py
+++ astrobin_apps_equipment/models/camera.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.utils.translation import gettext_lazy
 
-from astrobin_apps_equipment.models.camera_base_model import CameraBaseModel
+from astrobin_apps_equipment.models.camera_base_model import CameraBaseModel, CameraType
 
 
 class Camera(CameraBaseModel):
@@ -9,6 +10,22 @@ class Camera(CameraBaseModel):
         default=False,
         editable=False,
     )
+
+    def __str__(self):
+        base = super().__str__()
+
+        if self.type == CameraType.DSLR_MIRRORLESS:
+            modified = gettext_lazy('modified')
+            cooled = gettext_lazy('cooled')
+
+            if self.modified and self.cooled:
+                return f'{base} ({modified} + {cooled})'
+            elif self.modified:
+                return f'{base} ({modified})'
+            elif self.cooled:
+                return f'{base} ({cooled})'
+
+        return base
 
     class Meta(CameraBaseModel.Meta):
         abstract = False
