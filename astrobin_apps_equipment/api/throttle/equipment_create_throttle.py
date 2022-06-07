9ee0e9f4d7e14ac228fff159d583926b9945bdc6
Salvatore Iovene <salvatore@iovene.com>
2022-06-07 14:58:17 +0200

    (equipment/api) Do not throttle paying users

--- astrobin_apps_equipment/api/throttle/equipment_create_throttle.py
+++ astrobin_apps_equipment/api/throttle/equipment_create_throttle.py
@@ -1,11 +1,21 @@
 from rest_framework import throttling
 
+from astrobin_apps_premium.services.premium_service import PremiumService
+from astrobin_apps_premium.templatetags.astrobin_apps_premium_tags import is_free
+
 
 class EquipmentCreateThrottle(throttling.UserRateThrottle):
     rate = '10/day'
 
     def allow_request(self, request, view):
-        if request.method != "POST" or request.user.is_superuser:
-                return True
+        if request.method != "POST":
+            return True
+
+        if request.user.is_superuser:
+            return True
+
+        valid_subscription = PremiumService(request.user).get_valid_usersubscription()
+        if not is_free(valid_subscription):
+            return True
 
         return super().allow_request(request, view)
