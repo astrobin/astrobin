8657efdba048c2b0642d58722dc89f67c75d6d69
Salvatore Iovene <salvatore@iovene.com>
2022-06-09 21:16:55 +0200

    (equipment) Add search index to Sensor (#2821)

--- astrobin_apps_equipment/search_indexes/__init__.py
+++ astrobin_apps_equipment/search_indexes/__init__.py
@@ -2,5 +2,6 @@ from .accessory_index import AccessoryIndex
 from .camera_index import CameraIndex
 from .filter_index import FilterIndex
 from .mount_index import MountIndex
+from .sensor_index import SensorIndex
 from .software_index import SoftwareIndex
 from .telelscope_index import TelescopeIndex
