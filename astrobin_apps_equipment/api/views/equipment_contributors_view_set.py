74b16dcd76a45956237e0c591e0384e77a75a6fe
Salvatore Iovene <salvatore@iovene.com>
2022-06-27 14:19:40 +0200

    (equipment) Make it so only moderators can approve edit proposal that change the name of the item (#2847)

--- astrobin_apps_equipment/api/views/equipment_contributors_view_set.py
+++ astrobin_apps_equipment/api/views/equipment_contributors_view_set.py
@@ -6,12 +6,15 @@ from django.db.models import Count
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework import views
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from astrobin_apps_equipment.models.equipment_item_group import EquipmentItemKlass
 
 
 class EquipmentContributorsViewSet(views.APIView):
+    permission_classes = [AllowAny]
+
     @method_decorator(cache_page(60 * 60))
     def get(self, request):
         cache_key = 'astrobin_apps_equipment_contributors'
