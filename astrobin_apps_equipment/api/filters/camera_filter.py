30cc17c105340e723b1113f88ec9c732e096f99b
Salvatore Iovene <salvatore@iovene.com>
2022-06-30 14:28:43 +0200

    (equipment) Don't show modified/cooled DSLR/mirrorless cameras on review page

--- astrobin_apps_equipment/api/filters/camera_filter.py
+++ astrobin_apps_equipment/api/filters/camera_filter.py
@@ -1,8 +1,17 @@
+from django.db.models import QuerySet
+
 from astrobin_apps_equipment.api.filters.equipment_item_filter import EquipmentItemFilter
 from astrobin_apps_equipment.models import Camera
+from astrobin_apps_equipment.services.camera_service import CameraService
 
 
 class CameraFilter(EquipmentItemFilter):
+    def has_pending_review(self, queryset: QuerySet, value, *args, **kwargs):
+        queryset = super().has_pending_review(queryset, value, args, kwargs)
+        queryset = queryset.filter(CameraService.variant_exclusion_query())
+
+        return queryset
+
     class Meta(EquipmentItemFilter.Meta):
         model = Camera
         fields = EquipmentItemFilter.Meta.fields + ['modified']
