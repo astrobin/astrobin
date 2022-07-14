4a19a468e02dd32789b207b1beb604be139da1f4
Salvatore Iovene <salvatore@iovene.com>
2022-07-14 14:55:13 +0200

    (general) Allow older TLS when checking if URL is available

--- astrobin_apps_json_api/common/views/url_is_available.py
+++ astrobin_apps_json_api/common/views/url_is_available.py
@@ -4,6 +4,8 @@ import requests
 from braces.views import JSONResponseMixin
 from django.views.generic.base import View
 
+from common.tls_adapter import TLSAdapter
+
 log = logging.getLogger('apps')
 
 
@@ -16,9 +18,11 @@ class UrlIsAvailable(JSONResponseMixin, View):
             if not '://' in url:
                 url = f'http://{url}'
             try:
-                requests.head(url, timeout=5)
+                session = requests.session()
+                session.mount('https://', TLSAdapter())
+                session.request('HEAD', url, timeout=5)
                 available = True
-            except requests.ConnectionError:
-                pass
+            except requests.ConnectionError as e:
+                log.debug(f'Unable to connect to {url}: {str(e)}')
 
         return self.render_json_response({'available': available})
---
+++ common/tls_adapter.py
@@ -0,0 +1,18 @@
+import ssl
+
+from requests.adapters import HTTPAdapter
+from urllib3 import poolmanager
+
+
+class TLSAdapter(HTTPAdapter):
+    def init_poolmanager(self, connections, maxsize, block=False):
+        """Create and initialize the urllib3 PoolManager."""
+        ctx = ssl.create_default_context()
+        ctx.set_ciphers('DEFAULT@SECLEVEL=1')
+        self.poolmanager = poolmanager.PoolManager(
+            num_pools=connections,
+            maxsize=maxsize,
+            block=block,
+            ssl_version=ssl.PROTOCOL_TLS,
+            ssl_context=ctx
+        )
