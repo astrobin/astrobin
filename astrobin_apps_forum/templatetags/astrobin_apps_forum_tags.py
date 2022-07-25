1b411c0af76bf747494877cee4c6d88961312030
Salvatore Iovene <salvatore@iovene.com>
2022-07-25 17:22:47 +0200

    (simply don't list empty equipment forums)

--- astrobin_apps_forum/templatetags/astrobin_apps_forum_tags.py
+++ astrobin_apps_forum/templatetags/astrobin_apps_forum_tags.py
@@ -1,11 +1,31 @@
 from typing import List
 
+from annoying.functions import get_object_or_None
 from django.template import Library
-from pybb.models import Category
+from pybb.models import Forum
+
+from astrobin_apps_equipment.models import Accessory, Camera, Filter, Mount, Sensor, Software, Telescope
+from common.services import AppRedirectionService
 
 register = Library()
 
 
 @register.filter
-def exclude_category(categories: List[Category], category_name: str) -> List[Category]:
-    return [x for x in categories if x.name != category_name]
+def exclude_empty_forums_in_category(forums: List[Forum], category_slug: str) -> List[Forum]:
+    return [x for x in forums if x.category.slug != category_slug or x.topic_count > 0]
+
+
+@register.simple_tag
+def forum_equipment_item_url(forum: Forum) -> str:
+    for ModelClass in (
+            Sensor,
+            Camera,
+            Telescope,
+            Mount,
+            Filter,
+            Accessory,
+            Software
+    ):
+        item: ModelClass = get_object_or_None(ModelClass, forum=forum)
+        if item:
+            return AppRedirectionService.redirect(f'/equipment/explorer/{item.klass.lower()}/{item.id}/{item.slug}');
