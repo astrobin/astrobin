6081f19768f1d6599cda71c305230009974701cf
Salvatore Iovene <salvatore@iovene.com>
2022-08-29 10:04:40 +0200

    (equipment) Do not notify moderators if new equipment item is programmatically created as already approved

--- astrobin_apps_equipment/signals.py
+++ astrobin_apps_equipment/signals.py
@@ -280,6 +280,9 @@ def send_equipment_item_requires_moderation_notification(sender, instance, creat
     ):
         return
 
+    if instance.migration_flag_reviewer_decision is not None:
+        return
+
     url: str = build_notification_url(instance.get_absolute_url())
     parsed = urlparse(url)
     url_dict = dict(parse_qsl(parsed.query))
