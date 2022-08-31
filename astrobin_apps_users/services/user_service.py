513c427f01c1b15c57124a40122b6c3b7d512887
Salvatore Iovene <salvatore@iovene.com>
2022-08-31 21:59:10 +0200

    (refactoring) Use UserService to get image queries when indexing

--- astrobin_apps_users/services/user_service.py
+++ astrobin_apps_users/services/user_service.py
@@ -66,17 +66,16 @@ class UserService:
 
     def get_all_images(self) -> QuerySet:
         from astrobin.models import Image
-        return Image.objects_including_wip.filter(user=self.user).union(
-            Image.objects_including_wip.filter(collaborators=self.user)
-        )
+        return Image.objects_including_wip.filter(Q(user=self.user) | Q(collaborators=self.user)).distinct()
+
 
     def get_public_images(self) -> QuerySet:
         from astrobin.models import Image
-        return Image.objects.filter(user=self.user).union(Image.objects.filter(collaborators=self.user))
+        return Image.objects.filter(Q(user=self.user) | Q(collaborators=self.user)).distinct()
 
     def get_wip_images(self) -> QuerySet:
         from astrobin.models import Image
-        return Image.wip.filter(user=self.user).union(Image.wip.filter(collaborators=self.user))
+        return Image.wip.filter(Q(user=self.user) | Q(collaborators=self.user)).distinct()
 
     def get_deleted_images(self) -> QuerySet:
         from astrobin.models import Image
