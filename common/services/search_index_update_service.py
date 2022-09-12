f6ab82e5eaebab61906f590f6b6f50d0d117dddb
Salvatore Iovene <salvatore@iovene.com>
2022-09-12 10:20:27 +0200

    (general) Delay manual indexing (used after m2m changes) to allow Django to process al stages (#2913)

--- common/services/search_index_update_service.py
+++ common/services/search_index_update_service.py
@@ -1,21 +1,21 @@
-from django.apps import apps
+from django.contrib.contenttypes.models import ContentType
 from django.core.cache import cache
 
 
 class SearchIndexUpdateService:
     @staticmethod
-    def update_index(model_class, instance, max_frequency=10):
-        signal_processor = apps.get_app_config('haystack').signal_processor
+    def update_index(instance, max_frequency=10):
+        from astrobin.tasks import update_index
 
-        if not hasattr(signal_processor, 'enqueue_save'):
-            return
+        content_type = ContentType.objects.get_for_model(instance)
 
-        cache_key = f'astrobin_common_search_index_update_service_{model_class.__name__}_{instance.pk}'
+        cache_key = f'astrobin_common_search_index_update_service_{content_type.pk}_{instance.pk}'
 
         if cache.get(cache_key):
             return
 
-        signal_processor.enqueue_save(model_class, instance)
+        # Delay by a few seconds so that the instance will have time to process all the m2m stages.
+        update_index.apply_async(args=(content_type.pk, instance.pk), countdown=30)
 
-        # Don't update an item more frequently than once in 10 seconds.
+        # Don't update an item more frequently than once in `max_frequency` seconds.
         cache.set(cache_key, '1', max_frequency)
