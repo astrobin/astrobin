4f2d529a63138ca169f5bc74f48589bed185e956
Salvatore Iovene <salvatore@iovene.com>
2022-12-08 11:07:33 +0100

    (general) Add ui-multiselect-pt-BR.js and ui-multiselect-pt-BR.js

---
+++ astrobin/static/astrobin/js/locale/ui-multiselect-pt.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale en, en-US
+ */
+
+$.extend($.ui.multiselect.locale, {
+	addAll: 'Adicionar tudo',
+	removeAll: 'Deletar tudo',
+	itemsCount: 'itens selecionados'
+});
