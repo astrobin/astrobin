dc7eee11d493f7afd4fdf0e0f215779ca929b112
Salvatore Iovene <salvatore@iovene.com>
2023-02-10 17:53:07 +0100

    (security) Add some logging to enforce_otp_verification_middleware.py (#2982)

--- astrobin/middleware/enforce_otp_verification_middleware.py
+++ astrobin/middleware/enforce_otp_verification_middleware.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
@@ -7,6 +9,8 @@ from astrobin.middleware.mixins import MiddlewareParentClass
 from astrobin.utils import get_client_country_code
 from astrobin_apps_notifications.utils import push_notification
 from astrobin_apps_users.services import UserService
+
+log = logging.getLogger('apps')
 
 
 class EnforceOtpVerificationMiddleware(MiddlewareParentClass):
@@ -42,6 +46,7 @@ class EnforceOtpVerificationMiddleware(MiddlewareParentClass):
         password = request.POST.get('auth-password')
 
         if not username:
+            log.warning('enforce_otp_verification_middleware: unable to find username in request')
             return
 
         try:
@@ -50,7 +55,14 @@ class EnforceOtpVerificationMiddleware(MiddlewareParentClass):
             is_new_country = country_code.lower() != user.userprofile.last_seen_in_country
 
             if is_new_country or country_code in (None, 'UNKNOWN'):
+                log.debug(
+                    f'enforce_otp_verification_code: user {username} attempted to log in from new country '
+                    f'{country_code}'
+                )
+
                 device, created = self._create_email_device(user)
+
+                log.debug(f'enforce_otp_verification_middleware: user {username} -> email device created = {created}')
 
                 if created:
                     push_notification([user], None, 'access_attempted_from_different_country', {})
@@ -58,10 +70,22 @@ class EnforceOtpVerificationMiddleware(MiddlewareParentClass):
                 try:
                     if user.check_password(password):
                         validate_password(password, user)
+                        log.debug(f'enforce_otp_verification_code: user {username} used a correct and valid password')
+                    else:
+                        log.debug(f'enforce_otp_verification_code: user {username} used an incorrect password')
                 except ValidationError as e:
                     device, created = self._create_email_device(user)
+                    log.debug(
+                        f'enforce_otp_verification_code: user {username} attempted to log in with password {password} '
+                        f'and it does not meet security standards'
+                    )
+
+                    log.debug(
+                        f'enforce_otp_verification_middleware: user {username} -> email device created = {created}'
+                    )
 
                     if created:
                         push_notification([user], None, 'access_attempted_with_weak_password', {})
         except User.DoesNotExist:
+            log.debug(f'enforce_otp_verification_middleware: user {username} does not exist')
             pass
