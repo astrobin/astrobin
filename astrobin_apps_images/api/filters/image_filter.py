ac427828f9132bf5e5172bc9876285cfb6b3c338
Salvatore Iovene <salvatore@iovene.com>
2023-02-22 13:16:58 +0100

    (api/v2/images) Add ability to search by title and description (#2989)

--- astrobin_apps_images/api/filters/image_filter.py
+++ astrobin_apps_images/api/filters/image_filter.py
@@ -1,5 +1,6 @@
+from django.contrib.postgres.search import SearchVector
 from django.db import models
-from django_filters import FilterSet, IsoDateTimeFilter
+from django_filters import CharFilter, FilterSet, IsoDateTimeFilter
 
 from astrobin.models import Image
 from common.filters.list_filter import ListFilter
@@ -8,6 +9,10 @@ from common.filters.list_filter import ListFilter
 class ImageFilter(FilterSet):
     id = ListFilter(field_name="id", lookup_expr='in')
     hash = ListFilter(field_name="hash", lookup_expr='in')
+    q = CharFilter(method='search')
+
+    def search(self, queryset, name, value):
+        return queryset.annotate(search=SearchVector('title', 'description')).filter(search=value)
 
     class Meta:
         model = Image
