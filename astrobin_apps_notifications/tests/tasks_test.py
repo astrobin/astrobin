381ff20b60b9bad2f06ae935bde46022a25bcd46
Salvatore Iovene <salvatore@iovene.com>
2023-03-31 21:29:35 +0200

    (notifications) Avoid multiple 'new_image' notifications if user follows multiple collaborators

--- astrobin_apps_notifications/tests/tasks_test.py
+++ astrobin_apps_notifications/tests/tasks_test.py
@@ -51,6 +51,37 @@ class TasksTest(TestCase):
 
     @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
     @mock.patch('astrobin_apps_notifications.tasks.push_notification')
+    def test_new_image_notification_collaborators_no_duplicates(self, push_notification):
+        user = Generators.user()
+        collaborator1 = Generators.user()
+        collaborator2 = Generators.user()
+
+        follower = Generators.user()
+
+        ToggleProperty.objects.create(
+            property_type='follow',
+            user=follower,
+            content_object=user
+        )
+        ToggleProperty.objects.create(
+            property_type='follow',
+            user=follower,
+            content_object=collaborator1
+        )
+        ToggleProperty.objects.create(
+            property_type='follow',
+            user=follower,
+            content_object=collaborator2
+        )
+
+        image = Generators.image(user=user, is_wip=True)
+        image.collaborators.add(collaborator1)
+        ImageService(image).promote_to_public_area(skip_notifications=False)
+
+        push_notification.assert_called_with([follower], user, 'new_image', mock.ANY)
+
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+    @mock.patch('astrobin_apps_notifications.tasks.push_notification')
     def test_new_image_revision_notification(self, push_notification):
         user = Generators.user()
         follower = Generators.user()
