c5640ee28eaf548d1cd75c091133465b3628aad6
Salvatore Iovene <salvatore@iovene.com>
2023-04-09 12:32:14 +0200

    (auth) Fix some edge cases related to token authentication and add more email logging

--- astrobin/middleware/enforce_otp_verification_middleware.py
+++ astrobin/middleware/enforce_otp_verification_middleware.py
@@ -1,9 +1,12 @@
 import logging
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
+from django_otp import devices_for_user
 from django_otp.plugins.otp_email.models import EmailDevice
+from two_factor.views.utils import validate_remember_device_cookie
 
 from astrobin.middleware.mixins import MiddlewareParentClass
 from astrobin.utils import get_client_country_code
@@ -38,10 +41,24 @@ class EnforceOtpVerificationMiddleware(MiddlewareParentClass):
             user=user, name="default", confirmed=True
         )
 
+    def _remember_agent(self, request, user, otp_device_id):
+        remember = False
+
+        for key, value in request.COOKIES.items():
+            if key.startswith(settings.TWO_FACTOR_REMEMBER_COOKIE_PREFIX) and value:
+                remember = validate_remember_device_cookie(
+                    value,
+                    user,
+                    otp_device_id
+                )
+
+        return remember
+
     def process_request(self, request):
         if not self._process(request):
             return
 
+        response = self.get_response(request)
         handle = request.POST.get('auth-username')
         password = request.POST.get('auth-password')
         user = None
@@ -65,6 +82,25 @@ class EnforceOtpVerificationMiddleware(MiddlewareParentClass):
                 log.debug(f'enforce_otp_verification_middleware: user with email {handle}: multiple found')
                 return
 
+        # Bail if the password is not matching.
+
+        if not user.check_password(password):
+            log.debug(f'enforce_otp_verification_middleware: user {handle} used an incorrect password')
+            return
+
+        # First check if we remember this device.
+
+        devices = list(devices_for_user(user))
+        for device in devices:
+            if self._remember_agent(request, user, device.persistent_id):
+                log.debug(
+                    f'enforce_otp_verification_middleware: user {handle} skips token authentication because we '
+                    f'remember this agent'
+                )
+                return
+
+        # Then check for the country.
+
         country_code = get_client_country_code(request)
         is_new_country = country_code.lower() != user.userprofile.last_seen_in_country
 
@@ -78,28 +114,27 @@ class EnforceOtpVerificationMiddleware(MiddlewareParentClass):
             request.session['enforce_otp_middleware_new_country'] = 1
 
             log.debug(f'enforce_otp_verification_middleware: user {handle} -> email device created = {created}')
+            push_notification([user], None, 'access_attempted_from_different_country', {})
 
-            if created:
-                push_notification([user], None, 'access_attempted_from_different_country', {})
-        else:
-            try:
-                if user.check_password(password):
-                    validate_password(password, user)
-                    log.debug(f'enforce_otp_verification_middleware: user {handle} used a correct and valid password')
-                else:
-                    log.debug(f'enforce_otp_verification_middleware: user {handle} used an incorrect password')
-            except ValidationError as e:
-                device, created = self._create_email_device(user)
-                request.session['enforce_otp_middleware_invalid_password'] = 1
-                log.debug(
-                    f'enforce_otp_verification_middleware: user {handle} attempted to log in with password {password} '
-                    f'and it does not meet security standards'
-                )
+            return
 
-                log.debug(
-                    f'enforce_otp_verification_middleware: user {handle} -> email device created = {created}'
-                )
+        # Then check for the password validity.
 
-                if created:
-                    push_notification([user], None, 'access_attempted_with_weak_password', {})
+        try:
+            validate_password(password, user)
+            log.debug(f'enforce_otp_verification_middleware: user {handle} used a correct and valid password')
+        except ValidationError:
+            device, created = self._create_email_device(user)
+            request.session['enforce_otp_middleware_invalid_password'] = 1
+
+            log.debug(
+                f'enforce_otp_verification_middleware: user {handle} attempted to log in with password {password} '
+                f'and it does not meet security standards'
+            )
+
+            log.debug(
+                f'enforce_otp_verification_middleware: user {handle} -> email device created = {created}'
+            )
+
+            push_notification([user], None, 'access_attempted_with_weak_password', {})
 
