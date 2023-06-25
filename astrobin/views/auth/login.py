59133eeda83d96925ef64e5b3bb1e71f4bc756b2
Salvatore Iovene <salvatore@iovene.com>
2023-06-25 22:50:53 +0200

    (auth) Show more detailed label and help text on login form

--- astrobin/views/auth/login.py
+++ astrobin/views/auth/login.py
@@ -1,9 +1,17 @@
+from two_factor.forms import AuthenticationTokenForm, BackupTokenForm
 from two_factor.views import LoginView as BaseLoginView
+
+from astrobin.auth import CustomAuthenticationForm
 
 
 class LoginView(BaseLoginView):
     success_url_allowed_hosts = {'localhost:4400', 'app.astrobin.com'}
     redirect_authenticated_user = True
+    form_list = (
+        ('auth', CustomAuthenticationForm),
+        ('token', AuthenticationTokenForm),
+        ('backup', BackupTokenForm),
+    )
 
     def get_redirect_url(self):
         url = super().get_redirect_url()
