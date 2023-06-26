36dd1ae5e6278d8086d0b94dc9273d25d8fecc49
Salvatore Iovene <salvatore@iovene.com>
2023-06-26 21:09:48 +0200

    (forums) Add ability to move and split topics

--- astrobin_apps_forum/models.py
+++ astrobin_apps_forum/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from pybb.models import Topic
 
-# Create your models here.
+
+class TopicRedirect(models.Model):
+    category_slug = models.CharField(max_length=255)
+    forum_slug = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='redirects')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Topic redirect'
+        verbose_name_plural = 'Topic redirects'
+        unique_together = ('category_slug', 'forum_slug', 'slug')
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return f'Redirect for Topic {self.topic.id} on {self.created_at}'
--- astrobin_apps_forum/services/forum_service.py
+++ astrobin_apps_forum/services/forum_service.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Union
 
 from annoying.functions import get_object_or_None
@@ -9,8 +10,11 @@ from pybb.models import Forum, ForumReadTracker, Post, Topic, TopicReadTracker
 
 from astrobin_apps_equipment.models import Accessory, Camera, EquipmentItem, Filter, Mount, Sensor, Software, Telescope
 from astrobin_apps_equipment.services import EquipmentItemService
+from astrobin_apps_forum.models import TopicRedirect
 from astrobin_apps_notifications.utils import build_notification_url, push_notification
 from common.services import AppRedirectionService
+
+log = logging.getLogger(__name__)
 
 
 class ForumService:
@@ -43,11 +47,13 @@ class ForumService:
         item: EquipmentItem = ForumService(topic.forum).get_equipment_item()
 
         if not item:
+            log.error(f"Topic {topic.id} is not in an equipment forum")
             return
 
         recipients: QuerySet = EquipmentItemService(item).get_users().exclude(pk=topic.user.pk)
 
         if recipients.exists():
+            log.debug(f"Found {recipients.count()} users for equipment item {item}")
             push_notification(
                 list(recipients),
                 topic.user,
@@ -65,6 +71,8 @@ class ForumService:
                     ),
                 }
             )
+        else:
+            log.debug(f"No users found for equipment item {item}")
 
     @staticmethod
     def get_topic_first_unread(topic: Topic, user: User) -> Union[Post, None]:
@@ -92,3 +100,20 @@ class ForumService:
                 return None
 
         return None
+
+    @staticmethod
+    def create_topic_redirect(new_category_slug:str , new_forum_slug:str , new_slug: str, topic: Topic):
+        # Prevent loops.
+        TopicRedirect.objects.filter(
+            category_slug=new_category_slug,
+            forum_slug=new_forum_slug,
+            slug=new_slug,
+            topic=topic,
+        ).delete()
+
+        TopicRedirect.objects.get_or_create(
+            category_slug=topic.forum.category.slug,
+            forum_slug=topic.forum.slug,
+            slug=topic.slug,
+            topic=topic,
+        )
