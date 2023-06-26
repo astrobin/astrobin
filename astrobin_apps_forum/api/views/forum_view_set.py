9b8b734981f1fe9eb8b685cf529e93c1a8ce1b35
Salvatore Iovene <salvatore@iovene.com>
2023-06-26 16:15:05 +0200

    (forums) Simplify ForumViewSet.select2 massively and use trigram distance

--- astrobin_apps_forum/api/views/forum_view_set.py
+++ astrobin_apps_forum/api/views/forum_view_set.py
@@ -1,6 +1,7 @@
 from django.conf import settings
+from django.contrib.postgres.search import TrigramDistance
 from django.core.paginator import Paginator
-from django.db.models import Q, QuerySet
+from django.db.models import QuerySet
 from django.db.models.functions import Lower
 from djangorestframework_camel_case.parser import CamelCaseJSONParser
 from djangorestframework_camel_case.render import CamelCaseJSONRenderer
@@ -11,7 +12,6 @@ from rest_framework.renderers import BrowsableAPIRenderer
 from rest_framework.response import Response
 
 from astrobin.permissions import CustomForumPermissions
-from astrobin_apps_equipment.models.equipment_item_group import EquipmentItemKlass
 from astrobin_apps_forum.api.filters.forum_filter import ForumFilter
 from astrobin_apps_forum.api.serializers.forum_serializer import ForumSerializer
 from common.permissions import ReadOnly
@@ -41,25 +41,20 @@ class ForumViewSet(viewsets.ModelViewSet):
         q = request.GET.get('q', '')
         is_equipment = request.GET.get('is-equipment', "false").lower() == "true"
 
-        queryset = self.get_queryset().filter(name__icontains=q, group__isnull=True)
-
-        equipment_related_queries = [
-            Q(**{f"is_astrobin_apps_equipment_{model}__isnull": not is_equipment})
-            for model in [k.lower() for k in vars(EquipmentItemKlass) if not k.startswith("__")]
-        ]
-        query_filter = equipment_related_queries.pop()
-        for q in equipment_related_queries:
-            if is_equipment:
-                query_filter |= q
-            else:
-                query_filter &= q
-
-        queryset = queryset.filter(query_filter)
+        queryset = self.get_queryset()\
+            .select_related('category')\
+            .annotate(distance=TrigramDistance('name', q))\
+            .filter(distance__lte=.85)\
+            .exclude(category__slug='group-forums')
 
         if is_equipment:
-            queryset = queryset.order_by(Lower('name'))
+            queryset = queryset\
+                .filter(category__slug='equipment-forums')\
+                .order_by(Lower('name'))
         else:
-            queryset = queryset.order_by('category__position', 'position')
+            queryset = queryset\
+                .exclude(category__slug='equipment-forums')\
+                .order_by('category__position', 'position')
 
         paginator = Paginator(queryset, page_size)
         page = paginator.get_page(page_number)
