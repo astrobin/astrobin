163d43f38e87a973795dc7ab86bd1f4c83c1751e
Salvatore Iovene <salvatore@iovene.com>
2023-07-15 19:22:07 +0200

    (image) Add badge to indicate that image is in IOTD/TP queue (only visible to owner) (#3071)

--- astrobin_apps_iotd/services/iotd_service.py
+++ astrobin_apps_iotd/services/iotd_service.py
@@ -31,6 +31,13 @@ log = logging.getLogger(__name__)
 
 
 class IotdService:
+    def is_in_iotd_queue(self, image: Image) -> bool:
+        return (
+                IotdSubmissionQueueEntry.objects.filter(image=image).exists() or
+                IotdReviewQueueEntry.objects.filter(image=image).exists() or
+                IotdJudgementQueueEntry.objects.filter(image=image).exists()
+        )
+
     def is_iotd(self, image):
         # type: (Image) -> bool
         return \
