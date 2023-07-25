77c15d8e0a94cf46707fa52ab714a6acc240700c
Salvatore Iovene <salvatore@iovene.com>
2023-07-25 22:56:14 +0200

    (add videojs to fancybox)

--- astrobin_apps_images/api/serializers/image_fancybox_serializer.py
+++ astrobin_apps_images/api/serializers/image_fancybox_serializer.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 from astrobin.models import Image
 from astrobin_apps_images.services import ImageService
+from common.templatetags.common_tags import get_mime_type
 
 
 class ImageFancyboxSerializer(serializers.ModelSerializer):
@@ -12,20 +13,25 @@ class ImageFancyboxSerializer(serializers.ModelSerializer):
         representation = super().to_representation(instance)
         final_revision = ImageService(instance).get_final_revision()
 
-        representation.update(
-            {
-                'url': instance.get_absolute_url(),
-                'src': reverse('image_rawthumb', kwargs={'id': instance.get_id(), 'r': 'final', 'alias': 'qhd'}) + '?sync',
-                'thumb': reverse(
-                    'image_rawthumb', kwargs={'id': instance.get_id(), 'r': 'final', 'alias': 'gallery'}
-                ) + '?sync',
-                'caption': f'{instance.user.userprofile.get_display_name()} - "{instance.title}"',
-                'slug': instance.get_id(),
-                'userId': instance.user.pk,
-                'finalRevisionLabel': '0' if final_revision == instance else final_revision.label,
-                'finalRevisionId': None if final_revision == instance else final_revision.id,
-            }
-        )
+        representation.update({
+            'url': instance.get_absolute_url(),
+            'src': reverse('image_rawthumb', kwargs={'id': instance.get_id(), 'r': 'final', 'alias': 'qhd'}) + '?sync',
+            'thumb': reverse(
+                'image_rawthumb', kwargs={'id': instance.get_id(), 'r': 'final', 'alias': 'gallery'}
+            ) + '?sync',
+            'caption': f'{instance.user.userprofile.get_display_name()} - "{instance.title}"',
+            'slug': instance.get_id(),
+            'userId': instance.user.pk,
+            'finalRevisionLabel': '0' if final_revision == instance else final_revision.label,
+            'finalRevisionId': None if final_revision == instance else final_revision.id,
+        })
+
+        if instance.video_file.name:
+            representation.update({
+                'videoUrl': instance.video_file.url,
+                'encodedVideoUrl': instance.encoded_video_file.url,
+                'videoFormat': get_mime_type(instance.video_file.url),
+            })
 
         return representation
 
