39d58b5efc17ec3c2cd3794fcfee0a3912c852d5
Stephen Astels <sastels@gmail.com>
2023-08-02 20:43:52 -0400

    add new_image_in_group notification type

--- astrobin_apps_notifications/types.py
+++ astrobin_apps_notifications/types.py
@@ -31,6 +31,12 @@ NOTICE_TYPES = (
         2,
     ),
     (
+        'new_image_in_group',
+        _('New image from a group you belong to'),
+        '',
+        2,
+    ),
+    (
         'image_solved',
         _('Your image was plate-solved'),
         '',
