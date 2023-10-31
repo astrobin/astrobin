86238cc4452b26db1c3d59f7cf3c12c9ea2a7cf6
Salvatore Iovene <salvatore@iovene.com>
2023-10-31 16:41:11 +0100

    (add feedback categories)

--- astrobin_apps_equipment/models/equipment_item_marketplace_feedback.py
+++ astrobin_apps_equipment/models/equipment_item_marketplace_feedback.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext
 from safedelete.models import SafeDeleteModel
 
 from astrobin_apps_equipment.types.marketplace_feedback import MarketplaceFeedback
+from astrobin_apps_equipment.types.marketplace_feedback_category import MarketplaceFeedbackCategory
 
 EQUIPMENT_ITEM_MARKETPLACE_FEEDBACK_CHOICES = (
     (MarketplaceFeedback.NEGATIVE.value, gettext("Negative")),
@@ -12,6 +13,12 @@ EQUIPMENT_ITEM_MARKETPLACE_FEEDBACK_CHOICES = (
     (MarketplaceFeedback.POSITIVE.value, gettext("Positive")),
 )
 
+EQUIPMENT_ITEM_MARKETPLACE_FEEDBACK_CATEGORY_CHOICES = (
+    (MarketplaceFeedbackCategory.COMMUNICATION.value, gettext("Communication and friendliness")),
+    (MarketplaceFeedbackCategory.SPEED.value, gettext("Speed of delivery")),
+    (MarketplaceFeedbackCategory.ACCURACY.value, gettext("Accuracy of item descriptions")),
+    (MarketplaceFeedbackCategory.PACKAGING.value, gettext("Packaging quality")),
+)
 
 class EquipmentItemMarketplaceFeedback(SafeDeleteModel):
     user = models.ForeignKey(
@@ -22,8 +29,8 @@ class EquipmentItemMarketplaceFeedback(SafeDeleteModel):
         editable=False,
     )
 
-    listing = models.ForeignKey(
-        'astrobin_apps_equipment.EquipmentItemMarketplaceListing',
+    line_item = models.ForeignKey(
+        'astrobin_apps_equipment.EquipmentItemMarketplaceListingLineItem',
         related_name='feedbacks',
         on_delete=models.CASCADE,
         null=False,
@@ -38,9 +45,16 @@ class EquipmentItemMarketplaceFeedback(SafeDeleteModel):
 
     value = models.CharField(
         max_length=16,
-        null=True,
-        blank=True,
+        null=False,
+        blank=False,
         choices=EQUIPMENT_ITEM_MARKETPLACE_FEEDBACK_CHOICES,
+    )
+
+    category = models.CharField(
+        max_length=16,
+        null=False,
+        blank=False,
+        choices=EQUIPMENT_ITEM_MARKETPLACE_FEEDBACK_CATEGORY_CHOICES,
     )
 
     text = models.TextField(
