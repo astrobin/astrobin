55988ae35b46b7819cd2445a8ce5325ac0c25cab
Salvatore Iovene <salvatore@iovene.com>
2023-11-16 11:30:20 +0100

    (images) Add "Deep Space Products Remote" to list of remote observatories (#3115)

--- astrobin/models.py
+++ astrobin/models.py
@@ -931,6 +931,7 @@ class Image(HasSolutionMixin, SafeDeleteModel):
         ("DSP", "Dark Sky Portal"),
         ("DSV", "Deepsky Villa"),
         ("DSC", "DeepSkyChile"),
+        ("DSPR", "Deep Space Products Remote"),
         ("DSW", "DeepSkyWest"),
         ("eEyE", "e-EyE Extremadura"),
         ("EITS", "Eye In The Sky"),
