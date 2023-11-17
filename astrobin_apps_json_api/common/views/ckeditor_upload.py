2b77b48c77011997eee78b801cb78bf84fb43cb6
Salvatore Iovene <salvatore@iovene.com>
2023-11-17 23:05:30 +0100

    (content-editor) Add support for image thumbnails and fancybox

--- astrobin_apps_json_api/common/views/ckeditor_upload.py
+++ astrobin_apps_json_api/common/views/ckeditor_upload.py
@@ -1,14 +1,23 @@
 import logging
+import os
+from io import BytesIO
 
-from braces.views import JSONResponseMixin, LoginRequiredMixin, CsrfExemptMixin
+from braces.views import CsrfExemptMixin, JSONResponseMixin, LoginRequiredMixin
 from django.conf import settings
+from django.core.files.base import ContentFile
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic.base import View
+from easy_thumbnails.files import get_thumbnailer
 
 from astrobin_apps_json_api.common.forms.CkEditorUploadForm import CkEditorUploadForm
+from astrobin_apps_json_api.models import CkEditorFile
 from astrobin_apps_premium.services.premium_service import PremiumService
-from astrobin_apps_premium.templatetags.astrobin_apps_premium_tags import is_any_lite, is_any_premium, \
-    is_any_ultimate
+from astrobin_apps_premium.templatetags.astrobin_apps_premium_tags import (
+    is_any_lite, is_any_premium,
+    is_any_ultimate,
+)
 
 log = logging.getLogger(__name__)
 
@@ -26,6 +35,23 @@ class CkEditorUpload(CsrfExemptMixin, LoginRequiredMixin, JSONResponseMixin, Vie
                 "message": message
             }
         })
+
+    def get(self, request, *args, **kwargs):
+        path = request.GET.get('path', None)
+
+        if path is None:
+            raise Http404
+
+        upload = get_object_or_404(CkEditorFile, upload=path)
+
+        return self.render_json_response(
+            {
+                "pk": upload.pk,
+                "fileName": upload.filename,
+                "url": upload.upload.url,
+                "thumbnail": upload.thumbnail.url if upload.thumbnail else None,
+            }
+        )
 
     def post(self, request, *args, **kwargs):
         log.info("CkEditorUpload: user %d - requested file upload" % request.user.pk)
@@ -64,8 +90,36 @@ class CkEditorUpload(CsrfExemptMixin, LoginRequiredMixin, JSONResponseMixin, Vie
         upload.filesize = uploaded_file.size
         upload.save(keep_deleted=True)
 
+        if self.is_image(uploaded_file):
+            self.create_and_save_thumbnail(upload)
+
         return self.render_json_response({
             "uploaded": 1,
             "fileName": upload.filename,
             "url": upload.upload.url,
+            "thumbnail": upload.thumbnail.url if upload.thumbnail else None,
         })
+
+    def is_image(self, uploaded_file) -> bool:
+        file_extension = os.path.splitext(uploaded_file.name)[1].lower()
+        return file_extension in ['.jpg', '.jpeg', '.png', '.bmp', '.webp']
+
+    def create_and_save_thumbnail(self, upload):
+        options = {
+            'size': (1024, 0),
+            'crop': True,
+            'keep_icc_profile': True,
+            'quality': 80,
+        }
+
+        thumbnailer = get_thumbnailer(upload.upload)
+        thumbnail = thumbnailer.get_thumbnail(options)
+
+        thumb_io = BytesIO()
+        with thumbnail.open('rb') as thumb_file:
+            thumb_io.write(thumb_file.read())
+            thumb_io.seek(0)
+
+        upload.thumbnail.save(thumbnail.name, ContentFile(thumb_io.read()))
+
+        thumb_io.close()
