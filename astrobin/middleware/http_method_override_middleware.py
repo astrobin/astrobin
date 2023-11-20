b9d50ecf56778957ba6c810225317b4c132844ff
Salvatore Iovene <salvatore@iovene.com>
2023-11-20 22:15:24 +0100

    (add logging)

--- astrobin/middleware/http_method_override_middleware.py
+++ astrobin/middleware/http_method_override_middleware.py
@@ -1,5 +1,9 @@
-METHOD_OVERRIDE_HEADER = 'HTTP_X_HTTP_METHOD_OVERRIDE'
+import logging
 
+
+logger = logging.getLogger(__name__)
+
+METHOD_OVERRIDE_HEADER = 'HTTP_X_HTTP_METHOD_OVERRIDE'
 
 class HttpMethodOverrideMiddleware:
     def __init__(self, get_response):
@@ -7,5 +11,7 @@ class HttpMethodOverrideMiddleware:
 
     def __call__(self, request):
         if request.method == 'POST' and METHOD_OVERRIDE_HEADER in request.META:
+            logger.debug(f"Overriding POST method with {request.META[METHOD_OVERRIDE_HEADER]} for {request.path}")
             request.method = request.META[METHOD_OVERRIDE_HEADER]
+
         return self.get_response(request)
