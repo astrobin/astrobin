f508c8898dcd83186433e9b2cf60089ed50356cf
Salvatore Iovene <salvatore@iovene.com>
2023-12-17 23:23:35 +0100

    (add first_Added_to_an_image to lineitem serializer)

--- astrobin_apps_equipment/api/serializers/equipment_item_marketplace_listing_line_item_serializer.py
+++ astrobin_apps_equipment/api/serializers/equipment_item_marketplace_listing_line_item_serializer.py
@@ -2,7 +2,7 @@ from django.db.models import Q
 from rest_framework import serializers
 
 from astrobin.enums.moderator_decision import ModeratorDecision
-from astrobin.models import Image
+from astrobin.models import Image, ImageEquipmentLog
 from astrobin_apps_equipment.api.serializers.equipment_item_marketplace_listing_line_item_image_serializer import \
     EquipmentItemMarketplaceListingLineItemImageSerializer
 from astrobin_apps_equipment.models import EquipmentItemMarketplaceListingLineItem
@@ -15,6 +15,7 @@ class EquipmentItemMarketplaceListingLineItemSerializer(serializers.ModelSeriali
     seller_image_count = serializers.SerializerMethodField(read_only=True)
     item_klass = serializers.SerializerMethodField(read_only=True)
     username = serializers.CharField(source='user.username', read_only=True)
+    first_added_to_an_image = serializers.SerializerMethodField(read_only=True)
 
     def create(self, validated_data):
         user = self.context['request'].user
@@ -27,28 +28,52 @@ class EquipmentItemMarketplaceListingLineItemSerializer(serializers.ModelSeriali
     def get_seller_image_count(self, obj):
         item = obj.item_content_object
 
-        if item.klass == EquipmentItemKlass.TELESCOPE:
-            q = Q(imaging_telescopes_2=item) | Q(guiding_telescopes_2=item)
-        elif item.klass == EquipmentItemKlass.CAMERA:
-            q = Q(imaging_cameras_2=item) | Q(guiding_cameras_2=item)
-        elif item.klass == EquipmentItemKlass.MOUNT:
-            q = Q(mounts_2=item)
-        elif item.klass == EquipmentItemKlass.FILTER:
-            q = Q(filters_2=item)
-        elif item.klass == EquipmentItemKlass.ACCESSORY:
-            q = Q(accessories_2=item)
-        elif item.klass == EquipmentItemKlass.SOFTWARE:
-            q = Q(software_2=item)
-        else:
-            q = Q()
-
         return Image.objects.filter(
-            Q(user=obj.user) & Q(moderator_decision=ModeratorDecision.APPROVED) & q
+            Q(user=obj.user) &
+            Q(moderator_decision=ModeratorDecision.APPROVED) &
+            self._get_equipment_query_for_image(item)
         ).count()
 
     def get_item_klass(self, obj):
         return obj.item_content_object.klass
 
+    def get_first_added_to_an_image(self, obj):
+        log_entry = ImageEquipmentLog.objects.filter(
+            equipment_item_content_type=obj.item_content_type,
+            equipment_item_object_id=obj.item_object_id,
+            image__user=obj.user,
+        ).order_by('date').first()
+
+        if log_entry:
+            return log_entry.date
+
+        image = Image.objects.filter(
+            Q(user=obj.user) &
+            Q(moderator_decision=ModeratorDecision.APPROVED) &
+            self._get_equipment_query_for_image(obj.item_content_object)
+        ).order_by('published').first()
+
+        if image:
+            return image.published
+
+        return None
+
+    def _get_equipment_query_for_image(self, item):
+        if item.klass == EquipmentItemKlass.TELESCOPE:
+            return Q(imaging_telescopes_2=item) | Q(guiding_telescopes_2=item)
+        elif item.klass == EquipmentItemKlass.CAMERA:
+            return Q(imaging_cameras_2=item) | Q(guiding_cameras_2=item)
+        elif item.klass == EquipmentItemKlass.MOUNT:
+            return Q(mounts_2=item)
+        elif item.klass == EquipmentItemKlass.FILTER:
+            return Q(filters_2=item)
+        elif item.klass == EquipmentItemKlass.ACCESSORY:
+            return Q(accessories_2=item)
+        elif item.klass == EquipmentItemKlass.SOFTWARE:
+            return Q(software_2=item)
+
+        return q
+    
     class Meta:
         model = EquipmentItemMarketplaceListingLineItem
         fields = '__all__'
