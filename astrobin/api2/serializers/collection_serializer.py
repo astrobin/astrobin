146e2f8ea0bcf103df2856c9cf9a521a494ce5d6
Salvatore Iovene <salvatore@iovene.com>
2024-01-19 11:30:12 +0100

    (api/v2/collection) Do not serialize all collection images for performance reasons

--- astrobin/api2/serializers/collection_serializer.py
+++ astrobin/api2/serializers/collection_serializer.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
-from astrobin.models import Collection, Location
+from astrobin.models import Collection
 
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = '__all__'
+        exclude = ['images']
