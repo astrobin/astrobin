6f0838c767b91b6f5310019e4a0161c56666f33a
Salvatore Iovene <salvatore@iovene.com>
2024-02-08 01:54:08 +0100

    (iotd) Set last check cache only after resubmitting images

--- astrobin_apps_iotd/tasks.py
+++ astrobin_apps_iotd/tasks.py
@@ -158,8 +158,6 @@ def resubmit_images_for_iotd_tp_consideration_if_they_did_not_get_enough_views()
         logger.warning("last_iotd_tp_resubmission_check not found. Defaulting to 2 hours.")
         delta = timedelta(hours=2)
 
-    cache.set(last_check_cache_key, datetime.now(), 60 * 60 * 24)
-
     recently_expired_images: QuerySet = IotdService.get_recently_expired_unsubmitted_images(delta).filter(
         submitted_for_iotd_tp_consideration__lt=DateTimeService.now() - delta
     )
@@ -176,6 +174,8 @@ def resubmit_images_for_iotd_tp_consideration_if_they_did_not_get_enough_views()
             .count()
         if users_who_saw_this < float(total_submitters) * min_percentage:
             IotdService.resubmit_to_iotd_tp_process(image.user, image)
+
+    cache.set(last_check_cache_key, datetime.now(), 60 * 60 * 24)
 
 
 @shared_task(time_limit=1200)
