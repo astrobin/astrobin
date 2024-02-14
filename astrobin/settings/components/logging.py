e697489eee68f3b160c09f9e3a9ed5bb03581a2f
Salvatore Iovene <salvatore@iovene.com>
2024-02-14 22:46:49 +0100

    (infra) Remove Scout APM

--- astrobin/settings/components/logging.py
+++ astrobin/settings/components/logging.py
@@ -148,11 +148,6 @@ LOGGING = {
             'level': 'DEBUG',
             'propagate': False,
         },
-        'scout_apm': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': True,
-        },
         '': {
             'handlers': ['console'],
             'level': 'DEBUG',
