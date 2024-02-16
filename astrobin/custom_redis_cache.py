9b1c7fa5743938c74e813f2850ba0fbf440d3d7e
Salvatore Iovene <salvatore@iovene.com>
2024-02-16 20:12:23 +0100

    (performance) Cap the entire caching operations at 50ms, including serialization/deserialization

--- astrobin/custom_redis_cache.py
+++ astrobin/custom_redis_cache.py
@@ -1,3 +1,5 @@
+import threading
+
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django_redis.cache import RedisCache
 from redis.exceptions import TimeoutError
@@ -8,23 +10,43 @@ log = logging.getLogger(__name__)
 
 
 class CustomRedisCache(RedisCache):
-    def get(self, key, default=None, version=None, client=None):
+    operation_timeout = 0.05
+
+    def get_with_timeout(self, key, default=None, version=None, client=None):
         try:
             return super().get(key, default, version, client)
         except TimeoutError:
             log.debug(f"TimeoutError while getting key {key}")
             return None
 
-    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None, client=None):
+    def get(self, key, default=None, version=None, client=None):
+        result = [default]
+        thread = threading.Thread(
+            target=lambda: result.insert(0, self.get_with_timeout(key, default, version, client))
+        )
+        thread.start()
+        thread.join(self.operation_timeout)
+        if thread.is_alive():
+            log.debug(f"Timeout while getting key {key}")
+            return default
+        return result[0]
+
+    def set_with_timeout(self, key, value, timeout=DEFAULT_TIMEOUT, version=None, client=None):
         try:
             return super().set(key, value, timeout, version, client)
         except TimeoutError:
             log.debug(f"TimeoutError while setting key {key}")
             return False
 
-    def delete(self, key, version=None, client=None):
-        try:
-            return super().delete(key, version, client)
-        except TimeoutError:
-            log.debug(f"TimeoutError while deleting key {key}")
+    def set(self, key, value, timeout_val=DEFAULT_TIMEOUT, version=None, client=None):
+        result = [False]
+        thread = threading.Thread(
+            target=lambda: result.insert(0, self.set_with_timeout(key, value, timeout_val, version, client))
+        )
+        thread.start()
+        thread.join(self.operation_timeout)
+        if thread.is_alive():
+            log.debug(f"Timeout while setting key {key}")
             return False
+        return result[0]
+
