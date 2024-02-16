3e17ec2e6236ae1704ff6b8572f1d2d462d1938b
Salvatore Iovene <salvatore@iovene.com>
2024-02-16 15:31:59 +0100

    (infra) Drop redis requests if they take more than 50ms

--- astrobin/settings/components/caches.py
+++ astrobin/settings/components/caches.py
@@ -5,7 +5,7 @@ CACHE_TYPE = os.environ.get('CACHE_TYPE', 'redis').strip()
 if CACHE_TYPE == 'redis':
     CACHES = {
         'default': {
-            'BACKEND': 'django_redis.cache.RedisCache',
+            'BACKEND': 'astrobin.custom_redis_cache.CustomRedisCache',
             'LOCATION': os.environ.get('CACHE_URL', 'redis://redis:6379/1').strip(),
             'OPTIONS': {
                 'CLIENT_CLASS': 'django_redis.client.DefaultClient',
@@ -14,6 +14,8 @@ if CACHE_TYPE == 'redis':
                 'CONNECTION_POOL_KWARGS': {
                     'max_connections': 100,
                 },
+                'SOCKET_TIMEOUT': 0.05,
+                'SOCKET_CONNECT_TIMEOUT': 0.05,
             },
             'KEY_PREFIX': 'astrobin',
             'TIMEOUT': 3600,
