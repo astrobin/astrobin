8e5c64b97bb986297971c1919a19992e276f1cf3
Salvatore Iovene <salvatore@iovene.com>
2024-02-27 09:53:39 +0100

    (api/v2/search) Add throttling to image search viewset

--- astrobin_apps_images/api/views/image_search_view.py
+++ astrobin_apps_images/api/views/image_search_view.py
@@ -2,6 +2,7 @@ from djangorestframework_camel_case.render import CamelCaseJSONRenderer
 from drf_haystack.filters import HaystackFilter, HaystackOrderingFilter
 from drf_haystack.viewsets import HaystackViewSet
 from rest_framework.renderers import BrowsableAPIRenderer
+from rest_framework.throttling import ScopedRateThrottle
 
 from astrobin.models import Image
 from astrobin_apps_images.api.serializers import ImageSearchSerializer
@@ -16,3 +17,5 @@ class ImageSearchView(HaystackViewSet):
     filter_backends = (HaystackFilter, HaystackOrderingFilter)
     ordering_fields = ('published', 'title', 'w', 'h', 'likes')
     ordering = ('-published',)
+    throttle_classes = [ScopedRateThrottle]
+    throttle_scope = 'search'
