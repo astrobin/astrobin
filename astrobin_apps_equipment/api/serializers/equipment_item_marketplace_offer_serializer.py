ddae49cd9b145329636e68fb90aef4277f0994c7
Salvatore Iovene <salvatore@iovene.com>
2024-03-25 10:51:34 +0100

    (add user display name to offer api)

--- astrobin_apps_equipment/api/serializers/equipment_item_marketplace_offer_serializer.py
+++ astrobin_apps_equipment/api/serializers/equipment_item_marketplace_offer_serializer.py
@@ -10,6 +10,10 @@ from common.mixins import RequestUserRestSerializerMixin
 class EquipmentItemMarketplaceOfferSerializer(RequestUserRestSerializerMixin, serializers.ModelSerializer):
     listing = serializers.PrimaryKeyRelatedField(queryset=EquipmentItemMarketplaceListing.objects.all())
     line_item = serializers.PrimaryKeyRelatedField(queryset=EquipmentItemMarketplaceListingLineItem.objects.all())
+    user_display_name = serializers.SerializerMethodField(read_only=True)
+
+    def get_user_display_name(self, obj: EquipmentItemMarketplaceOffer):
+        return obj.user.userprofile.get_display_name()
 
     class Meta:
         model = EquipmentItemMarketplaceOffer
