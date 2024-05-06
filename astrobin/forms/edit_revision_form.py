0032462fe98ba485e10062362ae0d9e269cdc649
Salvatore Iovene <salvatore@iovene.com>
2024-05-06 12:51:39 +0200

    (images) Fix square cropping of revisions

--- astrobin/forms/edit_revision_form.py
+++ astrobin/forms/edit_revision_form.py
@@ -69,7 +69,7 @@ class ImageEditRevisionForm(forms.ModelForm):
 
     class Meta:
         model = ImageRevision
-        fields = ('title', 'description', 'mouse_hover_image', 'square_cropping')
+        fields = ('image_file', 'title', 'description', 'mouse_hover_image', 'square_cropping')
         widgets = {
             'description': forms.Textarea(attrs={'rows': 4}),
             'image_file': ImageCropWidget
