9bf1d1478ef3370cb85c6579564cfb8713f3f364
Salvatore Iovene <salvatore@iovene.com>
2024-05-21 13:19:43 +0200

    (fix order of migrations)

--- astrobin_apps_equipment/migrations/0117_equipmentitemlisting_item_full_name.py
+++ astrobin_apps_equipment/migrations/0117_equipmentitemlisting_item_full_name.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('astrobin_apps_equipment', '0117_add_marketplace_models'),
+        ('astrobin_apps_equipment', '0116_add_equipment_item_thumbnail_field'),
     ]
 
     operations = [
--- astrobin_apps_equipment/migrations/0119_make_marketplace_line_item_price_decimal.py
+++ astrobin_apps_equipment/migrations/0119_make_marketplace_line_item_price_decimal.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('astrobin_apps_equipment', '0118_equipmentitemlisting_item_full_name'),
+        ('astrobin_apps_equipment', '0118_add_marketplace_models'),
     ]
 
     operations = [
