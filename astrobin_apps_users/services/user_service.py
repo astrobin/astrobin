ce3e7e0f225f862f7c3a1651ecfaf5d4409baa99
Salvatore Iovene <salvatore@iovene.com>
2024-07-07 09:09:59 +0200

    (general) Fix crash when deleting user

--- astrobin_apps_users/services/user_service.py
+++ astrobin_apps_users/services/user_service.py
@@ -836,29 +836,32 @@ class UserService:
         from astrobin.models import UserProfile
 
         profile: UserProfile = self.user.userprofile
-        profile.followers_count = ToggleProperty.objects.filter(
-            property_type='follow',
-            object_id=self.user.pk,
-            content_type=ContentType.objects.get_for_model(User)
-        ).count()
-        profile.save(keep_deleted=True)
+        if profile:
+            profile.followers_count = ToggleProperty.objects.filter(
+                property_type='follow',
+                object_id=self.user.pk,
+                content_type=ContentType.objects.get_for_model(User)
+            ).count()
+            profile.save(keep_deleted=True)
 
     def update_following_count(self):
         from astrobin.models import UserProfile
 
         profile: UserProfile = self.user.userprofile
-        profile.following_count = ToggleProperty.objects.filter(
-            user=self.user,
-            property_type='follow',
-            content_type=ContentType.objects.get_for_model(User)
-        ).count()
-        profile.save(keep_deleted=True)
+        if profile:
+            profile.following_count = ToggleProperty.objects.filter(
+                user=self.user,
+                property_type='follow',
+                content_type=ContentType.objects.get_for_model(User)
+            ).count()
+            profile.save(keep_deleted=True)
 
     def update_image_count(self):
         from astrobin.models import UserProfile
 
         profile: UserProfile = self.user.userprofile
-        profile.image_count = UserService(self.user).get_public_images().count()
-        profile.wip_image_count = UserService(self.user).get_wip_images().count()
-        profile.deleted_image_count = UserService(self.user).get_deleted_images().count()
-        profile.save(keep_deleted=True)
+        if profile:
+            profile.image_count = UserService(self.user).get_public_images().count()
+            profile.wip_image_count = UserService(self.user).get_wip_images().count()
+            profile.deleted_image_count = UserService(self.user).get_deleted_images().count()
+            profile.save(keep_deleted=True)
