22a316f05b1b656a7221e2981bced55dfc583006
Salvatore Iovene <salvatore@iovene.com>
2024-08-18 22:57:14 +0200

    (security) Set astrobin_paying_user cookie when paying user logs in: can be used as whitelist on WAF

--- astrobin/views/auth/login.py
+++ astrobin/views/auth/login.py
@@ -2,6 +2,8 @@ from two_factor.forms import AuthenticationTokenForm, BackupTokenForm
 from two_factor.views import LoginView as BaseLoginView
 
 from astrobin.auth import CustomAuthenticationForm
+from astrobin_apps_premium.services.premium_service import PremiumService
+from common.services import AppRedirectionService
 
 
 class LoginView(BaseLoginView):
@@ -20,3 +22,19 @@ class LoginView(BaseLoginView):
             return ''
 
         return url
+
+    def done(self, form_list, **kwargs):
+        response = super().done(form_list, **kwargs)
+
+        # If this is paying user, set a cookie to store this information. This can be used to bypass security checks
+        # from AWS WAF.
+        valid_usersubscription = PremiumService(self.request.user).get_valid_usersubscription()
+        if valid_usersubscription:
+            response.set_cookie(
+                'astrobin_paying_user',
+                '1',
+                max_age=60 * 60 * 24 * 30,
+                domain=AppRedirectionService.cookie_domain(self.request)
+            )
+
+        return response
