astrobin/astrobin

commit 19520fd272d538bf005c330502af54dbedd0b236

(api/v2/images) Add hd_anonymized thumbnail to serializer if present, and autogenerate in case of IOTD/TP submission

astrobin_apps_iotd/services/iotd_service.py
@@ -535,6 +535,8 @@ class IotdService:
 
             for alias in ('story', 'hd_anonymized', 'hd_anonymized_crop', 'real_anonymized'):
                 image.thumbnail(alias, 'final')
+                for revision in image.revisions.all():
+                    revision.thumbnail(alias, revision.label)
 
             Image.objects_including_wip.filter(pk=image.pk).update(submitted_for_iotd_tp_consideration=timezone.now())
 

astrobin/signals.py
@@ -372,20 +372,25 @@ def imagerevision_pre_save(sender, instance: ImageRevision, **kwargs):
 pre_save.connect(imagerevision_pre_save, sender=ImageRevision)
 
 
-def imagerevision_post_save(sender, instance, created, **kwargs):
+def imagerevision_post_save(sender, instance: ImageRevision, created: bool, **kwargs):
     if kwargs.get('update_fields', None):
         return
 
     uploading = instance.uploader_in_progress
 
-    if not uploading and instance.video_file.name:
-        if not instance.image_file.name:
-            ImageService(instance).generate_loading_placeholder()
-            generate_video_preview.apply_async(args=(instance.pk, ContentType.objects.get_for_model(ImageRevision).pk))
+    if not uploading:
+        if instance.video_file.name:
+            if not instance.image_file.name:
+                ImageService(instance.image).generate_loading_placeholder()
+                generate_video_preview.apply_async(args=(instance.pk, ContentType.objects.get_for_model(ImageRevision).pk))
 
-        if not instance.encoded_video_file.name:
-            log.debug(f'Encoding video file for {instance} in imagerevision_post_save signal handler')
-            encode_video_file.apply_async(args=(instance.pk, ContentType.objects.get_for_model(ImageRevision).pk))
+            if not instance.encoded_video_file.name:
+                log.debug(f'Encoding video file for {instance} in imagerevision_post_save signal handler')
+                encode_video_file.apply_async(args=(instance.pk, ContentType.objects.get_for_model(ImageRevision).pk))
+
+        if instance.image.submitted_for_iotd_tp_consideration:
+            for alias in ('story', 'hd_anonymized', 'hd_anonymized_crop', 'real_anonymized'):
+                instance.thumbnail(alias)
 
     if instance.image.is_wip:
         return

astrobin_apps_images/api/serializers/image_revision_serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from astrobin.enums.mouse_hover_image import MouseHoverImage
 from astrobin.models import ImageRevision, Image
+from astrobin_apps_images.models import ThumbnailGroup
 from astrobin_apps_platesolving.serializers import SolutionSerializer
 
 
@@ -14,7 +15,7 @@ class ImageRevisionSerializer(serializers.HyperlinkedModelSerializer):
     uploader_in_progress = serializers.NullBooleanField()
     solution = SolutionSerializer(read_only=True)
 
-    def to_representation(self, instance: Image):
+    def to_representation(self, instance: ImageRevision):
         representation = super().to_representation(instance)
 
         thumbnails = [
@@ -25,6 +26,20 @@ class ImageRevisionSerializer(serializers.HyperlinkedModelSerializer):
                 'url': instance.thumbnail(alias, sync=True)
             } for alias in ('gallery', 'story', 'regular', 'hd', 'qhd')
         ]
+
+        # Add hd_anonymized only if it's available (for IOTD/TP queue purposes)
+        thumbnail_group = ThumbnailGroup.objects.filter(
+            image=instance.image,
+            revision=instance.label,
+            hd_anonymized__isnull=False
+        )
+        if thumbnail_group.exists():
+            thumbnails.append({
+                'alias': 'hd_anonymized',
+                'id': instance.pk,
+                'revision': instance.label,
+                'url': thumbnail_group.first().hd_anonymized
+            })
 
         if instance.mouse_hover_image == MouseHoverImage.INVERTED:
             thumbnails += [

astrobin_apps_images/api/serializers/image_serializer.py
@@ -23,6 +23,7 @@ from astrobin_apps_images.api.fields import KeyValueTagsSerializerField
 from astrobin_apps_images.api.serializers import ImageRevisionSerializer
 from astrobin_apps_images.api.serializers.deep_sky_acquisition_serializer import DeepSkyAcquisitionSerializer
 from astrobin_apps_images.api.serializers.solar_system_acquisition_serializer import SolarSystemAcquisitionSerializer
+from astrobin_apps_images.models import ThumbnailGroup
 from astrobin_apps_iotd.models import TopPickArchive, TopPickNominationsArchive
 from astrobin_apps_platesolving.serializers import SolutionSerializer
 from common.serializers import AvatarField, UserSerializer
@@ -100,6 +101,22 @@ class ImageSerializer(serializers.ModelSerializer):
                     'url': instance.thumbnail(alias, '0', sync=True)
                 } for alias in ('gallery', 'story', 'regular', 'hd', 'qhd')
             ]
+
+            # Add hd_anonymized only if it's available (for IOTD/TP queue purposes)
+            thumbnail_group = ThumbnailGroup.objects.filter(
+                image=instance,
+                revision='0',
+                hd_anonymized__isnull=False
+            )
+            if thumbnail_group.exists():
+                thumbnails.append(
+                    {
+                        'alias': 'hd_anonymized',
+                        'id': instance.pk,
+                        'revision': '0',
+                        'url': thumbnail_group.first().hd_anonymized
+                    }
+                )
 
         if instance.mouse_hover_image == MouseHoverImage.INVERTED:
             thumbnails += [

commit 02c4d692c893b3451ec629e9dfa48364121b382e

(iotd) Fix issue with submitting images to the IOTD/TP process introduced in 19520fd272d538bf005c330502af54dbedd0b236

astrobin_apps_iotd/services/iotd_service.py
@@ -536,7 +536,7 @@ class IotdService:
             for alias in ('story', 'hd_anonymized', 'hd_anonymized_crop', 'real_anonymized'):
                 image.thumbnail(alias, 'final')
                 for revision in image.revisions.all():
-                    revision.thumbnail(alias, revision.label)
+                    revision.thumbnail(alias)
 
             Image.objects_including_wip.filter(pk=image.pk).update(submitted_for_iotd_tp_consideration=timezone.now())
 