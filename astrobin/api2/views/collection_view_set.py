f2fe89b8bb4a0043956ebf7af6ccc080d0ffa90b
Salvatore Iovene <salvatore@iovene.com>
2024-10-07 23:27:08 +0200

    (api/v2/collections) Add ability to filter by parent collection

--- astrobin/api2/views/collection_view_set.py
+++ astrobin/api2/views/collection_view_set.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db.models import QuerySet
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from astrobin.api2.serializers.collection_serializer import CollectionSerializer
 from astrobin.models import Collection
@@ -12,13 +13,36 @@ class CollectionViewSet(viewsets.ModelViewSet):
     permission_classes = [ReadOnly]
 
     def get_queryset(self) -> QuerySet:
+        queryset: QuerySet = Collection.objects.all()
+
         if 'user' in self.request.GET:
-            return Collection.objects.filter(user__pk=self.request.GET['user'])
+            queryset = queryset.filter(user__pk=self.request.GET.get('user'))
+        else:
+            queryset = queryset.filter(user=self.request.user)
 
         if 'ids' in self.request.GET:
-            return Collection.objects.filter(pk__in=self.request.GET.get('ids', '').split(','))
+            queryset = queryset.filter(pk__in=self.request.GET.get('ids', '').split(','))
 
-        if not self.request.user.is_authenticated:
-            return Collection.objects.none()
+        if 'parent' in self.request.GET:
+            parent = self.request.GET.get('parent')
+            if parent == 'null':
+                queryset = queryset.filter(parent__isnull=True)
+            else:
+                queryset = queryset.filter(parent__pk=self.request.GET.get('parent'))
 
-        return Collection.objects.filter(user=self.request.user)
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        if 'user' not in request.GET and 'ids' not in request.GET and 'parent' not in request.GET:
+            return Response({'detail': 'You must provide a user, ids or parent parameter.'}, status=400)
+
+        if 'ids' in request.GET:
+            ids = request.GET.get('ids')
+            if not ids:
+                return Response({'detail': 'ids parameter cannot be empty.'}, status=400)
+
+            ids = ids.split(',')
+            if len(ids) > 100:
+                return Response({'detail': 'You cannot request more than 100 collections at once.'}, status=400)
+
+        return super().list(request, *args, **kwargs)
