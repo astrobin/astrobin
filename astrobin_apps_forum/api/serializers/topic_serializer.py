bda89979316fa471c998c7b52087800387154a18
Salvatore Iovene <salvatore@iovene.com>
2024-12-15 22:14:28 +0100

    (api/v2/forums) Remove `readead_by` and add `read` property to TopicSerializer

--- astrobin_apps_forum/api/serializers/topic_serializer.py
+++ astrobin_apps_forum/api/serializers/topic_serializer.py
@@ -3,6 +3,18 @@ from rest_framework import serializers
 
 
 class TopicSerializer(serializers.ModelSerializer):
+    read = serializers.SerializerMethodField()
+
+    def get_read(self, obj: Topic) -> bool:
+        user = self.context['request'].user
+
+        if not user.is_authenticated:
+            return False
+
+        return obj.readed_by.filter(pk=self.context['request'].user.pk).exists()
+
     class Meta:
         model = Topic
-        fields = '__all__'
+        exclude = [
+            'readed_by'
+        ]
