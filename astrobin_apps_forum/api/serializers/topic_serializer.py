bda8f2e2cfeb878f84f75614d8a0f518846959aa
Salvatore Iovene <salvatore@iovene.com>
2024-12-16 15:49:28 +0100

    (api/v2/forums) Add more fields to TopicSerializer

--- astrobin_apps_forum/api/serializers/topic_serializer.py
+++ astrobin_apps_forum/api/serializers/topic_serializer.py
@@ -4,6 +4,11 @@ from rest_framework import serializers
 
 class TopicSerializer(serializers.ModelSerializer):
     read = serializers.SerializerMethodField()
+    forum_name = serializers.CharField(source='forum.name', read_only=True)
+    last_post_username = serializers.CharField(source='last_post.user.username', read_only=True)
+    last_post_user_display_name = serializers.CharField(source='last_post.user.userprofile.get_display_name', read_only=True)
+    last_post_timestamp = serializers.DateTimeField(source='last_post.created', read_only=True)
+    last_post_id = serializers.IntegerField(source='last_post.id', read_only=True)
 
     def get_read(self, obj: Topic) -> bool:
         user = self.context['request'].user
