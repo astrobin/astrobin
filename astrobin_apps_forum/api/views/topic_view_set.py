9a45f720996d15eb6f332fb8f50ab592868fb0df
Salvatore Iovene <salvatore@iovene.com>
2024-12-29 11:32:15 +0100

    (api/v2/forums) Add route to get latest topics that behaves the same as the classic home page

--- astrobin_apps_forum/api/views/topic_view_set.py
+++ astrobin_apps_forum/api/views/topic_view_set.py
@@ -3,11 +3,14 @@ from djangorestframework_camel_case.parser import CamelCaseJSONParser
 from djangorestframework_camel_case.render import CamelCaseJSONRenderer
 from pybb.models import Topic
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.renderers import BrowsableAPIRenderer
+from rest_framework.response import Response
 
 from astrobin.permissions import CustomForumPermissions
 from astrobin_apps_forum.api.filters import TopicFilter
 from astrobin_apps_forum.api.serializers.topic_serializer import TopicSerializer
+from astrobin_apps_forum.services import ForumService
 from common.permissions import ReadOnly
 
 
@@ -22,3 +25,10 @@ class TopicViewSet(viewsets.ModelViewSet):
     def get_queryset(self) -> QuerySet:
         queryset = Topic.objects.filter(on_moderation=False).select_related('forum', 'user')
         return CustomForumPermissions().filter_topics(self.request.user, queryset)
+
+    @action(detail=False, methods=['get'])
+    def latest(self, request) -> Response:
+        topics = ForumService.latest_topics(request.user)
+        page = self.paginate_queryset(topics)
+        serializer = self.get_serializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
