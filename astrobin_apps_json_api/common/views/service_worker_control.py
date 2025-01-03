62850b1bf9485bfad6fa3686495ae4b53670da96
Salvatore Iovene <salvatore@iovene.com>
2025-01-03 10:50:02 +0100

    (infra) Fix service workier control endpoint to use HTTP_AUTHORIZATION header

--- astrobin_apps_json_api/common/views/service_worker_control.py
+++ astrobin_apps_json_api/common/views/service_worker_control.py
@@ -2,14 +2,25 @@ import logging
 
 from braces.views import JSONResponseMixin
 from django.views.generic.base import View
+from rest_framework.authtoken.models import Token
 
 log = logging.getLogger(__name__)
 
 
 class ServiceWorkerControl(JSONResponseMixin, View):
     def get(self, request, *args, **kwargs):
+        if 'HTTP_AUTHORIZATION' in request.META:
+            token_in_header = request.META['HTTP_AUTHORIZATION'].replace('Token ', '')
+            try:
+                token = Token.objects.get(key=token_in_header)
+                user = token.user
+            except Token.DoesNotExist:
+                user = None
+        else:
+            user = request.user
+
         return self.render_json_response(
             {
-                'swEnabled': request.user.is_authenticated and request.user.is_superuser
+                'swEnabled': user is not None and user.is_superuser
             }
         )
