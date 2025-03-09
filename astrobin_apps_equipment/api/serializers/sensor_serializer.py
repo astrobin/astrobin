fa4119ff2534fb80fda21cad414f4c6f6fde670e
Salvatore Iovene <salvatore@iovene.com>
2025-03-09 17:23:15 +0100

    (api/v2/equipment) Only list approved cameras on sensor serializer

--- astrobin_apps_equipment/api/serializers/sensor_serializer.py
+++ astrobin_apps_equipment/api/serializers/sensor_serializer.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from astrobin_apps_equipment.api.serializers.equipment_item_serializer import EquipmentItemSerializer
 
 from astrobin_apps_equipment.models import Camera, Sensor
+from astrobin_apps_equipment.models.equipment_item import EquipmentItemReviewerDecision
 
 
 class SensorSerializer(EquipmentItemSerializer):
@@ -10,7 +11,10 @@ class SensorSerializer(EquipmentItemSerializer):
     cameras = serializers.SerializerMethodField(read_only=True)
 
     def get_cameras(self, item):
-        return Camera.objects.filter(sensor=item.id).values_list('pk', flat=True).order_by('-user_count')
+        return Camera.objects.filter(
+            sensor=item.id,
+            reviewer_decision=EquipmentItemReviewerDecision.APPROVED
+        ).values_list('pk', flat=True).order_by('-user_count')
 
     class Meta:
         model = Sensor
--- astrobin_apps_equipment/models/camera_base_model.py
+++ astrobin_apps_equipment/models/camera_base_model.py
@@ -14,6 +14,7 @@ class CameraType:
     FILM = 'FILM'
     OTHER = 'OTHER'
 
+
 class CameraBaseModel(EquipmentItem):
     CAMERA_TYPES = (
         (CameraType.DEDICATED_DEEP_SKY, _('Dedicated deep-sky camera')),
