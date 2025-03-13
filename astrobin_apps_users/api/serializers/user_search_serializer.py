544a8d1553d349d8140daed546dfdc61155c4930
Salvatore Iovene <salvatore@astrobin.com>
2025-03-13 11:59:58 +0100

    (api/v2/search) Add more fields to user search (#3330)

--- astrobin_apps_users/api/serializers/user_search_serializer.py
+++ astrobin_apps_users/api/serializers/user_search_serializer.py
@@ -1,5 +1,6 @@
 import logging
 
+from rest_framework import serializers
 from drf_haystack.serializers import HaystackSerializer
 
 from astrobin.search_indexes import UserIndex
@@ -8,6 +9,11 @@ log = logging.getLogger(__name__)
 
 
 class UserSearchSerializer(HaystackSerializer):
+    comments_received = serializers.SerializerMethodField()
+
+    def get_comments_received(self, obj) -> int:
+        return getattr(obj, 'comments', 0)
+
     class Meta:
         index_classes = [UserIndex]
         fields = [
@@ -19,4 +25,17 @@ class UserSearchSerializer(HaystackSerializer):
             'followers',
             'contribution_index',
             'normalized_likes',
+            'integration',
+            'top_pick_nominations',
+            'top_picks',
+            'iotds',
+            'normalized_likes',
+            'comments_written',
+            # Use a custom field because using 'comments' directly would raise an error:
+            # AttributeError: 'ManyToOneRel' object has no attribute 'validators'
+            'comments_received',
+            'comment_likes_received',
+            'forum_posts',
+            'forum_post_likes_received',
+            'contribution_index',
         ]
